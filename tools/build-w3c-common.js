0e399762cb71a88db6ab9551d3b7b1d73a46a5ad
Robin Berjon <robin@berjon.com>
2012-05-24 00:44:29 +0200

    outline of build process

---
+++ tools/build-w3c-common.js
@@ -0,0 +1,12 @@
+#!/usr/local/bin/node
+
+ // check that we are in a release branch
+ // get the version number from somewhere
+ // be sure to include RequireJS as a dependency so that just a single file can be loaded
+ //   node ../../r.js -o baseUrl=. paths.requireLib=../../require name=main include=requireLib out=main-built.js
+ // the important parts above are the mapping from paths.requireLib to the require.js source and the include
+ // for the time being, we also need to specify the biblio as a dependency -- that will change of course
+ // output both
+ //   builds/respec-w3c-common.js (latest)
+ //   builds/respec-w3c-common-3.0.7.js (specific version)
+
