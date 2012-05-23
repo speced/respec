188a1992181928f14f7cce2914357a0d2807aa09
Robin Berjon <robin@berjon.com>
2012-05-24 00:47:45 +0200

    added a file from which to get the version

--- tools/build-w3c-common.js
+++ tools/build-w3c-common.js
@@ -5,6 +5,7 @@
  // be sure to include RequireJS as a dependency so that just a single file can be loaded
  //   node ../../r.js -o baseUrl=. paths.requireLib=../../require name=main include=requireLib out=main-built.js
  // the important parts above are the mapping from paths.requireLib to the require.js source and the include
+ // note that we can load r.js here and use its API to drive the above (rather than shelling out)
  // for the time being, we also need to specify the biblio as a dependency -- that will change of course
  // output both
  //   builds/respec-w3c-common.js (latest)
