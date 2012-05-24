711d0e54985b0666fd37787183d38b1fc5112aef
Robin Berjon <robin@berjon.com>
2012-05-24 11:04:07 +0200

    start building the build script

--- tools/build-w3c-common.js
+++ tools/build-w3c-common.js
@@ -1,7 +1,21 @@
 #!/usr/local/bin/node
 
+var exec = require("child_process").exec
+,   fs   = require("fs")
+,   pth  = require("path")
+,   version = fs.readFileSync(pth.join(__dirname, "VERSION"), "utf-8").replace(/\n/g, "")
+;
+
+console.log("VERSION=" + version);
+
+exec("git symbolic-ref HEAD", function (err, stdout, stderr) {
+    if (err) return console.log("ERR: " + err);
+    var branch = stdout.replace(/refs\/heads\//, "").replace(/\n/, "");
+    console.log("<" + branch + ">");
+});
+
  // check that we are in a release branch
- // get the version number from somewhere
+ // get the version number from VERSION
  // be sure to include RequireJS as a dependency so that just a single file can be loaded
  //   node ../../r.js -o baseUrl=. paths.requireLib=../../require name=main include=requireLib out=main-built.js
  // the important parts above are the mapping from paths.requireLib to the require.js source and the include
