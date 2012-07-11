77037e136947d0be79b6a3eb3295e3fb8a9ece73
Robin Berjon <robin@berjon.com>
2012-07-11 00:27:02 -0400

    drop unhtml5 as it is no longer needed

--- js/profile-w3c-common.js
+++ js/profile-w3c-common.js
@@ -25,7 +25,7 @@ define([
         ,   "w3c/informative"
         ,   "core/section-refs"
         ,   "core/id-headers"
-        ,   "w3c/unhtml5"
+        // ,   "w3c/unhtml5"
         ,   "core/remove-respec"
         ],
         function (domReady, runner) {
