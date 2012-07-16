e3090d95461c42782fdf97891bda22dc4a8b5940
Robin Berjon <robin@berjon.com>
2012-07-16 13:25:59 +0200

    examples can now have more structure

--- js/core/examples.js
+++ js/core/examples.js
@@ -9,43 +9,60 @@
 define(
     ["text!core/css/examples.css"],
     function (css) {
+        var makeTitle = function ($el, num, report) {
+            var txt = (num > 0) ? " " + num : ""
+            ,   $tit = $("<div class='example-title'><span>Example" + txt + "</span></div>");
+            report.title = $el.attr("title");
+            if (report.title) {
+                $tit.append($el[0].ownerDocument.createTextNode(": " + report.title));
+                $el.removeAttr("title");
+            }
+            return $tit;
+        };
+        
         return {
             run:    function (conf, doc, cb, msg) {
                 msg.pub("start", "core/examples");
-                var $exes = $("pre.example, pre.illegal-example");
+                var $exes = $("pre.example, pre.illegal-example, aside.example")
+                ,   num = 0
+                ;
                 if ($exes.length) {
                     $(doc).find("head link").first().before($("<style/>").text(css));
                     $exes.each(function (i, ex) {
                         var $ex = $(ex)
-                        ,   num = i + 1
                         ,   report = { number: num, illegal: $ex.hasClass("illegal-example") }
                         ;
-                        // reindent
-                        var lines = $ex.html().split("\n");
-                        while (lines.length && /^\s*$/.test(lines[0])) lines.shift();
-                        while (lines.length && /^\s*$/.test(lines[lines.length - 1])) lines.pop();
-                        var matches = /^(\s+)/.exec(lines[0]);
-                        if (matches) {
-                            var rep = new RegExp("^" + matches[1]);
-                            for (var j = 0; j < lines.length; j++) {
-                                lines[j] = lines[j].replace(rep, "");
+                        if ($ex.is("aside")) {
+                            num++;
+                            var $tit = makeTitle($ex, num, report);
+                            $ex.prepend($tit);
+                            msg.pub("example", report);
+                        }
+                        else {
+                            var inAside = !!$ex.parents("aside").length;
+                            if (!inAside) num++;
+                            // reindent
+                            var lines = $ex.html().split("\n");
+                            while (lines.length && /^\s*$/.test(lines[0])) lines.shift();
+                            while (lines.length && /^\s*$/.test(lines[lines.length - 1])) lines.pop();
+                            var matches = /^(\s+)/.exec(lines[0]);
+                            if (matches) {
+                                var rep = new RegExp("^" + matches[1]);
+                                for (var j = 0; j < lines.length; j++) {
+                                    lines[j] = lines[j].replace(rep, "");
+                                }
                             }
+                            report.content = lines.join("\n");
+                            $ex.html(lines.join("\n"));
+                            // wrap
+                            var $div = $("<div class='example'></div>")
+                            ,   $tit = makeTitle($ex, inAside ? 0 : num, report)
+                            ;
+                            $div.append($tit);
+                            $div.append($ex.clone());
+                            $ex.replaceWith($div);
+                            if (!inAside) msg.pub("example", report);
                         }
-                        report.content = lines.join("\n");
-                        $ex.html(lines.join("\n"));
-                        // wrap
-                        var $div = $("<div class='example'></div>")
-                        ,   $tit = $("<div class='example-title'><span>Example " + num + "</span></div>")
-                        ;
-                        report.title = $ex.attr("title");
-                        if (report.title) {
-                            $tit.append(doc.createTextNode(": " + report.title));
-                            $ex.removeAttr("title");
-                        }
-                        $div.append($tit);
-                        $div.append($ex.clone());
-                        $ex.replaceWith($div);
-                        msg.pub("example", report);
                     });
                 }
                 msg.pub("end", "core/examples");
