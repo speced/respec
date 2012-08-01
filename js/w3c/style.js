a8fe564873b0266724ff615d2605ae6949585f6a
Robin Berjon <robin@berjon.com>
2012-08-01 10:37:51 +0200

    fix scheme-related bug

--- js/w3c/style.js
+++ js/w3c/style.js
@@ -20,19 +20,19 @@ define(
                 if (statStyle === "finding" || statStyle === "draft-finding") statStyle = "base";
                 var css;
                 if (statStyle === "unofficial") {
-                    css = conf.scheme + "://www.w3.org/StyleSheets/TR/w3c-unofficial";
+                    css = conf.httpScheme + "://www.w3.org/StyleSheets/TR/w3c-unofficial";
                 }
                 else if (statStyle === "base") {
-                    css = conf.scheme + "://www.w3.org/StyleSheets/TR/base";
+                    css = conf.httpScheme + "://www.w3.org/StyleSheets/TR/base";
                 }
                 else if (statStyle === "CG-DRAFT" || statStyle === "CG-FINAL" ||
                          statStyle === "BG-DRAFT" || statStyle === "BG-FINAL") {
                     // note: normally, the ".css" is not used in W3C, but here specifically it clashes
                     // with a PNG of the same base name. CONNEG must die.
-                    css = conf.scheme + "://www.w3.org/community/src/css/spec/" + statStyle.toLowerCase() + ".css";
+                    css = conf.httpScheme + "://www.w3.org/community/src/css/spec/" + statStyle.toLowerCase() + ".css";
                 }
                 else {
-                    css = conf.scheme + "://www.w3.org/StyleSheets/TR/W3C-" + statStyle;
+                    css = conf.httpScheme + "://www.w3.org/StyleSheets/TR/W3C-" + statStyle;
                 }
                 utils.linkCSS(doc, css);
                 msg.pub("end", "w3c/style");
