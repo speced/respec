278e2071e71df57a46227dbc81ac94d6cbdf3455
Tobie Langel <tobie.langel@gmail.com>
2012-10-17 12:39:06 +0200

    Add specs for core/requirements.

---
+++ tests/spec/core/requirements-spec.js
@@ -0,0 +1,51 @@
+describe("Core — Requirements", function () {
+    var MAXOUT = 5000
+    ,   basicConfig = {
+            editors:    [{ name: "Robin Berjon" }]
+        ,   specStatus: "WD"
+        }
+    ,   issueBaseConfig = {
+            editors:    [{ name: "Gregg Kellogg" }]
+        ,   issueBase:  "http://example.com/issues/"
+        ,   specStatus: "WD"
+      };
+    it("should process requirements", function () {
+        var doc;
+        runs(function () {
+            makeRSDoc({
+                        config: basicConfig
+                    ,   body: $("<p class='req' id='req-id'>REQ</p>")
+                    },
+                    function (rsdoc) { doc = rsdoc; });
+        });
+        waitsFor(function () { return doc; }, MAXOUT);
+        runs(function () {
+            var $req = $("p.req", doc)
+            ,   $a = $req.find("a")
+            ;
+
+            expect($req.text()).toEqual("Req. 1: REQ");
+
+            expect($a.length).toEqual(1);
+            expect($a.text()).toEqual("Req. 1");
+            expect($a.attr("href")).toEqual("#req-id");
+        });
+    });
+    it("should process requirement references", function () {
+      var doc;
+      runs(function () {
+          makeRSDoc({
+                      config: basicConfig
+                      ,   body: $("<a href='#req-id' class='reqRef'></a><a href='#foo' class='reqRef'></a><p class='req' id='req-id'>REQ</p>")
+                  },
+                  function (rsdoc) { doc = rsdoc; });
+      });
+      waitsFor(function () { return doc; }, MAXOUT);
+      runs(function () {
+        var $refs = $("a.reqRef", doc);
+        
+        expect($refs.first().text()).toEqual("Req. 1");
+        expect($refs.last().text()).toEqual("Req. not found 'foo'");
+      });
+    });
+});
