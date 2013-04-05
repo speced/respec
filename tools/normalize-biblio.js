f5f20793d2b9d1d21cb930e8ba5a1aa3ac930c3b
Tobie Langel <tobie.langel@gmail.com>
2013-04-05 12:04:58 +0200

    Refactor bib ref normalizing script.

--- tools/normalize-biblio.js
+++ tools/normalize-biblio.js
@@ -9,28 +9,13 @@ json = json.replace(INTRO, '').replace(/;\s*$/, '');
 json = JSON.parse(json);
 
 console.log("Sorting references...");
-var keys = Object.keys(json).sort(function(a, b) { return a.toLowerCase().localeCompare(b.toLowerCase()) });
 
-var PREFIX = '    ';
-function pad(str) {
-    return str.split("\n").map(function(line) { return PREFIX + line; }).join("\n");
-}
-
-console.log("Normalizing references...");
-var output = [];
-keys.forEach(function(k) {
-    output.push(pad('"' + k + '": ' + JSON.stringify(json[k], null, 4)));
-});
-output = "{\n" + output.join(",\n") + "\n}";
-
-console.log("Verifying output...");
-try {
-    var json2 = JSON.parse(output);
-    if (json2.length !== json.length) throw new Error("Lost some references in the process!");
-} catch(e) {
-    console.log(e);
-    process.exit(1);
-}
+var output = Object.keys(json).sort(function(a, b) {
+    return a.toLowerCase().localeCompare(b.toLowerCase())
+}).reduce(function(output, k) {
+    output[k] = json[k];
+    return output;
+}, {});
 
 console.log("Writing output to " + FILE + "...");
-fs.writeFileSync(FILE, INTRO + output + OUTRO, 'utf8');
+fs.writeFileSync(FILE, INTRO + JSON.stringify(output, null, 4) + OUTRO, 'utf8');
