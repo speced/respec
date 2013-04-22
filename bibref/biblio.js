1e88824e327e2ba6af577b524301182a23e061b8
Marcos Caceres <marcos.caceres@wacapps.net>
2013-04-22 09:56:01 +0100

    Modernized and updated Unicode

--- bibref/biblio.js
+++ bibref/biblio.js
@@ -5915,7 +5915,11 @@ berjon.biblio = {
         "status": "NOTE",
         "publisher": "W3C"
     },
-    "UNICODE": "The Unicode Consortium. <a href=\"http://www.unicode.org/unicode/standard/versions/enumeratedversions.html\"><cite>The Unicode Standard.</cite></a> 2003. Defined by: The Unicode Standard, Version 4.0 (Boston, MA, Addison-Wesley, ISBN 0-321-18578-1), as updated from time to time by the publication of new versions URL: <a href=\"http://www.unicode.org/unicode/standard/versions/enumeratedversions.html\">http://www.unicode.org/unicode/standard/versions/enumeratedversions.html</a> ",
+    "UNICODE": {
+        "href": "http://www.unicode.org/versions/latest/",
+        "publisher": "The Unicode Consortium",
+        "title": "The Unicode Standard"
+    },
     "UNICODE-XML": {
         "authors": [
             "Asmus Freytag",
