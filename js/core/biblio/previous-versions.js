df3a0ff4410847638d9ba3322214068c1d93727c
Marcos Caceres <marcos.caceres@wacapps.net>
2013-05-06 16:16:43 +0100

    style fix

--- js/core/biblio/previous-versions.js
+++ js/core/biblio/previous-versions.js
@@ -2,20 +2,20 @@
     expr:   true
 */
 // Module core/biblio/previous-versions
-// This module provides an object that finds "previousVersions" 
+// This module provides an object that finds "previousVersions"
 // properties in bibliographies and expands them so they can
-// be used in specifications. 
+// be used in specifications.
 
-define([], function () {
-    var prevVersionExpander = {},
-        biblio;
+define([], function() {
+    'use strict';
+    var prevVersionExpander = {};
 
     function expandPrevVersions(bib, msg) {
         var props = Object.getOwnPropertyNames(bib);
 
         function hasPrevVersion(key) {
             var entry = bib[key],
-                prop = "previousVersions",
+                prop = 'previousVersions',
                 isValid = !! entry[prop] && entry[prop] instanceof Array;
             if (isValid) {
                 processPrevVersions(key, entry);
@@ -23,29 +23,28 @@ define([], function () {
         }
 
         function processPrevVersions(key, parentEntry) {
-            parentEntry.previousVersions.forEach(function (prevObj, index) {
-                var warning = "",
-                    newKey = key + "-";
+            parentEntry.previousVersions.forEach(function(prevObj, index) {
+                var warning = '',
+                    newKey = key + '-' + prevObj['key'];
 
-                if ((typeof prevObj) !== "object") {
-                    warning = "Expected Object, but got an " + type + "?";
-                    warning += "Please fix: " + key + " at index " + index + ".";
-                    msg("error",warning);
+                if ((typeof prevObj) !== 'object') {
+                    warning = 'Expected Object, but got ' + (typeof prevObj) + '?';
+                    warning += ' Please fix: ' + key + ' at index ' + index + '.';
+                    msg('error', warning);
                     return;
                 }
 
-                if (!prevObj.hasOwnProperty("key")) {
-                    warning = "Previous versions require a key property. ";
-                    warning += "Please fix: " + key;
-                    msg("error",warning);
+                if (!prevObj.hasOwnProperty('key')) {
+                    warning = 'Previous versions require a key property. ';
+                    warning += 'Please fix: ' + key;
+                    msg('error', warning);
                     return;
                 }
 
-                newKey += String(prevObj.key);
                 if (bib[newKey]) {
-                    warning = "Previous version (" + newKey + ") already in biblio. ";
-                    warning += "Please fix previousVersion key: " + prevObj.key;
-                    msg("error",warning);
+                    warning = 'Previous version (' + newKey + ') already in biblio. ';
+                    warning += 'Please fix previousVersion key: ' + prevObj.key;
+                    msg('error', warning);
                     return;
                 }
                 addPrevVersion(newKey, parentEntry, prevObj);
@@ -58,7 +57,12 @@ define([], function () {
                 allProps = parentProps.concat(newProps).sort().filter(clean),
                 newEntry = bib[newKey] = {};
 
-            allProps.forEach(function (prop) {
+            //Don't add props to aliased entries, they are handeled by another plugin
+            if (prevObj.hasOwnProperty('aliasOf')) {
+                newEntry.aliasOf = prevObj.aliasOf;
+                return;
+            }
+            allProps.forEach(function(prop) {
                 var value = prevObj[prop] || parentEntry[prop];
                 this[prop] = value;
             }, newEntry);
@@ -66,7 +70,7 @@ define([], function () {
             //remove duplicates, "previousVersions", and "key" prop
 
             function clean(elem, pos, self) {
-                if (elem === "previousVersions" || elem === "key") {
+                if (elem === 'previousVersions' || elem === 'key') {
                     return false;
                 }
                 return self.lastIndexOf(elem) === pos;
@@ -75,28 +79,33 @@ define([], function () {
         props.forEach(hasPrevVersion);
     }
 
-    prevVersionExpander.run = function (conf, doc, cb, msg) {
-        var props ={
-            get: function () {
+    prevVersionExpander.run = function(conf, doc, cb, msg) {
+        var biblio,
+            props = {
+            get: function() {
                 return biblio;
             },
-            set: function (obj) {
-                biblio = obj;
-                expandPrevVersions(obj, msg);
+            set: function(obj) {
+                if ((typeof obj) === 'object') {
+                    biblio = obj;
+                    expandPrevVersions(obj, msg);
+                }
             }
         };
-        msg.pub("start", "core/biblio/previous-versions");
+        msg.pub('start', 'core/biblio/previous-versions');
         if (conf.localBiblio) {
             expandPrevVersions(conf.localBiblio, msg);
         }
         if (berjon.biblio) {
             expandPrevVersions(berjon.biblio, msg);
         } else {
-            Object.defineProperty(berjon, "biblio", props);
+            //we take over getting and setting of
+            //berjon.biblio, so to dynamically
+            //expand the biblio when loaded.
+            Object.defineProperty(berjon, 'biblio', props);
         }
-        msg.pub("end", "core/fix-headers");
+        msg.pub('end', 'core/fix-headers');
         cb();
     };
-
     return prevVersionExpander;
 });
