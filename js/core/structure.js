cab584add43b83af601f5a520959f6c24c005eab
sspeiche <sspeiche@gmail.com>
2014-01-15 16:26:04 -0500

    Issue292 maxTocLevel doesn't continue numbering sections

--- js/core/structure.js
+++ js/core/structure.js
@@ -66,8 +66,9 @@ define(
                                       .append(isIntro ? "" : $span.clone())
                                       .append($kidsHolder.contents());
                     var $item = $("<li class='tocline'/>").append($a);
-                    $ul.append($item);
-                    if (conf.maxTocLevel && level >= conf.maxTocLevel) continue;
+                    if (conf.maxTocLevel && level <= conf.maxTocLevel) {
+                    	$ul.append($item);
+                    }
                     current.push(0);
                     var $sub = makeTOCAtLevel($sec, doc, current, level + 1, conf);
                     if ($sub) $item.append($sub);
