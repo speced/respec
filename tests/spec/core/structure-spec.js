7481df3871a416494fac0c3a45148f2b140e7145
Shane McCarron <halindrome@gmail.com>
2014-04-26 09:17:53 -0500

    Made various changes to support the following:
      role of 'main' around main sections of document
      role of 'complementary' around appendices
      role of 'navigation' on table of contents section
      role of 'note' on notes
      role of 'definition' on definitions
      aria-describedby on dfn elements linking to their definitions
      aria-describedby and aria-label on references to internal definitions
      aria-labelledby on definitions pointing to their associated dfn
      elements
      Added tests
    
      (closes #304)

--- tests/spec/core/structure-spec.js
+++ tests/spec/core/structure-spec.js
@@ -3,7 +3,7 @@ describe("Core - Structure", function () {
     ,   basicConfig = {
             editors:    [{ name: "Robin Berjon" }]
         ,   specStatus: "WD"
-//        ,   doRDFa:  false
+        ,   doRDFa:  false
         }
     ,   body = "<section class='introductory'><h2>INTRO</h2></section>" +
               "<section><h2>ONE</h2><section><h2>TWO</h2><section><h2>THREE</h2><section><h2>FOUR</h2>" +
@@ -20,8 +20,7 @@ describe("Core - Structure", function () {
         waitsFor(function () { return doc; }, MAXOUT);
         // test default values
         runs(function () {
-            var $toc = $("#toc", doc)
-            ;
+            var $toc = $("#toc", doc);
             expect($toc.find("h2").text()).toEqual("Table of Contents");
             expect($toc.find("h2").attr('role')).toEqual('heading');
             expect($toc.find("h2").attr('aria-level')).toEqual('1');
@@ -32,6 +31,10 @@ describe("Core - Structure", function () {
             expect($toc.find("a[href='#six']").text()).toEqual("1.1.1.1.1.1 SIX");
             expect($toc.find("> ul > li").first().next().find("> a").text()).toEqual("A. ONE");
             expect($toc.find("a[href='#six-1']").text()).toEqual("A.1.1.1.1.1 SIX");
+            expect($toc.attr("role")).toEqual("navigation");
+            expect($toc.find("ul:first").attr("id")).toEqual("respecContents");
+            expect($toc.find("ul:first").attr("role")).toEqual("directory");
+            expect($toc.find("h2").text()).toEqual("Table of Contents");
             flushIframes();
         });
     });
@@ -96,5 +99,22 @@ describe("Core - Structure", function () {
             delete basicConfig.maxTocLevel;
         });
     });
+
+    it("should enclose content sections with appropriate roles", function() {
+        var doc;
+        runs(function () {
+            makeRSDoc({ config: basicConfig, body: body }, function (rsdoc) { doc = rsdoc; });
+        });
+        waitsFor(function () { return doc; }, MAXOUT);
+        runs(function () {
+            var $main = $("#respecMain", doc)
+            ,   $app  = $("#respecAppendices", doc)
+            ;
+            expect($main.attr('role')).toEqual("main");
+            expect($app.attr('role')).toEqual("complementary");
+            flushIframes();
+        });
+    });
+        
 });
 
