bfaedca3d27a82310709299b828c2cf403595af1
Dominique Hazael-Massieux <dom@w3.org>
2014-11-04 16:04:27 +0100

    report errors/warnings from cmd line tool

--- tools/respec2html.js
+++ tools/respec2html.js
@@ -5,22 +5,55 @@
 // Depends on PhantomJS <http://phantomjs.org>.
 
 var page = require("webpage").create()
-,   args = require("system").args
+,   args = require("system").args.slice()
 ,   fs = require("fs")
-,   source = args[1]
-,   output = args[2]
-,   timeout = isNaN(args[3]) ? 10: parseInt(args[3], 10)
 ,   timer
+,   reportErrors = false
+,   reportWarnings = false
+,   errors = []
+,   warnings = []
 ;
 
+// report console.error on stderr
+console.error = function () {
+    require("system").stderr.write(Array.prototype.join.call(arguments, ' ') + '\n');
+};
+
+var eOption = args.indexOf("-e");
+if (eOption !== -1) {
+    args.splice(args.indexOf("-e"), 1);
+    reportErrors = true;
+}
+
+if (args.indexOf("-w") !== -1) {
+    args.splice(args.indexOf("-w"), 1);
+    reportWarnings = true;
+}
+
+// Reading other parameters
+var source = args[1]
+,   output = args[2]
+,   timeout = isNaN(args[3]) ? 10: parseInt(args[3], 10);
+
+
 if (args.length < 2 || args.length > 4) {
-    var usage = "Usage:\n   phantomjs --ssl-protocol=any respec2html.js respec-source [html-output] [timeout]\n" +
+    var usage = "Usage:\n   phantomjs --ssl-protocol=any respec2html.js [-e] [-w] respec-source [html-output] [timeout]\n" +
                 "   respec-source  ReSpec source file, or an URL to the file" +
+                "   [-e]           Report ReSpec errors on stderr" +
+                "   [-w]           Report ReSpec warnings on stderr" +
                 "   [html-output]  Name for the HTML file to be generated, defaults to stdout" +
                 "   [timeout]      An optional timeout in seconds, default is 10\n";
     console.error(usage);
     phantom.exit(1);
 }
+
+page.onConsoleMessage = function (msg) {
+    if (msg.match(/^ERROR: /)) {
+        errors.push(msg);
+    } else if (msg.match(/^WARN: /)) {
+        warnings.push(msg);
+    }
+};
 
 page.open(source, function (status) {
     if (status !== "success") {
@@ -28,7 +61,7 @@ page.open(source, function (status) {
         phantom.exit(1);
     }
     else {
-        if (output) console.error("Loading " + source);
+        console.error("Loading " + source);
         page.evaluateAsync(function () {
             function saveToPhantom () {
                 require(["core/ui", "ui/save-html"], function (ui, saver) {
@@ -48,7 +81,7 @@ page.open(source, function (status) {
                 phantom.exit(1);
             }
             else {
-                if (output) console.error("Timing out in " + --timeout + "s...");
+                console.error("Timing out in " + --timeout + "s...");
             }
         }, 1000);
     }
@@ -56,8 +89,17 @@ page.open(source, function (status) {
 
 page.onCallback = function (data) {
     clearInterval(timer);
+    var exit = 0;
+    if (warnings.length && reportWarnings) {
+        console.error(warnings.join("\n"));
+        exit = 65;
+    }
+    if (errors.length && reportErrors) {
+        console.error(errors.join("\n"));
+        exit = 64;
+    }
     if (output) fs.write(output, data.html, "w");
-    else        console.log(data.html);
+    else console.log(data.html);
     if (output) console.error(output + " created!");
-    phantom.exit(0);
+    phantom.exit(exit);
 };
