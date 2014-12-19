44c0eb41f5b80f0c0d69dfbbdbb4e534775a6a95
Tobie <tobie.langel@gmail.com>
2014-12-19 12:56:37 +0100

    Use SSL for Specref DB search API request.
    Avoids triggering mixed-content warnings.

--- js/ui/search-specref.js
+++ js/ui/search-specref.js
@@ -50,8 +50,8 @@ define(
                     $status.html("Searching…");
                     var query = $search.val();
                     $.when(
-                        $.getJSON("http://specref.jit.su/search-refs", { q: query }),
-                        $.getJSON("http://specref.jit.su/reverse-lookup", { urls: query })
+                        $.getJSON("https://specref.jit.su/search-refs", { q: query }),
+                        $.getJSON("https://specref.jit.su/reverse-lookup", { urls: query })
                     ).done(function(search, revLookup) {
                         var ref;
                         search = search[0],
