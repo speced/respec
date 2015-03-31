74bf211ff0f549faacc9d8bb845e2700e576e7d4
Gregg Kellogg <gregg@kellogg-assoc.com>
2015-03-30 17:31:49 -0700

    Link definition to location in document and close modal dialog on click.

--- js/ui/dfn-list.js
+++ js/ui/dfn-list.js
@@ -1,16 +1,21 @@
 
-// Module ui/about-respec
-// A simple about dialogue with pointer to the help
-
+// Module ui/dfn-list
+// Displays all definitions with links to the defining element.
 define(
     ["jquery"],
     function ($) {
         return {
             show:   function (ui, _conf) {
-                var $halp = $("<dl></dl>");
+                var $halp = $("<ul></ul>");
                 Object.keys(_conf.definitionMap).sort().forEach(function(title) {
                     _conf.definitionMap[title].forEach(function(dfn) {
-                      $("<li>" + title + "</li>").appendTo($halp);
+                      // Link to definition
+                      var $link = $("<a>" + title + "</a>")
+                        .attr("href", "#" + dfn.attr("id"))
+                        .click(function () {
+                          ui.closeModal();
+                      });
+                      ($("<li></li>").append($link)).appendTo($halp);
                     });
                 });
 
