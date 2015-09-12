5de72816d3071cf5cdefbae7c9277854fb3f984c
Marcos Caceres <marcos@marcosc.com>
2015-09-12 17:26:51 -0400

    fixed tests to use data-*

--- tests/spec/core/webidl-contiguous-spec.js
+++ tests/spec/core/webidl-contiguous-spec.js
@@ -481,17 +481,18 @@ describe("Core - Contiguous WebIDL", function () {
     it("should link documentation", function() {
         $section = $("#documentation", doc);
         $target = $("#doc-iface", doc);
+
         expect($target.find(".idlAttrName:contains('docString') a").attr("href")).toEqual("#dom-documented-docstring");
         expect($section.find("dfn:contains('docString')").attr("id")).toEqual("dom-documented-docstring");
 
         expect($section.find("dfn:contains('Some generic term')").attr("id")).toEqual("dfn-some-generic-term");
         expect($section.find("a:contains('Some generic term')").attr("href")).toEqual("#dfn-some-generic-term");
-        expect($section.find("p[link-for] a:contains('docString')").attr("href")).toEqual("#dom-documented-docstring");
+        expect($section.find("p[data-link-for] a:contains('docString')").attr("href")).toEqual("#dom-documented-docstring");
         var notDefinedAttr = $target.find(".idlAttribute:contains('notDefined')");
         expect(notDefinedAttr.find(".idlAttrName").length).toEqual(1);
         expect(notDefinedAttr.find(".idlAttrName").find('a').length).toEqual(0);
         expect(notDefinedAttr.attr("id")).toEqual("idl-def-documented-notdefined");
-        expect($section.find("p[link-for] a:contains('notDefined')").attr("href")).toEqual("#idl-def-documented-notdefined");
+        expect($section.find("p[data-link-for] a:contains('notDefined')").attr("href")).toEqual("#idl-def-documented-notdefined");
 
         var definedElsewhere = $section.find("dfn:contains('definedElsewhere')");
         var linkFromElsewhere = $section.find("a:contains('Documented.docString')");
