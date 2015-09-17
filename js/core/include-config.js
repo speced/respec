a431599effc12e156cd1759fe879f9f6058e6d69
Marcos Caceres <marcos@marcosc.com>
2015-09-16 21:11:45 -0400

    fixed race condition using pub/sub

--- js/core/include-config.js
+++ js/core/include-config.js
@@ -3,28 +3,45 @@
 
 define(
   [],
-  function() {
+  function () {
+    'use strict';
     return {
-      run: function(conf, doc, cb, msg) {
-        msg.pub("start", "core/include-config");
-        var script = doc.createElement("script");
-        script.id = "initialUserConfig";
-        var confFilter = function(key, val) {
-          // DefinitionMap contains array of DOM elements that aren't serializable
-          // we replace them by their id
-          if (key === "definitionMap") {
-            var ret = {};
-            Object.keys(val).forEach(function(k) {
-              ret[k] = val[k].map(function(d) { return d[0].id});
-            });
-            return ret;
+      run: function (conf, doc, cb, msg) {
+        msg.pub('start', 'core/include-config');
+        var initialUserConfig;
+        try {
+          if (Object.assign) {
+            initialUserConfig = Object.assign({}, conf);
+          } else {
+            initialUserConfig = JSON.parse(JSON.stringify(conf));
           }
-          return val;
+        } catch (err) {
+          initialUserConfig = {};
         }
-        script.innerHTML = JSON.stringify(conf.initialUserConfig, confFilter, 2);
-        script.type = "application/json";
-        doc.head.appendChild(script);
-        msg.pub("end", "core/include-config");
+        msg.sub('end-all', function () {
+          var script = doc.createElement('script');
+          script.id = 'initialUserConfig';
+          var confFilter = function (key, val) {
+            // DefinitionMap contains array of DOM elements that aren't serializable
+            // we replace them by their id
+            if (key === 'definitionMap') {
+              var ret = {};
+              Object
+                .keys(val)
+                .forEach(function (k) {
+                  ret[k] = val[k].map(function (d) {
+                    return d[0].id;
+                  });
+                });
+              return ret;
+            }
+            return val;
+          };
+          script.innerHTML = JSON.stringify(initialUserConfig, confFilter, 2);
+          script.type = 'application/json';
+          doc.head.appendChild(script);
+        });
+        msg.pub('end', 'core/include-config');
         cb();
       }
     };
