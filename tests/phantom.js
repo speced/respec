20825d2376841d3d87f2e27b92af6613fedc400e
Marcos Caceres <marcos@marcosc.com>
2016-02-16 15:24:43 +1100

    Clean up phantom.js code

--- tests/phantom.js
+++ tests/phantom.js
@@ -1,21 +1,22 @@
-var page = require("webpage").create()
-,   env = require("system").env
-,   args = require("system").args
-;
+/*jshint strict: true, phantom:true*/
 
-page.open("http://localhost:3000/tests/phantom-runner.html?filter=" + (args.slice(1).join("%20")), function () {});
+"use strict";
+var page = require("webpage").create();
+var env = require("system").env;
+var args = require("system").args;
+var url = "http://localhost:3000/tests/phantom-runner.html?filter=" + (args.slice(1).join("%20"));
 
-page.onConsoleMessage = function (msg) {
-    if (msg === "JASMINE: ConsoleReporter finished") {
-        var console_reporter = page.evaluate(function () {
-            return console_reporter;
-        });
-        phantom.exit(console_reporter.status);
-    }
-    else if (msg.indexOf("JASMINE: ") === 0) {
-        console.log(msg.replace("JASMINE: ", ""));
-    }
-    else if (env.TRACE) {
-        console.log(msg);
-    }
+page.open(url, function() {});
+
+page.onConsoleMessage = function(msg) {
+  if (msg === "JASMINE: ConsoleReporter finished") {
+    var console_reporter = page.evaluate(function() {
+      return console_reporter;
+    });
+    phantom.exit(console_reporter.status);
+  } else if (msg.indexOf("JASMINE: ") === 0) {
+    console.log(msg.replace("JASMINE: ", ""));
+  } else if (env.TRACE) {
+    console.log(msg);
+  }
 };
