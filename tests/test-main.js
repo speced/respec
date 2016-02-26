2bf3bb3f87b61802441981c2dcc46550448d85e1
Marcos Caceres <marcos@marcosc.com>
2016-02-26 16:52:39 +1100

    Cleanup of tests/test-main.js

--- tests/test-main.js
+++ tests/test-main.js
@@ -1,38 +1,36 @@
+"use strict";
 var TEST_REGEXP = /(spec|test)\.js$/i;
-var allTestFiles = [];
 // Get a list of all the test files to include
-Object.keys(window.__karma__.files).forEach(function(file) {
-  if (TEST_REGEXP.test(file)) {
+var allTestFiles = Object.keys(window.__karma__.files)
+  .filter(function(file) {
+    return TEST_REGEXP.test(file);
+  })
+  .map(function(file) {
     // Normalize paths to RequireJS module names.
     // If you require sub-dependencies of test files to be loaded as-is (requiring file extension)
     // then do not normalize the paths
-    var normalizedTestModule = file.replace(/^\/base\/|\.js$/g, '');
-    allTestFiles.push(normalizedTestModule);
-  }
-});
+    return file.replace(/^\/base\/|\.js$/g, "");
+  })
+  .reduce(function(collector, nextFile) {
+    collector.push(nextFile);
+    return collector;
+  }, []);
 
-var requireConfig = {
+require.config({
   // Karma serves files under /base, which is the basePath from your config file
-  baseUrl: '/base/',
-
-  // example of using a couple path translations (paths), to allow us to refer to different library dependencies, without using relative paths
+  baseUrl: "/base/",
   paths: {
-    'js/jquery': 'jquery',
-    'core/utils': 'js/core/utils',
-    'ui': (window.respecVersion) ? "https://w3c.github.io/respec/js/ui" : "",
+    "js/jquery": "jquery",
+    "core/utils": "js/core/utils",
+    "ui": (window.respecVersion) ? "https://w3c.github.io/respec/js/ui" : "",
   },
-
   shim: {
     shortcut: {
       exports: "shortcut"
     }
   },
-
   // dynamically load all test files
   deps: allTestFiles,
-
   // we have to kickoff jasmine, as it is asynchronous
   callback: window.__karma__.start,
-}
-
-require.config(requireConfig);
+});
