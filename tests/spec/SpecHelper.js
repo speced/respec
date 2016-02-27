bdeaf3aec2c30dde4becab2fea6dfbcb9c2bc376
Marcos Caceres <marcos@marcosc.com>
2016-02-28 03:24:09 +1100

    Style fixes

--- tests/spec/SpecHelper.js
+++ tests/spec/SpecHelper.js
@@ -128,12 +128,12 @@ Polyfill for Object.assign() from:
 https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
 */
 /* jshint ignore:start */
-if (typeof Object.assign != 'function') {
+if (typeof Object.assign !== "function") {
   (function() {
     Object.assign = function(target) {
-      'use strict';
+      "use strict";
       if (target === undefined || target === null) {
-        throw new TypeError('Cannot convert undefined or null to object');
+        throw new TypeError("Cannot convert undefined or null to object");
       }
 
       var output = Object(target);
