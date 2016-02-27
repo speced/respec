0d0f1a2f21102bbac26c9f1a3370a31fc334e67a
Marcos Caceres <marcos@marcosc.com>
2016-02-28 02:27:15 +1100

    Fixup

--- karma.conf.js
+++ karma.conf.js
@@ -121,6 +121,6 @@ module.exports = function(config) {
     },
   });
   if(process.env.TRAVIS){
-    configuration.browsers = ['Chrome_travis_ci'];
+    conf.browsers = ['Chrome_travis_ci'];
   }
 };
