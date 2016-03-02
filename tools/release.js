b173372852d5cc15431d7183b1a4ae0e5c8acf95
Marcos Caceres <marcos@marcosc.com>
2016-03-02 20:25:10 +1100

    Rewrite release.js to work async

--- tools/release.js
+++ tools/release.js
@@ -1,203 +1,215 @@
 #!/usr/local/bin/node
 
 "use strict";
-var cmdPrompt = require("prompt");
-var async = require("async");
-var fs = require("fs-promise");
-var pth = require("path");
-var bwc = require("./build-w3c-common");
-var Builder = require("./builder").Builder;
-var exec = require("child_process").exec;
+const cmdPrompt = require("prompt");
+const async = require("marcosc-async");
+const fsp = require("fs-promise");
+const path = require("path");
+const w3cBuild = require("./build-w3c-common");
+const Builder = require("./builder").Builder;
+const exec = require("child_process").exec;
+const colors = require("colors");
+const MAIN_BRANCH = "develop";
+let DEBUG = false;
 
+colors.setTheme({
+  data: "grey",
+  debug: "cyan",
+  error: "red",
+  help: "cyan",
+  info: "green",
+  important: "red",
+  input: "grey",
+  prompt: "grey",
+  verbose: "cyan",
+  warn: "yellow",
+});
 function rel(f) {
-  return pth.join(__dirname, f);
-};
+  return path.join(__dirname, f);
+}
+
+function git(cmd) {
+  if(DEBUG){
+    console.log(colors.debug(`Pretending to run: ${"git " + colors.prompt(cmd)}`));
+    return Promise.resolve();
+  }
+  return toExecPromise(`git ${cmd}`);
+}
 
 cmdPrompt.start();
 
-const Tasks = {
-    // 1. Make sure you are up to date and on the develop branch (git up; git checkout develop)
-    upToDateAndDev: async(function * () {
+const Promps = {
+  askQuestion(promptOps) {
+    return new Promise((resolve, reject) => {
+      cmdPrompt.get(promptOps, (err, res) => {
+        if (err) {
+          return reject(new Error(err));
+        }
+        if (res.question.toLowerCase() === "n") {
+          return reject(new Error("🙅  user declined."));
+        }
+        resolve(res.question);
+      });
+    });
+  },
+
+  askSwitchToBranch(from, to) {
+    return async.task(function * () {
       const promptOps = {
-        description: "Are you up to date and on branch develop",
+        description: `You're on branch ${colors.info(from)}. Switch to ${colors.info(to)}?`,
         pattern: /^[yn]$/i,
         message: "Values can be 'y' or 'n'.",
         default: "y",
       };
-      yield new Promise((resolve, reject) => {
-        cmdPrompt.get(promptOps, (err, res) => {
-          if (err) {
-            return reject(new Error(err));
-          }
-          if (res.question.toLowerCase() === "n") {
-            const error = new Error("Make sure to run git up; git checkout develop");
-            return reject(error);
-          }
-          resolve();
-        });
-      })
-    }),
+      return yield this.askQuestion(promptOps);
+    }, this);
+  },
 
-    // 2. Bump the version in `package.json`.
-    bumpVersion: async(function * () {
-      const fileOps = {
-        encoding: "utf8"
+  askUpToDateAndDev() {
+    return async.task(function * () {
+      const promptOps = {
+        description: "Are you up to date?",
+        pattern: /^[yn]$/i,
+        message: "Values can be 'y' or 'n'.",
+        default: "y",
       };
+      try {
+        yield this.askQuestion(promptOps);
+      } catch (err) {
+        const warning = colors.warn("Make sure to run `git up; git checkout develop`");
+        console.warn(warning);
+        throw err;
+      }
+    }, this);
+  },
+
+  askBumpVersion() {
+    return async.task(function * () {
       const version = yield Builder.getRespecVersion();
       if (!version) {
         throw new Error("Version string not found in package.json");
       }
-      const newVersion = version.split(".");
-      newVersion[2]++;
-      newVersion = newVersion.join(".");
+      const newVersion = version.split(".")
+        .map((value, index) => (index === 2) ? parseInt(value) + 1 : value)
+        .join(".");
       const packagePath = rel("../package.json");
-      const data = yield fsp.readFile(packagePath, fileOps);
+      const data = yield fsp.readFile(packagePath, "utf8");
       const pack = JSON.parse(data);
-      pack.version = yield new Promise((resolve, reject) => {
-        const promptOps = {
-          description: `Current version is ${version}, bump it to`,
-          pattern: /^\d+\.\d+\.\d+$/i,
-          message: "Values must be x.y.z",
-          default: newVersion
-        };
-        cmdPrompt.get(promptOps, (err, res) => {
-          if (err) {
-            return reject(new Error(err));
-          }
-          resolve(targetVersion);
-        });
-      });
-      yield fs.writeFile(packagePath, JSON.stringify(pack, null, 2), fileOps);
+      const promptOps = {
+        description: `Current version is ${version}, bump it to`,
+        pattern: /^\d+\.\d+\.\d+$/i,
+        message: "Values must be x.y.z",
+        default: newVersion
+      };
+      pack.version = yield this.askQuestion(promptOps);
+      yield fsp.writeFile(packagePath, JSON.stringify(pack, null, 2) + "\n", "utf8");
       return pack.version;
-    }),
+    }, this);
+  },
 
-    // 3. Run the build script (node tools/build-w3c-common.js). This should respond "OK!" (if not, fix the
-    //    issue).
-    // 4. Add the new build (git add builds/respec-w3c-common-3.x.y.js).
-    // 5. Commit your changes (git commit -am v3.x.y)
-    // 6. Merge to gh-pages (git checkout gh-pages; git merge develop)
-    // 7. Tag the release (git tag v3.x.y) and be sure that git is pushing tags.
-    buildAddCommitMergeTag: async(function * () {
-        const promptOps = {
-          description: "Are you ready to build, add, commit, merge, and tag",
-          pattern: /^[yn]$/i,
-          message: "Values can be 'y' or 'n'.",
-          default: "y",
-        };
-        yield new Promise(resove, reject) => {
-          cmdPrompt.get(promptOps, (err, res) => {
-            var val = res.question.toLowerCase();
-            if (err) {
-              return reject(new Error(err));
-            }
-            if (val === "n") {
-              return reject(new Error("User not ready."));
-            }
-            resolve();
-          });
-        });
-    }),
+  askBuildAddCommitMergeTag() {
+    return async.task(function * () {
+      const promptOps = {
+        description: "Are you ready to build, add, commit, merge, and tag",
+        pattern: /^[yn]$/i,
+        message: "Values can be 'y' or 'n'.",
+        default: "y",
+      };
+      return yield this.askQuestion(promptOps);
+    }, this);
+  },
 
-  build: async(function * (targetVersion) {
-    yield bwc.buildW3C(targetVersion);
-  }),
-
-  add: async(function * (targetVersion) {
-    let path = rel("../builds/respec-w3c-common-" + targetVersion + ".js");
-    path += rel(" ../builds/respec-w3c-common-" + targetVersion + ".js.map");
-    yield toExecPromise("git add " + path);
-  }),
-
-  commit: async(function * (targetVersion) {
-    yield toExecPromise("git commit -am v" + targetVersion);
-  }),
-
-  checkoutGHPages: async(function * () {
-    yield toExecPromise("git checkout gh-pages");
-  }),
-
-  merge: async(function * () {
-    yield toExecPromise("git merge develop");
-  }),
-
-  checkoutDevelop: async(function * () {
-    yield toExecPromise("git checkout develop");
-  }),
-
-  tag: async(function * (targetVersion) {
-    var version = "v" + targetVersion;
-    yield toExecPromise(`git tag -m  ${version} ${version}`);
-  }),
-
-  // 8. Push everything back to the server (make sure you are pushing at least the `develop` and
-  //    `gh-pages` branches).
-  pushAll: async(function * () {
-      cmdPrompt.get(
-
-        {
-          description: "Are you ready to push everything? This is your last chance",
-          pattern: /^[yn]$/i,
-          message: "Values can be 'y' or 'n'.",
-          default: "y"
-        },
-        function(err, res) {
-          var val = res.question.toLowerCase();
-          if (err) {
-            return cb(err);
-          }
-          if (val === "n") {
-            return cb("User not ready! ABORT, ABORT!");
-          }
-          cb();
-        }
-      );
-    },
-
-    pushCommits: async(function * () {
-      exec("git push --all", cb);
-    }),
-
-    pushTags: async(function * () {
-      exec("git push --tags", cb);
-    }),
-
-    publishToNPM: async(function * () {
-      exec("npm publish", cb);
-    }),
-
-    toExecPromise(cmd) {
-      return new Promise((resolve, reject) => {
-        const id = setTimeout(() => {
-          reject(new Error(`Command took too long: ${cmd}`));
-          proc.kill("SIGTERM");
-        }, 20000);
-        const proc = exec(cmd, (err, stdout) => {
-          clearTimeout(id);
-          if (err) {
-            return reject(err);
-          }
-          resolve(stdout);
-        });
-      });
-    },
-
+  askPushAll() {
+    return async.task(function * () {
+      const promptOps = {
+        description: `${colors.important("🔥 Ready to make this live? 🔥")}  (last chance!)`,
+        pattern: /^[yn]$/i,
+        message: "Values can be 'y' or 'n'.",
+        default: "y",
+      };
+      return yield this.askQuestion(promptOps);
+    }, this);
   }
+};
 
+function toExecPromise(cmd) {
+  return new Promise((resolve, reject) => {
+    const id = setTimeout(() => {
+      reject(new Error(`Command took too long: ${cmd}`));
+      proc.kill("SIGTERM");
+    }, 20000);
+    const proc = exec(cmd, (err, stdout) => {
+      clearTimeout(id);
+      if (err) {
+        return reject(err);
+      }
+      resolve(stdout);
+    });
+  });
+}
+
+function getBranchState() {
+  return async.task(function * () {
+    const local = yield git(`rev-parse @`);
+    const remote = yield git(`rev-parse @{u}`);
+    const base = yield git(`merge-base @ @{u}`);
+    let result = "";
+    switch(local){
+      case remote:
+        result = "up-to-date";
+        break;
+      case base:
+        result = "needs a pull";
+        break;
+      default:
+        result = (remote === base) ? "needs to push" : "has diverged";
+    }
+    return result;
+  });
+}
 
 async.task(function * () {
-  yield upToDateAndDev();
-  const version = yield bumpVersion();
-  yield buildAddCommitMergeTag();
-  yield build();
-  yield add();
-  yield commit();
-  yield checkoutGHPages();
-  yield merge();
-  yield checkoutDevelop();
-  yield tag();
-  yield pushAll();
-  yield pushCommits();
-  yield pushTags();
-  yield publishToNPM();
-  console.log("OK!");
-});
+  try {
+    // 1. Confirm maintainer is on up-to-date and on the develop branch ()
+    console.log(colors.info(" 📡  Performing Git remote update..."));
+    //yield git(`remote update`);
+    const currentBranch = (yield git(`rev-parse --abbrev-ref HEAD`)).trim();
+    if (currentBranch !== MAIN_BRANCH) {
+      yield Promps.askSwitchToBranch(currentBranch, MAIN_BRANCH);
+    }
+    const branchState = yield getBranchState();
+    if (branchState !== "up-to-date") {
+      throw new Error("Your branch is not up-to-date. It ${branchState}.");
+    }
+    DEBUG = true;
+    // 2. Bump the version in `package.json`.
+    const version = yield Promps.askBumpVersion();
+    const file = rel(`../builds/respec-w3c-common-${version}.js`);
+    //yield Promps.askBuildAddCommitMergeTag();
+    // 3. Run the build script (node tools/build-w3c-common.js).
+    yield w3cBuild.buildW3C("latest", version);
+    // 4. Add the new build (git add builds/respec-w3c-common-3.x.y.js).
+    yield git(`add ${file} ${file}.map`);
+    // 5. Commit your changes (git commit -am v3.x.y)
+    yield git(`commit -am v${version}`);
+    // 6. Merge to gh-pages (git checkout gh-pages; git merge develop)
+    yield git(`checkout gh-pages`);
+    yield git(`merge develop`);
+    yield git(`checkout develop`);
+    // 7. Tag the release (git tag v3.x.y)
+    yield git(`tag -m v${version} v${version}`);
+    yield Promps.askPushAll();
+    console.log(colors.info(" 📡  Pushing everything back to server..."));
+    yield git("push develop");
+    yield git("push gh-pages");
+    yield git("push --tags");
+    console.log(colors.info(" 📡  Publishing to npm..."));
+    yield toExecPromise("npm publish");
+  } catch (err) {
+    process.exit(1);
+  }
+}).then(
+  () => process.exit(0)
+).catch(
+  err => console.error(err)
+);
