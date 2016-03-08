e34d629c6a603a145a2bad19768974e47b44ffea
Marcos Caceres <marcos@marcosc.com>
2016-03-08 17:21:24 +1100

    Fix (best-practices-spec.js): Unnecessary use of jQuery

--- tests/test-main.js
+++ tests/test-main.js
@@ -20,17 +20,9 @@ var allTestFiles = Object.keys(window.__karma__.files)
 require.config({
   // Karma serves files under /base, which is the basePath from your config file
   baseUrl: "/base/",
-  paths: {
-    "jquery": "/node_modules/jquery/dist/jquery.slim.js",
-    "ui": (window.respecVersion) ? "https://w3c.github.io/respec/js/ui" : "",
-  },
-  shim: {
-    shortcut: {
-      exports: "shortcut"
-    }
-  },
   // dynamically load all test files
   deps: allTestFiles,
   // we have to kickoff jasmine, as it is asynchronous
   callback: window.__karma__.start,
 });
+window.isKarma = true;
