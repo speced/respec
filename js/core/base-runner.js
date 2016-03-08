1d0ca6d207023671280d1e2251dbe374c6668169
Marcos Caceres <marcos@marcosc.com>
2016-03-08 15:03:25 +1100

    Fix (profile-w3c-common.js): Load Promise and core/ready-spec as deps

--- js/core/base-runner.js
+++ js/core/base-runner.js
@@ -50,35 +50,6 @@
             });
         }
     };
-    var respecDone = false;
-    var doneResolver;
-    var doneRejector;
-    var respecDonePromise = new Promise(function(resolve, reject){
-        doneResolver = resolve;
-        doneRejector = reject;
-    });
-    Object.defineProperty(document, "respecDone", {
-        get: function(){
-            var warn = "document.respecDone is deprecated, use document.respectIsReady instead.";
-            console.warn(warn);
-            return respecDone;
-        },
-        set: function(value){
-            if (typeof value === "boolean" && value){
-                respecDone =  value;
-                doneResolver(respecConfig);
-            }
-            if(value instanceof Error){
-                doneRejector(value)
-            }
-            return value;
-        }
-    });
-    Object.defineProperty(document, "respectIsReady", {
-        get: function(){
-            return respecDonePromise;
-        },
-    });
 }(this));
 
 // these need to be improved, or complemented with proper UI indications
@@ -98,8 +69,8 @@ if (window.console) {
 }
 
 define(
-    ["jquery", "Promise"],
-    function ($, Promise) {
+    ["jquery"],
+    function ($) {
         return {
             runAll:    function (plugs) {
                 // publish messages for beginning of all and end of all
