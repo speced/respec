62cd352fb023b6c6650f3f007754269f1a02eb16
Marcos Caceres <marcos@marcosc.com>
2016-03-09 12:28:21 +1100

    Feat (profile-w3c-common.js): Make jquery(-enhanced), Promise, global deps

--- js/profile-w3c-common.js
+++ js/profile-w3c-common.js
@@ -12,6 +12,7 @@ require.config({
     "Promise": "/node_modules/promise-polyfill/Promise",
     "webidl2": "/node_modules/webidl2/lib/webidl2",
   },
+  deps: ["core/jquery-enhanced", "Promise"]
 });
 
 define([
