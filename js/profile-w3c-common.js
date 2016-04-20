19b56476a1900df1ea2c94eae1ab7e9b897b0e26
Marcos Cáceres <marcos+github@marcosc.com>
2016-04-21 07:54:58 +1000

    fix(profile-w3c-common): forgot to include fetch poly for Safari
    * fix(profile-w3c-common): forgot to include fetch poly for Safari
    
    * Chore(.travis.yml): use latest firefox

--- js/profile-w3c-common.js
+++ js/profile-w3c-common.js
@@ -10,12 +10,14 @@ require.config({
     },
   },
   paths: {
+    "fetch": "/node_modules/whatwg-fetch/fetch",
     "handlebars": "/node_modules/handlebars/dist/handlebars",
     "jquery": "/node_modules/jquery/dist/jquery",
     "Promise": "/node_modules/promise-polyfill/promise",
     "webidl2": "/node_modules/webidl2/lib/webidl2",
   },
   deps: [
+    "fetch",
     "jquery",
     "Promise",
     "core/jquery-enhanced",
