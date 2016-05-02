e824904f9ed0da265ea76b2addc29227e1aa48c4
Marcos Cáceres <marcos+github@marcosc.com>
2016-05-03 01:16:21 +1000

    Fix(respec-ready): s/respectIsReady/respecIsReady/ typo

--- tests/spec/core/ready-promise-spec.js
+++ tests/spec/core/ready-promise-spec.js
@@ -1,19 +1,19 @@
 "use strict";
-describe("respectIsReady promise", function() {
+describe("respecIsReady promise", function() {
   it("should settle when processing is done", function(done) {
     var ops = makeStandardOps();
     makeRSDoc(ops, function(doc) {
       var props = new Set(Object.getOwnPropertyNames(doc));
-      expect(props.has("respectIsReady")).toBeTruthy();
-      expect(doc.respectIsReady instanceof doc.defaultView.Promise).toBeTruthy();
-      doc.respectIsReady.then(done);
+      expect(props.has("respecIsReady")).toBeTruthy();
+      expect(doc.respecIsReady instanceof doc.defaultView.Promise).toBeTruthy();
+      doc.respecIsReady.then(done);
     });
   });
 
   it("should resolve with the resulting respecConfig", function(done) {
     var ops = makeStandardOps();
     makeRSDoc(ops, function(doc) {
-      doc.respectIsReady.then(function(resultingConfig) {
+      doc.respecIsReady.then(function(resultingConfig) {
         // previousPublishDate gets changed to a Date object by ReSpec,
         // so not worth checking for equality.
         delete resultingConfig.previousPublishDate;
