209a4f2bfe5158dd1c9a672d0b437743c2d251fa
Marcos Caceres <marcos@marcosc.com>
2016-05-19 17:57:33 +1000

    Fix(ui): close ui when doc is clicked (closes #772)

--- tests/spec/core/ui-spec.js
+++ tests/spec/core/ui-spec.js
@@ -19,4 +19,28 @@ describe("Core - Ui", function () {
       expect(pillContainer.classList.contains("respec-hidden")).toBe(false);
     }).then(done);
   });
+
+  it("hides the UI when document is clicked", function (done) {
+    makeRSDoc(makeStandardOps(), function (doc) {
+      var menu = doc.querySelector("#respec-menu");
+      expect(window.getComputedStyle(menu).display).toEqual("none");
+      doc.querySelector("#respec-pill").click();
+      // spin the event loop
+      new Promise(function(resolve){
+        setTimeout(function(){
+          expect(window.getComputedStyle(menu).display).toEqual("block");
+          doc.body.click();
+          resolve();
+        }, 500);
+      })
+      // Allow time to fade in
+      .then(function(){
+        setTimeout(function(){
+          expect(window.getComputedStyle(menu).display).toEqual("none");
+          done();
+        }, 500);
+      });
+      // give it time to fade out
+    }, null, "display: block");
+  });
 });
