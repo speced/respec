20df60712ef3f93122e26de61af5b11dc953260c
Marcos Caceres <marcos@marcosc.com>
2016-05-20 14:35:59 +1000

    Feat(markdown): disable md linking via 'nolinks' class (closes #777)

--- js/core/markdown.js
+++ js/core/markdown.js
@@ -235,6 +235,13 @@ define([
     return process(fragment);
   }
 
+  function substituteWithTextNodes(elements){
+    for(var element of elements){
+      var textNode = element.ownerDocument.createTextNode(element.textContent);
+      element.parentElement.replaceChild(textNode, element);
+    }
+  }
+
   var processBlockLevelElements = processElements("section, .issue, .note, .req");
 
   return {
@@ -256,6 +263,8 @@ define([
         var cleanHTML = dirtyHTML.replace(/<p>\s*<\/p>/gm, "");
         var beautifulHTML = beautify.html_beautify(cleanHTML, beautifyOps);
         newBody.innerHTML = beautifulHTML;
+        // Remove links where class pre.nolinks
+        substituteWithTextNodes(newBody.querySelectorAll("pre.nolinks a[href]"));
         // Restructure the document properly
         var fragment = structure(newBody, doc);
         // Frankenstein the whole thing back together
