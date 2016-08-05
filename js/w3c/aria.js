4780f08fb792e189cc6453010d9cec5e9c029fe6
Marcos Caceres <marcos@marcosc.com>
2016-08-05 11:28:47 +1000

    style(aria): jscs fixup

--- js/w3c/aria.js
+++ js/w3c/aria.js
@@ -3,43 +3,45 @@
 // Introduced by Shane McCarron (shane@aptest.com) from the W3C PFWG
 
 define(
-    ["core/utils"], // load this to be sure that the jQuery extensions are loaded
-    function (utils) {
-        return {
-            run:    function (conf, doc, cb) {
-                // ensure head section is labelled
-                $('body', doc).attr('role', 'document') ;
-                $('body', doc).attr('id', 'respecDocument') ;
-                $('div.head', doc).attr('role', 'contentinfo') ;
-                $('div.head', doc).attr('id', 'respecHeader') ;
-                var toc = doc.querySelector('#toc ol:first-of-type');
-                if (toc) {
-                    // ensure toc is labeled
-                    toc.setAttribute('role', 'directory') ;
-                }
-                // mark issues and notes with heading
-                var noteCount = 0 ; var issueCount = 0 ; var ednoteCount = 0;
-                $(".note-title, .ednote-title, .issue-title", doc).each(function (i, item) {
-                    var $item = $(item)
-                    ,   isIssue = $item.hasClass("issue-title")
-                    ,   isEdNote = $item.hasClass("ednote-title")
-                    ,   level = $item.parents("section").length+2 ;
+  ["core/utils"], // load this to be sure that the jQuery extensions are loaded
+  function(utils) {
+    return {
+      run: function(conf, doc, cb) {
+        // ensure head section is labelled
+        $('body', doc).attr('role', 'document');
+        $('body', doc).attr('id', 'respecDocument');
+        $('div.head', doc).attr('role', 'contentinfo');
+        $('div.head', doc).attr('id', 'respecHeader');
+        var toc = doc.querySelector('#toc ol:first-of-type');
+        if (toc) {
+          // ensure toc is labeled
+          toc.setAttribute('role', 'directory');
+        }
+        // mark issues and notes with heading
+        var noteCount = 0;
+        var issueCount = 0;
+        var ednoteCount = 0;
+        $(".note-title, .ednote-title, .issue-title", doc).each(function(i, item) {
+          var $item = $(item),
+            isIssue = $item.hasClass("issue-title"),
+            isEdNote = $item.hasClass("ednote-title"),
+            level = $item.parents("section").length + 2;
 
-                    $item.attr('aria-level', level) ;
-                    $item.attr('role', 'heading') ;
-                    if (isIssue) {
-                        issueCount++;
-                        $item.makeID('h', "issue" + issueCount) ;
-                    } else if (isEdNote) {
-                        ednoteCount++;
-                        $item.makeID('h', "ednote" + ednoteCount) ;
-                    } else {
-                        noteCount++;
-                        $item.makeID('h', "note" + noteCount) ;
-                    }
-                });
-                cb();
-            }
-        };
-    }
+          $item.attr('aria-level', level);
+          $item.attr('role', 'heading');
+          if (isIssue) {
+            issueCount++;
+            $item.makeID('h', "issue" + issueCount);
+          } else if (isEdNote) {
+            ednoteCount++;
+            $item.makeID('h', "ednote" + ednoteCount);
+          } else {
+            noteCount++;
+            $item.makeID('h', "note" + noteCount);
+          }
+        });
+        cb();
+      }
+    };
+  }
 );
