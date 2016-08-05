d9d889ec7ba5c1a0a0c9b46b1b401e540bf78f46
Marcos Caceres <marcos@marcosc.com>
2016-08-05 12:02:08 +1000

    style(aria): use double quotes

--- js/w3c/aria.js
+++ js/w3c/aria.js
@@ -8,20 +8,20 @@ define([],
       run: function(conf, doc, cb) {
         // ensure head section is labeled
         if (!doc.body.hasAttribute("role")) {
-          doc.body.setAttribute('role', 'document');
+          doc.body.setAttribute("role", "document");
         }
         if (!doc.body.hasAttribute("id")) {
-          doc.body.setAttribute('id', 'respecDocument');
+          doc.body.setAttribute("id", "respecDocument");
         }
-        var head = document.querySelector('div.head');
+        var head = document.querySelector("div.head");
         if (head) {
-          head.setAttribute('role', 'contentinfo');
-          head.setAttribute('id', 'respecHeader');
+          head.setAttribute("role", "contentinfo");
+          head.setAttribute("id", "respecHeader");
         }
-        var toc = doc.querySelector('#toc ol:first-of-type');
+        var toc = doc.querySelector("#toc ol:first-of-type");
         if (toc) {
           // ensure toc is labeled
-          toc.setAttribute('role', 'directory');
+          toc.setAttribute("role", "directory");
         }
         // mark issues and notes with heading
         var noteCount = 1;
@@ -33,14 +33,14 @@ define([],
             var isIssue = element.classList.contains("issue-title");
             var isEdNote = element.classList.contains("ednote-title");
             var level = $element.parents("section").length + 2;
-            element.setAttribute('aria-level', level);
-            element.setAttribute('role', 'heading');
+            element.setAttribute("aria-level", level);
+            element.setAttribute("role", "heading");
             if (isIssue) {
-              $element.makeID('h', "issue" + issueCount++);
+              $element.makeID("h", "issue" + issueCount++);
             } else if (isEdNote) {
-              $element.makeID('h', "ednote" + ednoteCount++);
+              $element.makeID("h", "ednote" + ednoteCount++);
             } else {
-              $element.makeID('h', "note" + noteCount++);
+              $element.makeID("h", "note" + noteCount++);
             }
           });
         cb();
