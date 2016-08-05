d42c53e7f7ce615f4b8f246c740bb456404cc2eb
Marcos Caceres <marcos@marcosc.com>
2016-08-05 12:00:54 +1000

    refactor(aria): use less jquery

--- js/w3c/aria.js
+++ js/w3c/aria.js
@@ -1,45 +1,48 @@
 // Module w3c/aria
 // Adds wai-aria landmarks and roles to entire document.
 // Introduced by Shane McCarron (shane@aptest.com) from the W3C PFWG
-
-define(
-  ["core/utils"], // load this to be sure that the jQuery extensions are loaded
-  function(utils) {
+"use strict";
+define([],
+  function() {
     return {
       run: function(conf, doc, cb) {
-        // ensure head section is labelled
-        $('body', doc).attr('role', 'document');
-        $('body', doc).attr('id', 'respecDocument');
-        $('div.head', doc).attr('role', 'contentinfo');
-        $('div.head', doc).attr('id', 'respecHeader');
+        // ensure head section is labeled
+        if (!doc.body.hasAttribute("role")) {
+          doc.body.setAttribute('role', 'document');
+        }
+        if (!doc.body.hasAttribute("id")) {
+          doc.body.setAttribute('id', 'respecDocument');
+        }
+        var head = document.querySelector('div.head');
+        if (head) {
+          head.setAttribute('role', 'contentinfo');
+          head.setAttribute('id', 'respecHeader');
+        }
         var toc = doc.querySelector('#toc ol:first-of-type');
         if (toc) {
           // ensure toc is labeled
           toc.setAttribute('role', 'directory');
         }
         // mark issues and notes with heading
-        var noteCount = 0;
-        var issueCount = 0;
-        var ednoteCount = 0;
-        $(".note-title, .ednote-title, .issue-title", doc).each(function(i, item) {
-          var $item = $(item),
-            isIssue = $item.hasClass("issue-title"),
-            isEdNote = $item.hasClass("ednote-title"),
-            level = $item.parents("section").length + 2;
-
-          $item.attr('aria-level', level);
-          $item.attr('role', 'heading');
-          if (isIssue) {
-            issueCount++;
-            $item.makeID('h', "issue" + issueCount);
-          } else if (isEdNote) {
-            ednoteCount++;
-            $item.makeID('h', "ednote" + ednoteCount);
-          } else {
-            noteCount++;
-            $item.makeID('h', "note" + noteCount);
-          }
-        });
+        var noteCount = 1;
+        var issueCount = 1;
+        var ednoteCount = 1;
+        doc.querySelectorAll(".note-title, .ednote-title, .issue-title")
+          .forEach(function(element) {
+            var $element = $(element);
+            var isIssue = element.classList.contains("issue-title");
+            var isEdNote = element.classList.contains("ednote-title");
+            var level = $element.parents("section").length + 2;
+            element.setAttribute('aria-level', level);
+            element.setAttribute('role', 'heading');
+            if (isIssue) {
+              $element.makeID('h', "issue" + issueCount++);
+            } else if (isEdNote) {
+              $element.makeID('h', "ednote" + ednoteCount++);
+            } else {
+              $element.makeID('h', "note" + noteCount++);
+            }
+          });
         cb();
       }
     };
