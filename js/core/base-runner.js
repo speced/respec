56fcba03a6db3ee8a2053021e15faa8031c74c6e
Marcos Caceres <marcos@marcosc.com>
2016-09-22 14:55:17 +1000

    style(base-runner): use 2 spaces

--- js/core/base-runner.js
+++ js/core/base-runner.js
@@ -17,78 +17,75 @@
 //      are all considered deprecated and may all be removed.
 "use strict";
 define(
-    ["core/pubsubhub"],
-    function (pubsubhub) {
-        function createLegacyWrapper(pubsubhub){
-            var deprecatedMsg = "Using 'msg' is deprecated. " +
-                "Import 'pubsubhub' into your module instead."
-            return Object
-                .keys(pubsubhub)
-                .reduce(function(obj, key){
-                    return Object.defineProperty(obj, key, {
-                        value: function(){
-                            console.warn(deprecatedMsg);
-                            return pubsubhub[key].apply(pubsubhub, Array.from(arguments));
-                        }
-                    });
-                }, {});
-        }
-        return {
-            runAll:    function (plugs) {
-                // publish messages for beginning of all and end of all
-                pubsubhub.pub("start-all");
-                if (respecConfig.preProcess) {
-                    for (var i = 0; i < respecConfig.preProcess.length; i++) {
-                        try {
-                            respecConfig.preProcess[i].apply(this); }
-                        catch (e) {
-                            window.console.error(e.stack || JSON.stringify(e));
-                        }
-                    }
-                }
-                var deprecated = createLegacyWrapper(pubsubhub);
-                var pipeline = Promise.resolve();
-                // the first in the plugs is going to be us
-                plugs.shift();
-                plugs.forEach(function(plug) {
-                    pipeline = pipeline.then(function () {
-                        if (plug.run) {
-                            return new Promise(function runPlugin(resolve) {
-                                // We send pubsubhub in to retain backwards
-                                var result = plug.run.call(plug, respecConfig, document, resolve, deprecated);
-                                // If the plugin returns a promise, have that
-                                // control the end of the plugin's run.
-                                // Otherwise, assume it'll call resolve() as a
-                                // completion callback.
-                                if (result) {
-                                    resolve(result);
-                                }
-                            }).catch(function(e) {
-                                console.error(e, e.stack);
-                                throw e;
-                            });
-                        }
-                        else return Promise.resolve();
-                    });
-                });
-                return pipeline
-                    .then(function(){
-                        var resultingConfig = Object.assign({}, window.respecConfig);
-                        pubsubhub.pub("end-all", resultingConfig);
-                    })
-                    .then(function() {
-                        if (respecConfig.postProcess) {
-                            for (var i = 0; i < respecConfig.postProcess.length; i++) {
-                                try { respecConfig.postProcess[i].apply(this); }
-                                catch (e) { pubsubhub.pub("error", e); }
-                            }
-                        }
-                        if (respecConfig.afterEnd) {
-                            try { respecConfig.afterEnd.apply(window, Array.from(arguments)); }
-                            catch (e) { pubsubhub.pub("error", e); }
-                        }
-                    });
+  ["core/pubsubhub"],
+  function(pubsubhub) {
+    function createLegacyWrapper(pubsubhub) {
+      var deprecatedMsg = "Using 'msg' is deprecated. " +
+        "Import 'pubsubhub' into your module instead.";
+      return Object
+        .keys(pubsubhub)
+        .reduce(function(obj, key) {
+          return Object.defineProperty(obj, key, {
+            value: function() {
+              console.warn(deprecatedMsg);
+              return pubsubhub[key].apply(pubsubhub, Array.from(arguments));
             }
-        };
+          });
+        }, {});
     }
+    return {
+      runAll: function(plugs) {
+        // publish messages for beginning of all and end of all
+        pubsubhub.pub("start-all");
+        if (respecConfig.preProcess) {
+          for (var i = 0; i < respecConfig.preProcess.length; i++) {
+            try {
+              respecConfig.preProcess[i].apply(this);
+            } catch (e) {
+              window.console.error(e.stack || JSON.stringify(e));
+            }
+          }
+        }
+        var deprecated = createLegacyWrapper(pubsubhub);
+        var pipeline = Promise.resolve();
+        // the first in the plugs is going to be us
+        plugs.shift();
+        plugs.forEach(function(plug) {
+          pipeline = pipeline.then(function() {
+            if (plug.run) {
+              return new Promise(function runPlugin(resolve) {
+                // We send pubsubhub in to retain backwards
+                var result = plug.run.call(plug, respecConfig, document, resolve, deprecated);
+                // If the plugin returns a promise, have that
+                // control the end of the plugin's run.
+                // Otherwise, assume it'll call resolve() as a
+                // completion callback.
+                if (result) {
+                  resolve(result);
+                }
+              }).catch(function(e) {
+                console.error(e, e.stack);
+                throw e;
+              });
+            } else return Promise.resolve();
+          });
+        });
+        return pipeline
+          .then(function() {
+            var resultingConfig = Object.assign({}, window.respecConfig);
+            pubsubhub.pub("end-all", resultingConfig);
+          })
+          .then(function() {
+            if (respecConfig.postProcess) {
+              for (var i = 0; i < respecConfig.postProcess.length; i++) {
+                try { respecConfig.postProcess[i].apply(this); } catch (e) { pubsubhub.pub("error", e); }
+              }
+            }
+            if (respecConfig.afterEnd) {
+              try { respecConfig.afterEnd.apply(window, Array.from(arguments)); } catch (e) { pubsubhub.pub("error", e); }
+            }
+          });
+      }
+    };
+  }
 );
