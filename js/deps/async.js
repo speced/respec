ec71eca39203c17afa99690fa7a14a30b2f8caf9
Marcos Caceres <marcos@marcosc.com>
2016-10-06 18:49:49 +1100

    fix(async): override js/deps/async.js

--- js/deps/async.js
+++ js/deps/async.js
@@ -11,29 +11,29 @@
     // It is called with zero or more arguments...
     return function asyncFunction(...args) {
       return new Promise((resolve, reject) => {
-	let gen;
-	if (func.constructor.name === 'GeneratorFunction') {
-	  gen = func.call(self, ...args);
-	} else { // Wrap it
-	  gen = (function*() {
-	    return func.call(self, ...args);
-	  }());
-	}
+        let gen;
+        if (func.constructor.name === 'GeneratorFunction') {
+          gen = func.call(self, ...args);
+        } else { // Wrap it
+          gen = (function*() {
+            return func.call(self, ...args);
+          }());
+        }
 
-	step(gen.next());
+        step(gen.next());
 
-	function step({value, done}) {
-	  if (done) {
-	    return resolve(value);
-	  }
-	  Promise
-	    .resolve(value) // Normalize thenable
-	    .then(
-	      result => step(gen.next(result)),
-	      error => step(gen.throw(error))
-	    )
-	    .catch(reject);
-	}
+        function step({value, done}) {
+          if (done) {
+            return resolve(value);
+          }
+          Promise
+            .resolve(value) // Normalize thenable
+            .then(
+              result => step(gen.next(result)),
+              error => step(gen.throw(error))
+            )
+            .catch(reject);
+        }
       });
     };
   }
