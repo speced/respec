53ad64da1a3134211de5d3a0b7dd3045570802a8
Dominique Hazael-Massieux <dom@w3.org>
2016-10-12 04:24:17 +0200

    Allow configurable canonical URIs for documents (#938)
    * Allow configurable canonical URIs for documents
    * Can be configured to be either the latest TR url (default), the editors draft url, or any http(s) URL
    * Adds test cases for SEO module
    * style(seo): Cleanup white space
    * style(seo-spec.js): whitespace, other nits
    * refactor(seo): move to w3c namespace
    * fix(seo-spec): test URLs are incomplete
    * fix(seo): warning about https
    * fix: always run linter last
    * fix: always force canonicalURI into URL

---
+++ tests/spec/w3c/seo-spec.js
@@ -0,0 +1,50 @@
+"use strict";
+
+function makeTest(uri) {
+  return function(doc) {
+    var canLink = doc.querySelector("link[rel='canonical']");
+    expect(canLink.href).toEqual(uri);
+  };
+}
+
+describe("W3C - SEO", function() {
+  afterAll(function(done) {
+    flushIframes();
+    done();
+  });
+
+  it("should default to TR as canonical URI", function(done) {
+    var test = makeTest("https://www.w3.org/TR/Foo/");
+    makeRSDoc(makeStandardOps(), test)
+      .then(done);
+  });
+
+  it("should set the canonical URI to TR URI when so configured", function(done) {
+    var ops = makeStandardOps();
+    var test = makeTest("https://www.w3.org/TR/Foo/");
+    ops.config.canonicalURI = "TR";
+    makeRSDoc(ops, test).then(done);
+  });
+
+  it("should set the canonical URI to editors draft when so configured", function(done) {
+    var ops = makeStandardOps();
+    var test = makeTest("http://foo.com/");
+    ops.config.canonicalURI = "edDraft";
+    makeRSDoc(ops, test).then(done);
+  });
+
+  it("should not set any canonical URI if no shortname is defined", function(done) {
+    var ops = makeStandardOps();
+    ops.config.shortName = undefined;
+    makeRSDoc(ops, function(doc) {
+      expect(doc.querySelector("link[rel='canonical']")).toEqual(null);
+    }).then(done);
+  });
+
+  it("should set the canonical URI if explicitly set", function(done) {
+    var ops = makeStandardOps();
+    var test = makeTest("http://example.com/");
+    ops.config.canonicalURI = "http://example.com";
+    makeRSDoc(ops, test).then(done);
+  });
+});
