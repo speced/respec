c9f83f999dc00748cddd1cc3ec61d9b7c6b4a247
Marcos Cáceres <marcos+github@marcosc.com>
2016-12-11 15:52:32 -1000

    perf: compile handlebars templates (closes #233) (#988)

--- js/tmpl.js
+++ js/tmpl.js
@@ -1,23 +1,15 @@
 /*global Handlebars*/
 
 define([
-    "deps/handlebars",
     "deps/text",
-], function (hb, text) {
+], function (text) {
     var buildMap = {};
     return {
         load:   function (name, req, onLoad, config) {
             return text.load(name, req, function (content) {
                 if (config.isBuild && config.inlineText) buildMap[name] = content;
-                onLoad(config.isBuild ? content : hb.compile(content));
+                onLoad(content);
             }, config);
-        }
-    ,   write:  function (pluginName, moduleName, write) {
-            if (moduleName in buildMap) {
-                var content = text.jsEscape(buildMap[moduleName]);
-                write("define('" + pluginName + "!" + moduleName  +
-                      "', ['deps/handlebars'], function (hb) { return hb.compile('" + content + "');});\n");
-            }
-        }
+        } 
     };
 });
