e9fb1c69b3f82d5545d0b804f5afbf7077f47d94
Marcos Caceres <marcos@marcosc.com>
2016-12-13 21:03:35 -1000

    chore(override-configuration): reformat code

--- js/core/override-configuration.js
+++ js/core/override-configuration.js
@@ -9,37 +9,37 @@
 //  There could probably be a UI for this to make it even simpler.
 "use strict";
 define(
-    [],
-    function() {
-      return {
-        run: function(conf, doc, cb) { //jshint ignore:line
-          if (!location.search) {
-            return cb();
-          }
-          var overrideProps = doc.location.search
-            //Remove "?" from search
-            .replace(/^\?/, "")
-            // The default separator is ";" for key/value pairs
-            .split(";")
-            .filter(function removeEmpties(item) {
-              return item.trim();
-            })
-            .reduce(function decodeKeyValues(collector, item) {
-              var keyValue = item.split("=", 2);
-              var key = decodeURIComponent(keyValue[0]);
-              var value = decodeURIComponent(keyValue[1].replace(/%3D/g, "="));
-              var parsedValue;
-              try {
-                parsedValue = JSON.parse(value);
-              } catch (err) {
-                parsedValue = value;
-              }
-              collector[key] = parsedValue;
-              return collector;
-            }, {});
-          Object.assign(conf, overrideProps);
-          cb();
+  [],
+  function() {
+    return {
+      run: function(conf, doc, cb) { //jshint ignore:line
+        if (!location.search) {
+          return cb();
         }
-      };
-    }
+        var overrideProps = doc.location.search
+          //Remove "?" from search
+          .replace(/^\?/, "")
+          // The default separator is ";" for key/value pairs
+          .split(";")
+          .filter(function removeEmpties(item) {
+            return item.trim();
+          })
+          .reduce(function decodeKeyValues(collector, item) {
+            var keyValue = item.split("=", 2);
+            var key = decodeURIComponent(keyValue[0]);
+            var value = decodeURIComponent(keyValue[1].replace(/%3D/g, "="));
+            var parsedValue;
+            try {
+              parsedValue = JSON.parse(value);
+            } catch (err) {
+              parsedValue = value;
+            }
+            collector[key] = parsedValue;
+            return collector;
+          }, {});
+        Object.assign(conf, overrideProps);
+        cb();
+      }
+    };
+  }
 );
