e1353a21d9b3cced363496715cf7092d16cb2786
Marcos Caceres <marcos@marcosc.com>
2016-12-29 13:09:06 +1100

    style(default-root-attr): fix coding style

--- js/core/default-root-attr.js
+++ js/core/default-root-attr.js
@@ -1,23 +1,22 @@
-
 // Module core/default-root-attr
 // In cases where it is recommended that a document specify its language and writing direction,
 // this module will supply defaults of "en" and "ltr" respectively (but won't override
 // specified values).
 // Be careful in using this that these defaults make sense for the type of document you are
 // publishing.
-
+"use strict";
 define(
-    [],
-    function () {
-        return {
-            run:    function (config, doc, cb) {
-                var $root = $(doc.documentElement);
-                if (!$root.attr("lang")) {
-                    $root.attr("lang", "en");
-                    if (!$root.attr("dir")) $root.attr("dir", "ltr");
-                }
-                cb();
-            }
-        };
-    }
+  [],
+  function() {
+    return {
+      run: function(config, doc, cb) {
+        var $root = $(doc.documentElement);
+        if (!$root.attr("lang")) {
+          $root.attr("lang", "en");
+          if (!$root.attr("dir")) $root.attr("dir", "ltr");
+        }
+        cb();
+      }
+    };
+  }
 );
