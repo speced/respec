c07e514daab9c3bca471d58b8533a3bec9b65de5
Marcos Caceres <marcos@marcosc.com>
2017-01-03 15:04:36 +1100

    style(core/shiv): missing semi colons

--- js/core/shiv.js
+++ js/core/shiv.js
@@ -1,9 +1,9 @@
 // Module core/shiv
 // Injects the HTML5 shiv conditional comment
-"use strict"
+"use strict";
 define([], function() {
   var txt = "[if lt IE 9]><script src='https://www.w3.org/2008/site/js/html5shiv.js'></script><![endif]";
   var cmt = document.createComment(txt);
   document.head.appendChild(cmt);
-  return {}
+  return {};
 });
