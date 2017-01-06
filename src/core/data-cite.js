46b08b381db923613e9e44a5a8d14b43ff71369c
Marcos Caceres <marcos@marcosc.com>
2017-01-07 09:33:19 +1100

    feat(data-cite): support data-cite-frag + cite shorthand

--- src/core/data-cite.js
+++ src/core/data-cite.js
@@ -11,65 +11,73 @@
  * if it starts with a ! or not.
  *
  * Usage:
- * https://github.com/w3c/respec/wiki
+ * https://github.com/w3c/respec/wiki/data--cite
  */
-import pubsubhub from "core/pubsubhub";
-import { biblio } from "core/biblio";
+import { pub } from "core/pubsubhub";
+import { resolveRef } from "core/biblio";
 
 async function toLookupRequest(elem) {
   const originalKey = elem.dataset.cite;
-  var key = elem.dataset.cite.trim();
-  if (key.startsWith("!")) {
-    key = key.substr(1);
-  }
-  const entry = await biblio.resolveRef(key);
-  elem.removeAttribute("data-cite");
+  let { key, frag } = toCiteDetails(elem);
+  const entry = await resolveRef(key);
+  cleanElement(elem);
   if (!entry) {
-    var msg = "Couldn't find a match for `data-cite=" + originalKey + "`.";
+    var msg = `Couldn't find a match for 'data-cite=${originalKey}'.`;
     console.warn(msg, elem);
     msg += " Please check developer console for offending element.";
-    pubsubhub.pub("warn", msg);
+    pub("warn", msg);
     return;
   }
-  elem.href = entry.href;
-}
-
-export const dataCite = {
-  run(conf, doc, cb) {
-    Array
-      .from(doc.querySelectorAll(["a[data-cite]"]))
-      .filter(el => el.dataset.cite)
-      .map(el => {
-        const ref = el.dataset.cite.trim();
-        const isNormative = ref.startsWith("!");
-        return {
-          isNormative,
-          key: (isNormative) ? ref.substr(1) : ref
-        };
-      })
-      .reduce((conf, refDetails) => {
-        if (refDetails.isNormative) {
-          conf.normativeReferences.add(refDetails.key);
-        } else {
-          conf.informativeReferences.add(refDetails.key);
-        }
-        return conf;
-      }, conf);
-    cb();
-  },
-  async linkInlineCitations(doc) {
-    const citedSpecs = doc.querySelectorAll("a[data-cite]");
-    const lookupRequests = Array
-      .from(citedSpecs)
-      .map(toLookupRequest);
-    return await Promise.all(lookupRequests);
+  let { href } = entry;
+  if (frag) {
+    href += frag;
   }
-};
-
-export function run(...args){
-  dataCite.run(...args);
+  elem.href = href;
 }
 
-export function linkInlineCitations(doc){
-  return dataCite.linkInlineCitations(doc);
+function cleanElement(elem) {
+  ["data-cite", "data-cite-frag"]
+    .filter(attrName => elem.hasAttribute(attrName))
+    .forEach(attrName => elem.removeAttribute(attrName))
+}
+
+function toCiteDetails({ dataset }) {
+  let { cite: key, citeFrag: frag } = dataset;
+  const isNormative = key.startsWith("!");
+  const fragPosition = key.search("#");
+  if (fragPosition !== -1) {
+    frag = (!frag) ? key.substr(fragPosition) : frag;
+    key = key.substring(0, fragPosition);
+  }
+  if (isNormative) {
+    key = key.substr(1);
+  }
+  if (frag && !frag.startsWith("#")) {
+    frag = "#" + frag;
+  }
+  return { key, isNormative, frag };
+}
+
+export function run(conf, doc, cb) {
+  Array
+    .from(doc.querySelectorAll(["a[data-cite]"]))
+    .filter(el => el.dataset.cite)
+    .map(toCiteDetails)
+    .reduce((conf, { isNormative, key }) => {
+      if (isNormative) {
+        conf.normativeReferences.add(key);
+      } else {
+        conf.informativeReferences.add(key);
+      }
+      return conf;
+    }, conf);
+  cb();
+}
+
+export async function linkInlineCitations(doc) {
+  const citedSpecs = doc.querySelectorAll("a[data-cite]");
+  const lookupRequests = Array
+    .from(citedSpecs)
+    .map(toLookupRequest);
+  return await Promise.all(lookupRequests);
 }
