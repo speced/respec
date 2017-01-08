2c72b892560f181fa53072c1d75fb75a5f18b32c
Marcos Caceres <marcos@marcosc.com>
2017-01-08 18:36:53 +1100

    refactor(core/respec-ready): ES6ify

--- src/core/respec-ready.js
+++ src/core/respec-ready.js
@@ -3,28 +3,14 @@
  * The property returns a promise that settles when ReSpec finishes
  * processing the document.
  */
+import { sub } from "core/pubsubhub";
 
-"use strict";
-define(["core/pubsubhub"], function (pubsubhub) {
-  var respecDone = false;
-  var respecDonePromise = new Promise(function (resolve) {
-    var opaque = pubsubhub.sub("end-all", function(conf){
-      pubsubhub.unsub(opaque);
-      respecDone = true;
-      resolve(conf);
-    });
-  });
-  Object.defineProperty(document, "respecDone", {
-    get: function () {
-      var warn = "document.respecDone is deprecated, use document.respecIsReady instead.";
-      pubsubhub.pub("warn", warn);
-      return respecDone;
-    }
-  });
-  Object.defineProperty(document, "respecIsReady", {
-    get: function () {
-      return respecDonePromise;
-    },
-  });
-  return respecDonePromise;
+const respecDonePromise = new Promise(resolve => {
+  sub("end-all", resolve, { once: true });
+});
+
+Object.defineProperty(document, "respecIsReady", {
+  get() {
+    return respecDonePromise;
+  },
 });
