4b4908125cbb63611974029ecfac588b2948b481
Marcos Caceres <marcos@marcosc.com>
2017-01-08 18:35:15 +1100

    refactor(core/remove-respec): ES6ify

--- src/core/remove-respec.js
+++ src/core/remove-respec.js
@@ -1,18 +1,8 @@
-
 // Module core/remove-respec
 // Removes all ReSpec artefacts right before processing ends
+import utils from "core/utils";
+import { sub } from "core/pubsubhub";
 
-define(
-    ["core/utils"],
-    function (utils) {
-        return {
-            run:    function (conf, doc, cb) {
-                // it is likely that some asynch operations won't have completed at that moment
-                // if they happen to need the artefacts, we could change this to be hooked into
-                // the base-runner to run right before end-all
-                utils.removeReSpec(doc);
-                cb();
-            }
-        };
-    }
-);
+sub('end-all', () => {
+  utils.removeReSpec(document);
+}, { once: true });
