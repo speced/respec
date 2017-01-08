c32ca042fcbf60c8d3eacf529e2eb934df66c5cc
Marcos Caceres <marcos@marcosc.com>
2017-01-08 18:18:12 +1100

    refactor(pubsubhub): ES6ify

--- src/core/pubsubhub.js
+++ src/core/pubsubhub.js
@@ -4,78 +4,54 @@
  * Returns a singleton that can be used for message broadcasting
  * and message receiving. Replaces legacy "msg" code in ReSpec.
  */
+const subscriptions = new Map();
 
-"use strict";
-define([], function() {
-  var subscriptions = new Map();
-  var PubSubHub = {
-    pub: function(topic) {
-      // Nothing to do...
-      if (!subscriptions.has(topic)) {
-        return;
-      }
-      var restParams = Array
-        .from(arguments)
-        .slice(1);
-      // we take a copy, because subscribers can unsubscribe themselves
-      // leading to race conditions
-      Array
-        .from(subscriptions.get(topic))
-        .forEach(function(cb) {
-          cb.apply(null, restParams);
-        });
+export function pub(topic, ...data) {
+  if (!subscriptions.has(topic)) {
+    return; // Nothing to do...
+  }
+  Array
+    .from(subscriptions.get(topic))
+    .forEach(cb => cb.apply(null, data));
+  if (window.parent === window.self) {
+    return;
+  }
+  // If this is an iframe, postMessage parent (used in testing).
+  var args = data
+    // to structured clonable
+    .map(arg => String(JSON.stringify(arg.stack || arg)));
+  window.parent.postMessage({ topic, args }, window.parent.location.origin);
+};
 
-      // If this is an iframe, postMessage parent (used in testing).
-      if (window.parent === window.self) {
-        return;
-      }
-      var args = restParams.map(function toStructuredClonable(arg) {
-        var str = String(JSON.stringify(arg.stack || arg));
-        return str;
-      });
-      var msg = {
-        topic: topic,
-        args: args,
-      };
-      window.parent.postMessage(msg, window.parent.location.origin);
-    },
-    sub: function(topic, cb) {
-      if (!subscriptions.has(topic)) {
-        subscriptions.set(topic, [cb]);
-      } else {
-        subscriptions.get(topic).push(cb);
-      }
-      return {
-        topic: topic,
-        cb: cb,
-      };
-    },
-    unsub: function(opaque) { // opaque is whatever is returned by sub()
-      var callbacks = subscriptions.get(opaque.topic);
-      if (!callbacks || callbacks.indexOf(opaque.cb) === -1) {
-        console.warn("Already unsubscribed:", opaque.topic, opaque.cb);
-        return;
-      }
-      callbacks.splice(callbacks.indexOf(opaque.cb), 1);
-    }
-  };
+export function sub(topic, cb, opts = { once: false }) {
+  if (opts.once) {
+    const opaque = sub(topic, (...args) => {
+      cb(...args);
+      unsub(opaque);
+    });
+    return;
+  }
+  if (!subscriptions.has(topic)) {
+    subscriptions.set(topic, [cb]);
+  } else {
+    subscriptions.get(topic).push(cb);
+  }
+  return { topic, cb };
+};
 
-  PubSubHub.sub("error", function(err) {
-    console.error(err.stack || err);
-  });
+export function unsub(opaque) { // opaque is whatever is returned by sub()
+  var callbacks = subscriptions.get(opaque.topic);
+  if (!callbacks || callbacks.indexOf(opaque.cb) === -1) {
+    console.warn("Already unsubscribed:", opaque.topic, opaque.cb);
+    return;
+  }
+  callbacks.splice(callbacks.indexOf(opaque.cb), 1);
+};
 
-  PubSubHub.sub("warn", function(str) {
-    console.warn(str);
-  });
+sub("error", err => {
+  console.error(err.stack || err);
+});
 
-  // Add deprecation warning
-  Object.defineProperty(window, "respecEvents", {
-    get: function() {
-      var warning = "window.respecEvents() is deprecated. Use 'require([\"core/pubsubhub\"], function(){...})' instead.";
-      PubSubHub.pub("warn", warning);
-      return PubSubHub;
-    }
-  });
-
-  return PubSubHub;
+sub("warn", str => {
+  console.warn(str);
 });
