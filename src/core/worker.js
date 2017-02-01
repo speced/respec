980bf7f4b483b527c0d38011c6e3205a9654145d
Marcos Caceres <marcos@marcosc.com>
2017-02-01 14:53:40 +1100

    feat(core/worker): opportunistically preload syntax highlighter

--- src/core/worker.js
+++ src/core/worker.js
@@ -5,8 +5,20 @@
  * multi-threaded processing of things.
  */
 
+// Opportunistically preload syntax highlighter, which is used by the worker
+import utils from "core/utils";
+// Opportunistically preload syntax highlighter
+const hint = {
+  hint: "preload",
+  href: "https://www.w3.org/Tools/respec/respec-highlight.js",
+  as: "script",
+};
+const link = utils.createResourceHint(hint);
+document.head.appendChild(link);
+
 // Only shipping ReSpec has a version
 const url = (typeof window.respecVersion === "undefined" || window.respecVersion === "Developer Edition")
   ? "/worker/respec-worker.js"
   : "https://www.w3.org/Tools/respec/respec-worker";
+
 export const worker = new Worker(url);
