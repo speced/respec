3dbfab290328579334e98e11c77a2cd603cbad2a
Marcos Caceres <marcos@marcosc.com>
2017-02-01 17:07:56 +1100

    style: fix indenting

--- worker/respec-worker.js
+++ worker/respec-worker.js
@@ -5,13 +5,13 @@ importScripts("https://www.w3.org/Tools/respec/respec-highlight.js");
 hljs.configure({
   tabReplace: "  ", // 2 spaces
   languages: [
-   "css",
-   "http",
-   "javascript",
-   "json",
-   "markdown",
-   "xml",
-   "xquery",
+    "css",
+    "http",
+    "javascript",
+    "json",
+    "markdown",
+    "xml",
+    "xquery",
   ],
 });
 
@@ -22,6 +22,13 @@ self.addEventListener("message", function(e) {
       const langs = e.data.languages.length ? e.data.languages : undefined;
       const result = self.hljs.highlightAuto(code, langs);
       const data = Object.assign({}, e.data, result);
-      self.postMessage(data);
+      try {
+        self.postMessage(data);
+      } catch (err) {
+        console.error("Could not transform some code?", err);
+        // Post back the original unhighlighted code.
+        const fallbackData = Object.assign({}, e.data, {value: e.data.code});
+        self.postMessage(data);
+      }
   }
 });
