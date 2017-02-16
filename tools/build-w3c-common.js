315a62eb1b339475f4fc377a206e2e150f4b34b9
Marcos Caceres <marcos@marcosc.com>
2017-02-17 01:02:56 +1100

    chore(build): generalize builder

--- tools/build-w3c-common.js
+++ tools/build-w3c-common.js
@@ -3,41 +3,22 @@
 "use strict";
 const async = require("marcosc-async");
 const Builder = require("./builder").Builder;
-const path = require("path");
 const colors = require("colors");
 
 colors.setTheme({
-  data: "grey",
-  debug: "cyan",
   error: "red",
-  help: "cyan",
-  info: "green",
-  input: "grey",
-  prompt: "grey",
-  verbose: "cyan",
-  warn: "yellow",
 });
 
-const buildW3C = async(function*() {
-  let versionsToBuild = Array.from(arguments);
-  if (!versionsToBuild.length){
-    versionsToBuild.push("latest");
-  }
-  const builds = path.join(__dirname, "../builds");
-  for(let aVersion of versionsToBuild){
-    const isLatest = aVersion === "latest";
-    const version = (isLatest) ? yield Builder.getRespecVersion() : aVersion;
-    const outFile = "respec-w3c-common" + ((isLatest) ? ".js" : `-${aVersion}.js`);
-    const out = path.join(builds, outFile);
-    console.log(colors.info(`  ⏲  Generating ${outFile}. Please wait...`));
-    yield Builder.build({out, version});
-  }
-});
+function buildW3C() {
+  return async.task(function*() {
+    yield Builder.build({ name: "w3c-common" });
+  });
+}
 
 if (require.main === module) {
-  buildW3C("latest")
+  buildW3C()
     .catch(
-      err => console.error(`☠️ ${colors.error(err.stack)}`)
+      err => console.error(` ☠ ️ ${colors.error(err.stack)}`)
     );
 }
 
--- tools/builder.js
+++ tools/builder.js
@@ -2,10 +2,16 @@
 
 "use strict";
 const async = require("marcosc-async");
+const colors = require("colors");
 const fsp = require("fs-promise");
-const pth = require("path");
+const loading = require("loading-indicator");
+const path = require("path");
+const presets = require('loading-indicator/presets');
 const r = require("requirejs");
 const UglifyJS = require("uglify-js");
+colors.setTheme({
+  info: "green",
+});
 
 /**
  * Async function that appends the boilerplate to the generated script
@@ -16,7 +22,7 @@ const UglifyJS = require("uglify-js");
  * @param  {String} version The version of the script.
  * @return {Promise} Resolves when done writing the files.
  */
-function appendBoilerplate(outPath, version) {
+function appendBoilerplate(outPath, version, name) {
   return async(function*(optimizedJs, sourceMap) {
     const respecJs = `"use strict";
 /* ReSpec ${version}
@@ -25,12 +31,12 @@ Documentation: http://w3.org/respec/.
 See original source for licenses: https://github.com/w3c/respec */
 window.respecVersion = "${version}";
 ${optimizedJs}
-require(['profile-w3c-common']);`;
+require(['profile-${name}']);`;
     const result = UglifyJS.minify(respecJs, {
       fromString: true,
-      outSourceMap: "respec-w3c-common.build.js.map",
+      outSourceMap: `respec-${name}.build.js.map`,
     });
-    const mapPath = pth.dirname(outPath) + "/respec-w3c-common.build.js.map";
+    const mapPath = path.dirname(outPath) + `/respec-${name}.build.js.map`;
     const promiseToWriteJs = fsp.writeFile(outPath, result.code, "utf-8");
     const promiseToWriteMap = fsp.writeFile(mapPath, sourceMap, "utf-8");
     yield Promise.all([promiseToWriteJs, promiseToWriteMap]);
@@ -44,8 +50,8 @@ var Builder = {
    * @returns {Promise<String>} The version string.
    */
   getRespecVersion: async(function*() {
-    const path = pth.join(__dirname, "../package.json");
-    const content = yield fsp.readFile(path, "utf-8");
+    const packagePath = path.join(__dirname, "../package.json");
+    const content = yield fsp.readFile(packagePath, "utf-8");
     return JSON.parse(content).version;
   }),
 
@@ -56,22 +62,33 @@ var Builder = {
    * @param  {[type]} options [description]
    * @return {[type]}         [description]
    */
-  build(options) {
+  build({ name }) {
+    if (!name)  {
+      throw new TypeError("name is required");
+    }
+    const buildPath = path.join(__dirname, "../builds");
+    const outFile = `respec-${name}.js`;
+    const outPath = path.join(buildPath, outFile);
+    const loadingMsg = colors.info(` Generating ${outFile}. Please wait... `);
+    const timer = loading.start(loadingMsg, {
+      frames: presets.clock,
+      delay: 1,
+    });
     return async.task(function*() {
       // optimisation settings
-      const version = options.version || (yield this.getRespecVersion());
-      const outputWritter = appendBoilerplate(options.out, version);
+      const buildVersion = yield this.getRespecVersion();
+      const outputWritter = appendBoilerplate(outPath, buildVersion, name);
       const config = {
-        generateSourceMaps: true,
-        mainConfigFile: "js/profile-w3c-common.js",
-        baseUrl: pth.join(__dirname, "../js/"),
-        optimize: options.optimize || "uglify",
-        name: "profile-w3c-common",
-        logLevel: 2, // Show uglify warnings and errors.
+        baseUrl: path.join(__dirname, "../js/"),
         deps: [
           "deps/require",
         ],
+        generateSourceMaps: true,
         inlineText: true,
+        logLevel: 2, // Show uglify warnings and errors.
+        mainConfigFile: `js/profile-${name}.js`,
+        name: `profile-${name}`,
+        optimize: "uglify",
         preserveLicenseComments: false,
         useStrict: true,
       };
@@ -83,14 +100,15 @@ var Builder = {
         };
       });
       r.optimize(config);
-      const buildDir = pth.resolve(__dirname, "../builds/");
-      const workerDir = pth.resolve(__dirname, "../worker/");
+      const buildDir = path.resolve(__dirname, "../builds/");
+      const workerDir = path.resolve(__dirname, "../worker/");
       //console.log(buildDir, workerDir);
       yield promiseToWrite;
       // copy respec-worker
       fsp
         .createReadStream(`${workerDir}/respec-worker.js`)
         .pipe(fsp.createWriteStream(`${buildDir}/respec-worker.js`));
+      loading.stop(timer);
     }, this);
   },
 };
