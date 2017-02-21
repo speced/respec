e707394e5979e9da0bd78c9fc77cdcdc0a5a4c8f
Marcos Caceres <marcos@marcosc.com>
2017-02-21 19:40:10 +1100

    feat(js/profile-geonovum): base module for geonovum

---
+++ js/profile-geonovum.js
@@ -0,0 +1,113 @@
+"use strict";
+// Hide document, because we are about to change it radically.
+if (document.body) {
+  document.body.hidden = true;
+} else {
+  document.addEventListener("DOMContentLoaded", function() {
+    document.body.hidden = true;
+  }, {once: true});
+}
+
+// In case everything else fails, we always want to show the document
+window.addEventListener("error", function(err) {
+  console.error(err);
+  document.body.hidden = false;
+});
+
+// this is only set in a build, not at all in the dev environment
+require.config({
+  shim: {
+    shortcut: {
+      exports: "shortcut"
+    },
+    highlight: {
+      exports: "hljs"
+    },
+    beautify: {
+      exports: "beautify"
+    }
+  },
+  paths: {
+    "beautify-css": "deps/beautify-css",
+    "beautify-html": "deps/beautify-html",
+    "handlebars.runtime": "deps/handlebars",
+    "deps/highlight": "https://www.w3.org/Tools/respec/respec-highlight",
+  },
+  deps: [
+    "deps/fetch",
+  ],
+});
+
+define([
+    // order is significant
+    "deps/domReady",
+    "core/base-runner",
+    "core/ui",
+
+    // ********************************************
+    // Uncomment the relevant ones below...
+    // ********************************************
+
+    // "core/aria",
+    // "core/style",
+    // "w3c/style",
+    // "w3c/l10n",
+    // "core/data-include",
+    // "core/markdown",
+    // "w3c/headers",
+    // "w3c/abstract",
+    // "w3c/conformance",
+    // "core/data-transform",
+    // "core/inlines",
+    // "core/dfn",
+    // "w3c/rfc2119",
+    // "core/examples",
+    // "core/issues-notes",
+    // "core/requirements",
+    // "core/best-practices",
+    // "core/figures",
+    // "core/webidl-contiguous",
+    // "core/data-cite",
+    // "core/biblio",
+    // "core/webidl-index",
+    // "core/webidl-oldschool",
+    // "core/link-to-dfn",
+    // "core/contrib",
+    // "core/fix-headers",
+    // "core/structure",
+    // "w3c/informative",
+    // "w3c/permalinks",
+    // "core/id-headers",
+    // "core/rdfa",
+    // "w3c/aria",
+    // "core/shiv",
+    // "core/location-hash",
+    // "ui/about-respec",
+    // "ui/dfn-list",
+    // "ui/save-html",
+    // "ui/search-specref",
+    // "w3c/seo",
+    // "core/highlight",
+    // /*Linter must be the last thing to run*/
+    // "w3c/linter",
+  ],
+  function(domReady, runner, ui) {
+    var args = Array.from(arguments).filter(function(item) {
+      return item;
+    });
+    ui.show();
+    domReady(function() {
+      runner
+        .runAll(args)
+        .then(document.respecIsReady)
+        .then(ui.enable)
+        .catch(function(err) {
+          console.error(err);
+          // In case processing fails, we still want to show the document.
+          document.body.hidden = false;
+          // even if things go critically bad, we should still try to show the UI
+          ui.enable();
+        });
+    });
+  }
+);
