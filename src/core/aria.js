a7fda70fea5434c47c2d6119b88c182a9690a634
Marcos Caceres <marcos@marcosc.com>
2017-03-01 15:33:24 +1100

    fix(core/aria): race condition when no body (closes #1113)

--- src/core/aria.js
+++ src/core/aria.js
@@ -2,9 +2,14 @@ import { sub } from "core/pubsubhub";
 
 export const name = "core/aria";
 
-document.body.setAttribute("aria-busy", "true");
+if (document.body) {
+  document.body.setAttribute("aria-busy", "true");
+} else {
+  document.addEventListener("DOMContentLoaded", () => {
+    document.body.setAttribute("aria-busy", "true");
+  }, { once: true });
+}
 
 sub("end-all", () => {
   document.body.setAttribute("aria-busy", "false");
-  document.body.hidden = false;
 }, { once: true });
