6c678f9a0b3d9a4faf4f67541d2ecf6dbc4b1031
Marcos Caceres <marcos@marcosc.com>
2017-03-09 19:00:26 +1100

    refactor(core/best-practices): convert to ES6 module

---
+++ src/core/best-practices.js
@@ -0,0 +1,32 @@
+// Module core/best-practices
+// Handles the marking up of best practices, and can generate a summary of all of them.
+// The summary is generated if there is a section in the document with ID bp-summary.
+// Best practices are marked up with span.practicelab.
+import css from "deps/text!core/css/bp.css";
+import { pub } from "core/pubsubhub";
+
+export function run(conf, doc, cb) {
+  var num = 0;
+  var $bps = $("span.practicelab", doc);
+  var $content = $("<div><h2>Best Practices Summary</h2><ul></ul></div>");
+  var $ul = $content.find("ul");
+  $bps.each(function() {
+    var $bp = $(this);
+    var id = $bp.makeID("bp");
+    var $li = $("<li><a></a></li>");
+    var $a = $li.find("a");
+    num++;
+    $a.attr("href", "#" + id).text("Best Practice " + num);
+    $li.append(doc.createTextNode(": " + $bp.text()));
+    $ul.append($li);
+    $bp.prepend(doc.createTextNode("Best Practice " + num + ": "));
+  });
+  if ($bps.length) {
+    $(doc).find("head link").first().before($("<style/>").text(css));
+    if ($("#bp-summary")) $("#bp-summary").append($content.contents());
+  } else if ($("#bp-summary").length) {
+    pub("warn", "Using best practices summary (#bp-summary) but no best practices found.");
+    $("#bp-summary").remove();
+  }
+  cb();
+}
