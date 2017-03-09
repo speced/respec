55e1f4fced74f7dac277af8564a848b001090502
Marcos Caceres <marcos@marcosc.com>
2017-03-10 00:01:29 +1100

    BREAKING CHANGE: remove shiv

--- js/core/shiv.js
+++
@@ -1,9 +0,0 @@
-// Module core/shiv
-// Injects the HTML5 shiv conditional comment
-"use strict";
-define([], function() {
-  var txt = "[if lt IE 9]><script src='https://www.w3.org/2008/site/js/html5shiv.js'></script><![endif]";
-  var cmt = document.createComment(txt);
-  document.head.appendChild(cmt);
-  return {};
-});
