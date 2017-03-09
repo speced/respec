850eca5e0c6968c24308e281b806791e5dd9f450
Marcos Caceres <marcos@marcosc.com>
2017-03-10 10:31:34 +1100

    refactor(core/style): convert to ES6 module

---
+++ src/core/style.js
@@ -0,0 +1,24 @@
+// Module core/style
+// Inserts the CSS that ReSpec uses into the document.
+//
+// IMPORTANT NOTE
+//  To add you own styles, create a plugin that declares the css as a dependency
+//  and create a build of your new ReSpec profile.
+//
+// CONFIGURATION
+//  - noReSpecCSS: if you're using a profile that loads this module but you don't want
+//    the style, set this to true
+import css from "deps/text!core/css/respec2.css";
+
+// Opportunistically inserts the style, with the chance to reduce some FOUC
+const styleElement = document.createElement("style");
+styleElement.id = "respec-mainstyle";
+styleElement.textContent = css;
+document.head.appendChild(styleElement);
+
+export function run(conf, doc, cb) {
+  if (conf.noReSpecCSS) {
+    styleElement.remove();
+  }
+  cb();
+}
