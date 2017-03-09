48bee2a40fc845ddfc9cc758e7025c94f73cf937
Marcos Caceres <marcos@marcosc.com>
2017-03-09 19:20:08 +1100

    refactor(core/dfn): convert to ES6 module

---
+++ src/core/dfn.js
@@ -0,0 +1,45 @@
+// Module core/dfn
+// Finds all <dfn> elements and populates conf.definitionMap to identify them.
+
+function canonicalize(query, attributeName) {
+  Array
+    .from(document.querySelectorAll(query))
+    .forEach(function(el) {
+      el.dataset.dfnFor = el.getAttribute(attributeName).toLowerCase();
+      el.removeAttribute(attributeName);
+    });
+}
+
+export function run(conf, doc, cb) {
+  if (!conf.hasOwnProperty("definitionMap")) {
+    conf.definitionMap = Object.create(null);
+  }
+  // Canonicalize "dfn-for" and "for" attributes to data-dfn-for
+  canonicalize("[dfn-for]", "dfn-for");
+  canonicalize("dfn[for]", "for");
+  Array
+    .from(document.querySelectorAll("dfn"))
+    .forEach(function(dfn) {
+      // TODO: Microsoft Edge doesn't support Element.closest() yet.
+      const value = $(dfn).closest("[data-dfn-for]").attr("data-dfn-for");
+      dfn.dataset.dfnFor = (value) ? value.toLowerCase() : "";
+      // TODO: dfn's are tragically jquery'ed and stored. Should fix this.
+      // Also, we should probably use weakmaps and weaksets here
+      // to avoid leaks.
+      const $dfn = $(dfn);
+      $dfn
+        .getDfnTitles({ isDefinition: true })
+        .map(function(dfnTitle) {
+          if (!conf.definitionMap[dfnTitle]) {
+            conf.definitionMap[dfnTitle] = [];
+          }
+          return conf.definitionMap[dfnTitle];
+        })
+        .reduce(function($dfn, dfnTitleContainer) {
+          dfnTitleContainer.push($dfn);
+          return $dfn;
+        }, $dfn);
+    });
+  cb();
+}
+
