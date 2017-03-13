3a86e74df9a217379c8a67a1f1e0101430a49c8c
Marcos Caceres <marcos@marcosc.com>
2017-03-13 21:34:47 +1100

    refactor(w3c/abstract): convert to ES6 module

---
+++ src/w3c/abstract.js
@@ -0,0 +1,19 @@
+// Module w3c/abstract
+// Handle the abstract section properly.
+import { pub } from "core/pubsubhub";
+
+function run(conf, doc, cb) {
+  var $abs = $("#abstract");
+  if ($abs.length) {
+    if ($abs.find("p").length === 0) $abs.contents().wrapAll($("<p></p>"));
+    $abs.prepend("<h2>" + conf.l10n.abstract + "</h2>");
+    $abs.addClass("introductory");
+    if (conf.doRDFa) {
+      var rel = "dc:abstract",
+        ref = $abs.attr("property");
+      if (ref) rel = ref + " " + rel;
+      $abs.attr({ property: rel });
+    }
+  } else pub("error", "Document must have one element with ID 'abstract'");
+  cb();
+}
