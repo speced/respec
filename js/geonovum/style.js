feb94ee13cae280f00858602c0f926c8b48c96e9
Dirk Jan Venema <DVenema@geonovum.nl>
2017-03-15 12:21:47 +0100

    New links to tools.geostandards

--- js/geonovum/style.js
+++ js/geonovum/style.js
@@ -57,11 +57,11 @@ define([
         as: "script",
       }, {
         hint: "preload", // all specs include on base.css.
-        href: "/media/base.css",
+        href: "http://tools.geostandaarden.nl/respec/media/base.css",
         as: "style",
       }, {
         hint: "preload", // all specs show the logo.
-        href: "/media/logos/Geonovum",
+        href: "http://tools.geostandaarden.nl/respec/media/logos/Geonovum.png",
         as: "image",
       }]
       .map(utils.createResourceHint.bind(utils))
@@ -91,7 +91,7 @@ define([
         pubsubhub.pub("warn", warn);
       }
 
-      var styleBaseURL = "/media/";
+      var styleBaseURL = "http://tools.geostandaarden.nl/respec/media/";
       var finalStyleURL = "";
       var styleFile = "";
 
