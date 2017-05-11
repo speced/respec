39982ba106bb66f9e8e9a66a59354eaff94d0759
Marcos Caceres <marcos@marcosc.com>
2017-05-11 14:12:38 +1000

    feat(core/github): add github: option

---
+++ src/core/github.js
@@ -0,0 +1,89 @@
+/**
+ * core/github
+ *
+ * @see https://github.com/w3c/respec/wiki/github
+ */
+
+import "deps/regenerator";
+import l10n from "core/l10n";
+import { pub } from "core/pubsubhub";
+
+export const name = "core/github";
+
+function isValidGitHubURL(ghURL) {
+  if (ghURL.protocol !== "https:") {
+    return false;
+  }
+  if (ghURL.origin !== "https://github.com") {
+    return false;
+  }
+  return true;
+}
+
+export async function run(conf) {
+  if (!conf.hasOwnProperty("github") || !conf.github) {
+    // nothing to do, bail out.
+    return;
+  }
+  if (
+    typeof conf.github === "object" &&
+    !conf.github.hasOwnProperty("repoURL")
+  ) {
+    pub(
+      "error",
+      "`respecConf.github` missing property `repoURL`. See https://github.com/w3c/respec/wiki/github"
+    );
+    return;
+  }
+  let ghURL;
+  try {
+    ghURL = new URL(conf.github.repoURL || conf.github);
+  } catch (err) {
+    pub("error", `\`respecConf.github\` is not a valid URL? (${ghURL})`);
+    return;
+  }
+  if (!isValidGitHubURL(ghURL)) {
+    pub(
+      "error",
+      `\`respecConf.github\` must be HTTPS and pointing to GitHub. (${ghURL})`
+    );
+    return;
+  }
+  const [org, repo] = ghURL.pathname.split("/").filter(item => item);
+  const branch = conf.github.branch || "gh-pages";
+  const newProps = {
+    edDraftURI: `https://${org}.github.io/${repo}/`,
+    githubAPI: `https://api.github.com/repos/${org}/${repo}`,
+    issueBase: `${ghURL.href}${ghURL.pathname.endsWith("/") ? "" : "/"}issues`,
+  };
+  const commitsHref = `${ghURL.href}${ghURL.pathname.endsWith("/") ? "" : "/"}commits/${branch}`;
+  const otherLink = {
+    key: conf.l10n.participate,
+    data: [
+      {
+        value: `GitHub ${org}/${repo}`,
+        href: ghURL,
+      },
+      {
+        value: conf.l10n.file_a_bug,
+        href: newProps.issueBase,
+      },
+      {
+        value: conf.l10n.commit_history,
+        href: commitsHref,
+      },
+    ],
+  };
+  // Write new properties, ignoring existing ones
+  Object.getOwnPropertyNames(newProps)
+    .filter(key => !conf.hasOwnProperty(key))
+    .map(key => ({ key, value: newProps[key] }))
+    .reduce((conf, { key, value }) => {
+      conf[key] = value;
+      return conf;
+    }, conf);
+  if (!conf.hasOwnProperty("otherLinks")) {
+    conf.otherLinks = [];
+  }
+  conf.otherLinks.unshift(otherLink);
+}
