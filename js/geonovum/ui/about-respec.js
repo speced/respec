688821278f11391bff3bfbea591d6768ddc0edfc
dhvenema <DVenema@geonovum.nl>
2017-05-19 12:48:00 +0200

    refacter: prettier ui es5 style

--- js/geonovum/ui/about-respec.js
+++ js/geonovum/ui/about-respec.js
@@ -1,19 +1,27 @@
-// Module ui/about-respec
+// Module geonovum/ui/about-respec
 // A simple about dialog with pointer to the help
 "use strict";
-define(
-  ["core/ui"],
-  function(ui) {
-    // window.respecVersion is added at build time (see tools/builder.js)
-    window.respecVersion = window.respecVersion || "Developer Edition";
-    const $halp = $("<div><p>ReSpec ondersteunt het productieproces van Standaardisatiedocumenten.</p></div>");
-    $("<p>Meer informatie over de tool leest u in de <a href='https://w3.org/respec/'>documentatie</a>.</p>").appendTo($halp);
-    $("<p>Een issue gevonden in ReSpec? <a href='https://github.com/w3c/respec/issues'>Meld het</a>!</p>").appendTo($halp);
-    const button = ui.addCommand("Over ReSpec", "geonovum/ui/about-respec", "Ctrl+Shift+Alt+A", "ℹ️");
-    return {
-      show: function() {
-        ui.freshModal("About ReSpec - " + window.respecVersion, $halp, button);
-      }
-    };
-  }
-);
+define(["core/ui"], function(ui) {
+  // window.respecVersion is added at build time (see tools/builder.js)
+  window.respecVersion = window.respecVersion || "Developer Edition";
+  const $halp = $(
+    "<div><p>ReSpec ondersteunt het productieproces van Standaardisatiedocumenten.</p></div>"
+  );
+  $(
+    "<p>Meer informatie over de tool leest u in de <a href='https://w3.org/respec/'>documentatie</a>.</p>"
+  ).appendTo($halp);
+  $(
+    "<p>Een issue gevonden in ReSpec? <a href='https://github.com/w3c/respec/issues'>Meld het</a>!</p>"
+  ).appendTo($halp);
+  const button = ui.addCommand(
+    "Over ReSpec",
+    "geonovum/ui/about-respec",
+    "Ctrl+Shift+Alt+A",
+    "ℹ️"
+  );
+  return {
+    show: function() {
+      ui.freshModal("About ReSpec - " + window.respecVersion, $halp, button);
+    },
+  };
+});
--- js/geonovum/ui/dfn-list.js
+++ js/geonovum/ui/dfn-list.js
@@ -1,13 +1,18 @@
-// Module ui/dfn-list
+// Module geonovum/ui/dfn-list
 // Displays all definitions with links to the defining element.
-define(
-  ["core/ui"],
-  function(ui) {
-    const button = ui.addCommand("Definitie Lijst", "geonovum/ui/dfn-list", "Ctrl+Shift+Alt+D", "📔");
-    return {
-      show: function() {
-        var $halp = $("<ul class='respec-dfn-list'></ul>");
-        Object.keys(window.respecConfig.definitionMap).sort().forEach(function(title) {
+define(["core/ui"], function(ui) {
+  const button = ui.addCommand(
+    "Definitie Lijst",
+    "geonovum/ui/dfn-list",
+    "Ctrl+Shift+Alt+D",
+    "📔"
+  );
+  return {
+    show: function() {
+      var $halp = $("<ul class='respec-dfn-list'></ul>");
+      Object.keys(window.respecConfig.definitionMap)
+        .sort()
+        .forEach(function(title) {
           window.respecConfig.definitionMap[title].forEach(function(dfn) {
             // Link to definition
             var $link = $("<a>" + title + "</a>")
@@ -15,12 +20,11 @@ define(
               .click(function() {
                 ui.closeModal();
               });
-            ($("<li></li>").append($link)).appendTo($halp);
+            $("<li></li>").append($link).appendTo($halp);
           });
         });
 
-        ui.freshModal("Lijst van Definities", $halp, button);
-      }
-    };
-  }
-);
+      ui.freshModal("Lijst van Definities", $halp, button);
+    },
+  };
+});
