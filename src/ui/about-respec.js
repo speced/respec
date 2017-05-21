795e648de3d1706af8982d7601c529597a23d398
Marcos Cáceres <marcos+github@marcosc.com>
2017-05-21 20:48:02 +1000

    refactor(ui): es6 + hyperHTML  (#1234)
    * refactor(ui/about-respec): convert to ES6
    * refactor(ui/dfn-list): ES6 + hyperHTML

---
+++ src/ui/about-respec.js
@@ -0,0 +1,31 @@
+// Module ui/about-respec
+// A simple about dialog with pointer to the help
+import ui from "core/ui";
+
+// window.respecVersion is added at build time (see tools/builder.js)
+window.respecVersion = window.respecVersion || "Developer Edition";
+const div = document.createElement("div");
+div.innerHTML = `
+  <p>
+    ReSpec is a document production toolchain, with a notable focus on W3C specifications.
+  </p>
+  <p>
+    You can find more information in the <a href='https://w3.org/respec/'>documentation</a>.
+  </p>
+  <p>
+    Found a bug in ReSpec? <a href='https://github.com/w3c/respec/issues'>File it!</a>.
+  </p>
+`;
+
+const button = ui.addCommand(
+  "About ReSpec",
+  "ui/about-respec",
+  "Ctrl+Shift+Alt+A",
+  "ℹ️"
+);
+
+function  show() {
+  ui.freshModal("About ReSpec - " + window.respecVersion, div, button);
+}
+
+export { show };
