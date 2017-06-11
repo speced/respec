f96aaf45727254b05ee37f3ad32d002951425f94
Marcos Caceres <marcos@marcosc.com>
2017-06-11 18:35:48 +1000

    WIP: experiment running on OSX

--- karma.conf.js
+++ karma.conf.js
@@ -119,9 +119,9 @@ module.exports = function(config) {
     options.detectBrowsers.enabled = false;
     options.autoWatch = false;
     options.singleRun = true;
-    options.concurrency = 3;
+    options.concurrency = 2;
     options.reporters = ["mocha"];
-    options.browsers = ["chrome_canary_travis", "Safari", "Firefox"]; //"FirefoxNightly"
+    options.browsers = ["chrome_canary_travis", "Firefox"]; //"FirefoxNightly"
   }
   config.set(options);
 };
