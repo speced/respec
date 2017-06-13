94f7b4d99cb44a5a9b75b912ec98851c771e23c6
Marcos Caceres <marcos@marcosc.com>
2017-06-13 14:05:24 +1000

    refactor: remove fetch poly

--- tools/copydeps.js
+++ tools/copydeps.js
@@ -25,7 +25,6 @@ const srcDesMap = new Map([
   ["./node_modules/text/text.js", "./js/deps/"],
   ["./node_modules/url-search-params/build/url-search-params.js", "./js/deps/"],
   ["./node_modules/webidl2/lib/webidl2.js", "./js/deps/"],
-  ["./node_modules/whatwg-fetch/fetch.js", "./js/deps/"],
 ]);
 
 function makePathResolver(base) {
