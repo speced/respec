b39f9af5e204bfba6188fcf17506aacc6be673d0
Marcos Caceres <marcos@marcosc.com>
2017-07-19 19:48:58 +1000

    refactor(geonovum/leafletfigures): hide images, remove divs on save

--- src/geonovum/leafletfigures.js
+++ src/geonovum/leafletfigures.js
@@ -11,9 +11,17 @@ export const name = "geonovum/leafletfigures";
 export async function run(conf, doc, cb) {
   cb();
   await document.respecIsReady;
-  Array.from(doc.querySelectorAll("figure.scalable img")).forEach(image => {
+  processImages();
+}
+
+function processImages() {
+  Array.from(
+    document.querySelectorAll("figure.scalable img")
+  ).forEach(image => {
     const { width, height, src } = image;
+    image.hidden = true;
     const div = document.createElement("div");
+    div.classList.add("removeOnSave");
     const map = L.map(div, {
       maxZoom: 4,
       minZoom: -4,
@@ -21,7 +29,7 @@ export async function run(conf, doc, cb) {
       crs: L.CRS.Simple,
     });
     const imageBounds = [[0, 0], [height, width]];
-    image.parentElement.replaceChild(div, image);
+    image.insertAdjacentElement("beforebegin", div);
     map.setView([height / 2, width / 2], 1);
     [
       L.easyButton("fa-arrows-alt", () => window.open(src, "_blank")),
