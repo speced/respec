eecdd5e089cdf3831eee813d30f8d3c704eca34b
Marcos Caceres <marcos@marcosc.com>
2017-07-27 23:46:27 +1000

    fix(core/aria): reduce chance of race condition

--- src/core/aria.js
+++ src/core/aria.js
@@ -2,23 +2,15 @@ import { sub } from "core/pubsubhub";
 
 export const name = "core/aria";
 
-if ("body" in document) {
-  document.body.setAttribute("aria-busy", "true");
-} else {
-  document.addEventListener(
-    "DOMContentLoaded",
+export function run(conf, doc, cb) {
+  doc.body.setAttribute("aria-busy", "true");
+  sub(
+    "end-all",
     () => {
-      document.body.setAttribute("aria-busy", "true");
+      doc.body.setAttribute("aria-busy", "false");
+      doc.body.hidden = false;
     },
     { once: true }
   );
+  cb();
 }
-
-sub(
-  "end-all",
-  () => {
-    document.body.setAttribute("aria-busy", "false");
-    document.body.hidden = false;
-  },
-  { once: true }
-);
