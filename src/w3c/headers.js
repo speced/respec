speced/respec

commit 0b706ce8ce3b524ba442bedd154dfb056503e131

fix(core/headers): SoTD doesn't respect sections
* closes #1236

src/w3c/headers.js
@@ -104,9 +104,7 @@ import { pub } from "core/pubsubhub";
 import tmpls from "templates";
 
 const cgbgHeadersTmpl = tmpls["cgbg-headers.html"];
-const cgbgSotdTmpl = tmpls["cgbg-sotd.html"];
 const headersTmpl = tmpls["headers.html"];
-const sotdTmpl = tmpls["sotd.html"];
 
 hb.registerHelper("showPeople", function(name, items) {
   // stuff to handle RDFa
@@ -609,14 +607,16 @@ export function run(conf, doc, cb) {
   $("body", doc).prepend($(bp)).addClass("h-entry");
 
   // handle SotD
-  var $sotd = $("#sotd");
-  if ((conf.isCGBG || !conf.isNoTrack || conf.isTagFinding) && !$sotd.length)
+  var sotd =
+    document.body.querySelector("#sotd") || document.createElement("section");
+  if ((conf.isCGBG || !conf.isNoTrack || conf.isTagFinding) && !sotd.id) {
     pub(
       "error",
       "A custom SotD paragraph is required for your type of document."
     );
-  conf.sotdCustomParagraph = $sotd.html();
-  $sotd.remove();
+  }
+  sotd.id = sotd.id || "stod";
+  sotd.classList.add("introductory");
   // NOTE:
   //  When arrays, wg and wgURI have to be the same length (and in the same order).
   //  Technically wgURI could be longer but the rest is ignored.
@@ -627,17 +627,14 @@ export function run(conf, doc, cb) {
   //  can be shorter — but it still needs to be an array.
   var wgPotentialArray = [conf.wg, conf.wgURI, conf.wgPatentURI];
   if (
-    wgPotentialArray.some(function(it) {
-      return $.isArray(it);
-    }) &&
-    wgPotentialArray.some(function(it) {
-      return !$.isArray(it);
-    })
-  )
+    wgPotentialArray.some(item => Array.isArray(item)) &&
+    wgPotentialArray.every(item => Array.isArray(item))
+  ) {
     pub(
       "error",
       "If one of 'wg', 'wgURI', or 'wgPatentURI' is an array, they all have to be."
     );
+  }
   if ($.isArray(conf.wg)) {
     conf.multipleWGs = conf.wg.length > 1;
     conf.wgHTML = joinAnd(conf.wg, function(wg, idx) {
@@ -686,10 +683,8 @@ export function run(conf, doc, cb) {
   // ensure subjectPrefix is encoded before using template
   if (conf.subjectPrefix !== "")
     conf.subjectPrefixEnc = encodeURIComponent(conf.subjectPrefix);
-  var sotd;
-  if (conf.isCGBG) sotd = cgbgSotdTmpl(conf);
-  else sotd = sotdTmpl(conf);
-  if (sotd) $(sotd).insertAfter($("#abstract"));
+
+  sotd.innerHTML = populateSoTD(conf, sotd);
 
   if (!conf.implementationReportURI && (conf.isCR || conf.isPR || conf.isRec)) {
     pub(
@@ -697,12 +692,35 @@ export function run(conf, doc, cb) {
       "CR, PR, and REC documents need to have an implementationReportURI defined."
     );
   }
-  if (conf.isTagFinding && !conf.sotdCustomParagraph) {
+  if (conf.isTagFinding && !conf.additionalContent) {
     pub(
-      "error",
+      "warn",
       "ReSpec does not support automated SotD generation for TAG findings, " +
-        "please specify one using a <code><section></code> element with ID=sotd."
+        "please add the prerequisite content in the 'sotd' section"
     );
   }
   cb();
+}
+
+function populateSoTD(conf, sotd) {
+  const sotdClone = sotd.cloneNode(true);
+  const additionalNodes = document.createDocumentFragment();
+  const additionalContent = document.createElement("temp");
+  // we collect everything until we hit a section,
+  // that becomes the custom content.
+  while (sotdClone.hasChildNodes()) {
+    if (
+      sotdClone.firstChild.nodeType !== Node.ELEMENT_NODE ||
+      sotdClone.firstChild.localName !== "section"
+    ) {
+      additionalNodes.appendChild(sotdClone.firstChild);
+      continue;
+    }
+    break;
+  }
+  additionalContent.appendChild(additionalNodes);
+  conf.additionalContent = additionalContent.innerHTML;
+  // Whatever sections are left, we throw at the end.
+  conf.additionalSections = sotdClone.innerHTML;
+  return tmpls[conf.isCGBG ? "cgbg-sotd.html" : "sotd.html"](conf);
 }

js/w3c/templates/cgbg-sotd.html
@@ -1,32 +1,32 @@
-<section id='sotd' class='introductory'><h2>{{l10n.sotd}}</h2>
-  <p>
-    This specification was published by the <a href='{{wgURI}}'>{{wg}}</a>.
-    It is not a W3C Standard nor is it on the W3C Standards Track.
-    {{#if isCGFinal}}
-      Please note that under the 
-      <a href="https://www.w3.org/community/about/agreements/final/">W3C Community Final Specification Agreement (FSA)</a> 
-      other conditions apply.
-    {{else}}
-      Please note that under the 
-      <a href="https://www.w3.org/community/about/agreements/cla/">W3C Community Contributor License Agreement (CLA)</a>
-      there is a limited opt-out and other conditions apply.
-    {{/if}}
-    Learn more about 
-    <a href="https://www.w3.org/community/">W3C Community and Business Groups</a>.
-  </p>
-  {{#unless sotdAfterWGinfo}}
-  {{{sotdCustomParagraph}}}
-  {{/unless}}
-    {{#if wgPublicList}}
-      <p>If you wish to make comments regarding this document, please send them to 
-      <a href='mailto:{{wgPublicList}}@w3.org{{#if subjectPrefix}}?subject={{subjectPrefixEnc}}{{/if}}'>{{wgPublicList}}@w3.org</a> 
-      (<a href='mailto:{{wgPublicList}}-request@w3.org?subject=subscribe'>subscribe</a>,
-      <a
-        href='https://lists.w3.org/Archives/Public/{{wgPublicList}}/'>archives</a>){{#if subjectPrefix}}
-      with <code>{{subjectPrefix}}</code> at the start of your
-      email's subject{{/if}}.</p>
-    {{/if}}
-  {{#if sotdAfterWGinfo}}
-  {{{sotdCustomParagraph}}}
+<h2>{{l10n.sotd}}</h2>
+<p>
+  This specification was published by the <a href='{{wgURI}}'>{{wg}}</a>.
+  It is not a W3C Standard nor is it on the W3C Standards Track.
+  {{#if isCGFinal}}
+    Please note that under the
+    <a href="https://www.w3.org/community/about/agreements/final/">W3C Community Final Specification Agreement (FSA)</a>
+    other conditions apply.
+  {{else}}
+    Please note that under the
+    <a href="https://www.w3.org/community/about/agreements/cla/">W3C Community Contributor License Agreement (CLA)</a>
+    there is a limited opt-out and other conditions apply.
   {{/if}}
-</section>
+  Learn more about
+  <a href="https://www.w3.org/community/">W3C Community and Business Groups</a>.
+</p>
+{{#unless sotdAfterWGinfo}}
+  {{{additionalContent}}}
+{{/unless}}
+  {{#if wgPublicList}}
+    <p>If you wish to make comments regarding this document, please send them to
+    <a href='mailto:{{wgPublicList}}@w3.org{{#if subjectPrefix}}?subject={{subjectPrefixEnc}}{{/if}}'>{{wgPublicList}}@w3.org</a>
+    (<a href='mailto:{{wgPublicList}}-request@w3.org?subject=subscribe'>subscribe</a>,
+    <a
+      href='https://lists.w3.org/Archives/Public/{{wgPublicList}}/'>archives</a>){{#if subjectPrefix}}
+    with <code>{{subjectPrefix}}</code> at the start of your
+    email's subject{{/if}}.</p>
+  {{/if}}
+{{#if sotdAfterWGinfo}}
+  {{{additionalContent}}}
+{{/if}}
+{{{additionalSections}}}

js/w3c/templates/sotd.html
@@ -1,169 +1,168 @@
-<section id='sotd' class='introductory'><h2>{{l10n.sotd}}</h2>
-  {{#if isUnofficial}}
-    <p>
-      This document is merely a public working draft of a potential specification. It has
-      no official standing of any kind and does not represent the support or consensus of any
-      standards organisation.
-    </p>
-    {{{sotdCustomParagraph}}}
+<h2>{{l10n.sotd}}</h2>
+{{#if isUnofficial}}
+  <p>
+    This document is draft of a potential specification. It has no official standing of
+    any kind and does not represent the support or consensus of any standards organisation.
+  </p>
+  {{{additionalContent}}}
+{{else}}
+  {{#if isTagFinding}}
+    {{{additionalContent}}}
   {{else}}
-    {{#if isTagFinding}}
-      {{{sotdCustomParagraph}}}
+    {{#if isNoTrack}}
+      <p>
+        This document is merely a W3C-internal {{#if isMO}}member-confidential{{/if}} document. It
+        has no official standing of any kind and does not represent consensus of the W3C
+        Membership.
+      </p>
+      {{{additionalContent}}}
     {{else}}
-      {{#if isNoTrack}}
-        <p>
-          This document is merely a W3C-internal {{#if isMO}}member-confidential{{/if}} document. It
-          has no official standing of any kind and does not represent consensus of the W3C
-          Membership.
-        </p>
-        {{{sotdCustomParagraph}}}
-      {{else}}
-        <p>
-          <em>{{{l10n.status_at_publication}}}</em>
-        </p>
-        {{#if isSubmission}}
-          {{{sotdCustomParagraph}}}
-          {{#if isMemberSubmission}}
-            <p>By publishing this document, W3C acknowledges that the <a href="https://www.w3.org/Submission/@@@submissiondoc@@@">Submitting Members</a> have made a formal Submission request to W3C for discussion. Publication of this document by W3C indicates no endorsement of its content by W3C, nor that W3C has, is, or will be allocating any resources to the issues addressed by it. This document is not the product of a chartered W3C group, but is published as potential input to the <a href="https://www.w3.org/Consortium/Process">W3C Process</a>. A <a href="https://www.w3.org/Submission/@@@teamcomment@@@">W3C Team Comment</a> has been published in conjunction with this Member Submission. Publication of acknowledged Member Submissions at the W3C site is one of the benefits of <a href="https://www.w3.org/Consortium/Prospectus/Joining">W3C Membership</a>. Please consult the requirements associated with Member Submissions of <a href="https://www.w3.org/Consortium/Patent-Policy-20040205/#sec-submissions">section 3.3 of the W3C Patent Policy</a>. Please consult the complete <a href="https://www.w3.org/Submission">list of acknowledged W3C Member Submissions</a>.</p>
-          {{else}}
-            {{#if isTeamSubmission}}
-              <p>If you wish to make comments regarding this document, please send them to
-              <a href='mailto:{{wgPublicList}}@w3.org{{#if subjectPrefix}}?subject={{subjectPrefixEnc}}{{/if}}'>{{wgPublicList}}@w3.org</a>
-              (<a href='mailto:{{wgPublicList}}-request@w3.org?subject=subscribe'>subscribe</a>,
-              <a
-                href='https://lists.w3.org/Archives/Public/{{wgPublicList}}/'>archives</a>){{#if subjectPrefix}}
-                with <code>{{subjectPrefix}}</code> at the start of your email's subject{{/if}}.</p>
-              <p>Please consult the complete <a href="https://www.w3.org/TeamSubmission/">list of Team Submissions</a>.</p>
-            {{/if}}
-          {{/if}}
+      <p>
+        <em>{{{l10n.status_at_publication}}}</em>
+      </p>
+      {{#if isSubmission}}
+        {{{additionalContent}}}
+        {{#if isMemberSubmission}}
+          <p>By publishing this document, W3C acknowledges that the <a href="https://www.w3.org/Submission/@@@submissiondoc@@@">Submitting Members</a> have made a formal Submission request to W3C for discussion. Publication of this document by W3C indicates no endorsement of its content by W3C, nor that W3C has, is, or will be allocating any resources to the issues addressed by it. This document is not the product of a chartered W3C group, but is published as potential input to the <a href="https://www.w3.org/Consortium/Process">W3C Process</a>. A <a href="https://www.w3.org/Submission/@@@teamcomment@@@">W3C Team Comment</a> has been published in conjunction with this Member Submission. Publication of acknowledged Member Submissions at the W3C site is one of the benefits of <a href="https://www.w3.org/Consortium/Prospectus/Joining">W3C Membership</a>. Please consult the requirements associated with Member Submissions of <a href="https://www.w3.org/Consortium/Patent-Policy-20040205/#sec-submissions">section 3.3 of the W3C Patent Policy</a>. Please consult the complete <a href="https://www.w3.org/Submission">list of acknowledged W3C Member Submissions</a>.</p>
         {{else}}
-          {{#unless sotdAfterWGinfo}}
-          {{{sotdCustomParagraph}}}
-          {{/unless}}
-          {{#unless overrideStatus}}
-          <p>
-            This document was published by {{{wgHTML}}} as {{anOrA}} {{longStatus}}.
-            {{#if notYetRec}}
-              This document is intended to become a W3C Recommendation.
-            {{/if}}
-            {{#if wgPublicList}}
-              Comments regarding this document are welcome. Please send them to
-              <a href='mailto:{{wgPublicList}}@w3.org{{#if subjectPrefix}}?subject={{subjectPrefixEnc}}{{/if}}'>{{wgPublicList}}@w3.org</a>
-              (<a href='mailto:{{wgPublicList}}-request@w3.org?subject=subscribe'>subscribe</a>,
-              <a
-                href='https://lists.w3.org/Archives/Public/{{wgPublicList}}/'>archives</a>){{#if subjectPrefix}}
-                with <code>{{subjectPrefix}}</code> at the start of your email's subject{{/if}}.
-            {{/if}}
-            {{#if isCR}}
-              W3C publishes a Candidate Recommendation to indicate that the document is believed to be
-              stable and to encourage implementation by the developer community. This Candidate
-              Recommendation is expected to advance to Proposed Recommendation no earlier than
-              {{humanCREnd}}.
-            {{/if}}
-            {{#if isPER}}
-                W3C Advisory Committee Members are invited to
-                send formal review comments on this Proposed
-                Edited Recommendation to the W3C Team until
-                {{humanPEREnd}}.
-                Members of the Advisory Committee will find the
-                appropriate review form for this document by
-                consulting their list of current
-                <a href='https://www.w3.org/2002/09/wbs/myQuestionnaires'>WBS questionnaires</a>.
-            {{/if}}
-            {{#if isPR}}
-                The W3C Membership and other interested parties are invited to review the document and
-                send comments to
-                <a rel='discussion' href='mailto:{{wgPublicList}}@w3.org'>{{wgPublicList}}@w3.org</a>
-                (<a href='mailto:{{wgPublicList}}-request@w3.org?subject=subscribe'>subscribe</a>,
-                <a href='https://lists.w3.org/Archives/Public/{{wgPublicList}}/'>archives</a>)
-                through {{humanPREnd}}. Advisory Committee Representatives should consult their
-                <a href='https://www.w3.org/2002/09/wbs/myQuestionnaires'>WBS questionnaires</a>.
-                Note that substantive technical comments were expected during the Candidate Recommendation
-                review period that ended {{humanCREnd}}.
-            {{else}}
-            {{/if}}
-          </p>
-          {{/unless}}
-          {{#if implementationReportURI}}
-            <p>
-              Please see the Working Group's  <a href='{{implementationReportURI}}'>implementation
-              report</a>.
-            </p>
+          {{#if isTeamSubmission}}
+            <p>If you wish to make comments regarding this document, please send them to
+            <a href='mailto:{{wgPublicList}}@w3.org{{#if subjectPrefix}}?subject={{subjectPrefixEnc}}{{/if}}'>{{wgPublicList}}@w3.org</a>
+            (<a href='mailto:{{wgPublicList}}-request@w3.org?subject=subscribe'>subscribe</a>,
+            <a
+              href='https://lists.w3.org/Archives/Public/{{wgPublicList}}/'>archives</a>){{#if subjectPrefix}}
+              with <code>{{subjectPrefix}}</code> at the start of your email's subject{{/if}}.</p>
+            <p>Please consult the complete <a href="https://www.w3.org/TeamSubmission/">list of Team Submissions</a>.</p>
           {{/if}}
-          {{#if sotdAfterWGinfo}}
-          {{{sotdCustomParagraph}}}
-          {{/if}}
-          {{#if notRec}}
-            <p>
-              Publication as {{anOrA}} {{textStatus}} does not imply endorsement by the W3C
-              Membership. This is a draft document and may be updated, replaced or obsoleted by other
-              documents at any time. It is inappropriate to cite this document as other than work in
-              progress.
-            </p>
-          {{/if}}
-          {{#if isRec}}
-            <p>
-              This document has been reviewed by W3C Members, by software developers, and by other W3C
-              groups and interested parties, and is endorsed by the Director as a W3C Recommendation.
-              It is a stable document and may be used as reference material or cited from another
-              document. W3C's role in making the Recommendation is to draw attention to the
-              specification and to promote its widespread deployment. This enhances the functionality
-              and interoperability of the Web.
-            </p>
-          {{/if}}
-          <p>
-            {{#unless isIGNote}}
-              This document was produced by
-              {{#if multipleWGs}}
-              groups
-              {{else}}
-              a group
-              {{/if}} operating under the
-              <a{{#if doRDFa}} id="sotd_patent" property='w3p:patentRules'{{/if}}
-              href='https://www.w3.org/Consortium/Patent-Policy-20040205/'>5 February 2004 W3C Patent
-              Policy</a>.
-            {{/unless}}
-            {{#if recNotExpected}}
-              The group does not expect this document to become a W3C Recommendation.
-            {{/if}}
-            {{#unless isIGNote}}
-              {{#if multipleWGs}}
-                W3C maintains {{{wgPatentHTML}}}
-              {{else}}
-                W3C maintains a <a href='{{wgPatentURI}}' rel='disclosure'>public list of any patent
-                disclosures</a>
-              {{/if}}
-              made in connection with the deliverables of
-              {{#if multipleWGs}}
-              each group; these pages also include
-              {{else}}
-              the group; that page also includes
-              {{/if}}
-              instructions for disclosing a patent. An individual who has actual knowledge of a patent
-              which the individual believes contains
-              <a href='https://www.w3.org/Consortium/Patent-Policy-20040205/#def-essential'>Essential
-              Claim(s)</a> must disclose the information in accordance with
-              <a href='https://www.w3.org/Consortium/Patent-Policy-20040205/#sec-Disclosure'>section
-              6 of the W3C Patent Policy</a>.
-            {{/unless}}
-            {{#if isIGNote}}
-              The disclosure obligations of the Participants of this group are described in the
-              <a href='{{charterDisclosureURI}}'>charter</a>.
-            {{/if}}
-          </p>
-          {{#if isNewProcess}}
-            <p>This document is governed by the <a id="w3c_process_revision"
-              href="https://www.w3.org/2017/Process-20170301/">1 March 2017 W3C Process Document</a>.
-            </p>
-          {{else}}
-            <p>
-              This document is governed by the  <a id="w3c_process_revision"
-              href="https://www.w3.org/2005/10/Process-20051014/">14 October 2005 W3C Process Document</a>.
-            </p>
-          {{/if}}
-          {{#if addPatentNote}}<p>{{{addPatentNote}}}</p>{{/if}}
         {{/if}}
+      {{else}}
+        {{#unless sotdAfterWGinfo}}
+        {{{additionalContent}}}
+        {{/unless}}
+        {{#unless overrideStatus}}
+        <p>
+          This document was published by {{{wgHTML}}} as {{anOrA}} {{longStatus}}.
+          {{#if notYetRec}}
+            This document is intended to become a W3C Recommendation.
+          {{/if}}
+          {{#if wgPublicList}}
+            Comments regarding this document are welcome. Please send them to
+            <a href='mailto:{{wgPublicList}}@w3.org{{#if subjectPrefix}}?subject={{subjectPrefixEnc}}{{/if}}'>{{wgPublicList}}@w3.org</a>
+            (<a href='mailto:{{wgPublicList}}-request@w3.org?subject=subscribe'>subscribe</a>,
+            <a
+              href='https://lists.w3.org/Archives/Public/{{wgPublicList}}/'>archives</a>){{#if subjectPrefix}}
+              with <code>{{subjectPrefix}}</code> at the start of your email's subject{{/if}}.
+          {{/if}}
+          {{#if isCR}}
+            W3C publishes a Candidate Recommendation to indicate that the document is believed to be
+            stable and to encourage implementation by the developer community. This Candidate
+            Recommendation is expected to advance to Proposed Recommendation no earlier than
+            {{humanCREnd}}.
+          {{/if}}
+          {{#if isPER}}
+              W3C Advisory Committee Members are invited to
+              send formal review comments on this Proposed
+              Edited Recommendation to the W3C Team until
+              {{humanPEREnd}}.
+              Members of the Advisory Committee will find the
+              appropriate review form for this document by
+              consulting their list of current
+              <a href='https://www.w3.org/2002/09/wbs/myQuestionnaires'>WBS questionnaires</a>.
+          {{/if}}
+          {{#if isPR}}
+              The W3C Membership and other interested parties are invited to review the document and
+              send comments to
+              <a rel='discussion' href='mailto:{{wgPublicList}}@w3.org'>{{wgPublicList}}@w3.org</a>
+              (<a href='mailto:{{wgPublicList}}-request@w3.org?subject=subscribe'>subscribe</a>,
+              <a href='https://lists.w3.org/Archives/Public/{{wgPublicList}}/'>archives</a>)
+              through {{humanPREnd}}. Advisory Committee Representatives should consult their
+              <a href='https://www.w3.org/2002/09/wbs/myQuestionnaires'>WBS questionnaires</a>.
+              Note that substantive technical comments were expected during the Candidate Recommendation
+              review period that ended {{humanCREnd}}.
+          {{else}}
+          {{/if}}
+        </p>
+        {{/unless}}
+        {{#if implementationReportURI}}
+          <p>
+            Please see the Working Group's  <a href='{{implementationReportURI}}'>implementation
+            report</a>.
+          </p>
+        {{/if}}
+        {{#if sotdAfterWGinfo}}
+          {{{additionalContent}}}
+        {{/if}}
+        {{#if notRec}}
+          <p>
+            Publication as {{anOrA}} {{textStatus}} does not imply endorsement by the W3C
+            Membership. This is a draft document and may be updated, replaced or obsoleted by other
+            documents at any time. It is inappropriate to cite this document as other than work in
+            progress.
+          </p>
+        {{/if}}
+        {{#if isRec}}
+          <p>
+            This document has been reviewed by W3C Members, by software developers, and by other W3C
+            groups and interested parties, and is endorsed by the Director as a W3C Recommendation.
+            It is a stable document and may be used as reference material or cited from another
+            document. W3C's role in making the Recommendation is to draw attention to the
+            specification and to promote its widespread deployment. This enhances the functionality
+            and interoperability of the Web.
+          </p>
+        {{/if}}
+        <p>
+          {{#unless isIGNote}}
+            This document was produced by
+            {{#if multipleWGs}}
+            groups
+            {{else}}
+            a group
+            {{/if}} operating under the
+            <a{{#if doRDFa}} id="sotd_patent" property='w3p:patentRules'{{/if}}
+            href='https://www.w3.org/Consortium/Patent-Policy-20040205/'>5 February 2004 W3C Patent
+            Policy</a>.
+          {{/unless}}
+          {{#if recNotExpected}}
+            The group does not expect this document to become a W3C Recommendation.
+          {{/if}}
+          {{#unless isIGNote}}
+            {{#if multipleWGs}}
+              W3C maintains {{{wgPatentHTML}}}
+            {{else}}
+              W3C maintains a <a href='{{wgPatentURI}}' rel='disclosure'>public list of any patent
+              disclosures</a>
+            {{/if}}
+            made in connection with the deliverables of
+            {{#if multipleWGs}}
+            each group; these pages also include
+            {{else}}
+            the group; that page also includes
+            {{/if}}
+            instructions for disclosing a patent. An individual who has actual knowledge of a patent
+            which the individual believes contains
+            <a href='https://www.w3.org/Consortium/Patent-Policy-20040205/#def-essential'>Essential
+            Claim(s)</a> must disclose the information in accordance with
+            <a href='https://www.w3.org/Consortium/Patent-Policy-20040205/#sec-Disclosure'>section
+            6 of the W3C Patent Policy</a>.
+          {{/unless}}
+          {{#if isIGNote}}
+            The disclosure obligations of the Participants of this group are described in the
+            <a href='{{charterDisclosureURI}}'>charter</a>.
+          {{/if}}
+        </p>
+        {{#if isNewProcess}}
+          <p>This document is governed by the <a id="w3c_process_revision"
+            href="https://www.w3.org/2017/Process-20170301/">1 March 2017 W3C Process Document</a>.
+          </p>
+        {{else}}
+          <p>
+            This document is governed by the  <a id="w3c_process_revision"
+            href="https://www.w3.org/2005/10/Process-20051014/">14 October 2005 W3C Process Document</a>.
+          </p>
+        {{/if}}
+        {{#if addPatentNote}}<p>{{{addPatentNote}}}</p>{{/if}}
       {{/if}}
     {{/if}}
   {{/if}}
-</section>
+{{/if}}
+{{{additionalSections}}}

src/core/structure.js
@@ -101,9 +101,11 @@ export function run(conf, doc, cb) {
   if ("maxTocLevel" in conf === false) {
     conf.maxTocLevel = 0;
   }
-  var $secs = $("section:not(.introductory)", doc).find(
-    "h1:first, h2:first, h3:first, h4:first, h5:first, h6:first"
-  );
+  var $secs = $("section:not(.introductory)", doc)
+    .find("h1:first, h2:first, h3:first, h4:first, h5:first, h6:first")
+    .toArray()
+    .filter(elem => elem.closest("section.introductory") === null);
+  $secs = $($secs);
   if (!$secs.length) {
     return cb();
   }

tests/spec/w3c/headers-spec.js
@@ -822,6 +822,87 @@ describe("W3C — Headers", function() {
       simpleSpecURL
     ).then(done);
   });
+  it("allows custom sections and custom content, not just paragraphs", done => {
+    var ops = makeStandardOps();
+    ops.body = `
+        <section>
+          <h2>PASS</h2>
+          <p>Normal section.</p>
+        </section>
+        <section id="sotd">
+          <p id="p1">
+            CUSTOM PARAGRAPH 1
+          </p>
+          <p id="p2">
+            CUSTOM PARAGRAPH 2
+          </p><!--
+          comment node
+          -->
+          text node
+          <ol id="ol">
+            <li>item 1</li>
+            <li>item 2</li>
+          </ol>
+          <section id="first-sub-section">
+            <h3>Testing</h3>
+          </section>
+          <p id="p3">
+            This is terrible, but can happen.
+          </p>
+          <section id="last-sub-section">
+            <h2>not in toc...</h2>
+          </section>
+        </section>`;
+    const theTest = doc => {
+      // the class introductory is added by script
+      const sotd = doc.querySelector("#sotd");
+      expect(sotd.classList.contains("introductory")).toBe(true);
+
+      const p1 = sotd.querySelector("#p1");
+      expect(p1).toBeTruthy();
+      expect(p1.textContent.trim()).toEqual("CUSTOM PARAGRAPH 1");
+
+      const p2 = sotd.querySelector("#p2");
+      expect(p2).toBeTruthy();
+      expect(p2.textContent.trim()).toEqual("CUSTOM PARAGRAPH 2");
+
+      const commentNode = p2.nextSibling;
+      expect(commentNode.nodeType).toEqual(Node.COMMENT_NODE);
+
+      const textNode = commentNode.nextSibling;
+      expect(textNode.nodeType).toEqual(Node.TEXT_NODE);
+
+      const ol = sotd.querySelector("#ol");
+      expect(ol).toBeTruthy();
+      expect(ol.querySelectorAll("li").length).toEqual(2);
+
+      const firstSection = sotd.querySelector("#first-sub-section");
+      expect(sotd.lastElementChild).not.toEqual(firstSection);
+
+      const lastSection = sotd.querySelector("#last-sub-section");
+      expect(sotd.lastElementChild).toEqual(lastSection);
+
+      // p3 is sadwiched in between the sections
+      const p3 = sotd.querySelector("#p3");
+      expect(p3).toBeTruthy();
+      expect(p3.previousElementSibling).toEqual(firstSection);
+      expect(p3.nextElementSibling).toEqual(lastSection);
+
+      // There should only be one thing in the ToC
+      expect(doc.querySelectorAll("#toc li").length).toEqual(1);
+      // and it should say "PASS"
+      expect(doc.querySelector("#toc li span").nextSibling.textContent).toEqual(
+        "PASS"
+      );
+    };
+    Promise.all([
+      // Test regular spec using "stod.html"
+      makeRSDoc(ops, theTest),
+      // Test CG spec, using "cg-bg-stod.html"
+      makeRSDoc(Object.assign({}, ops, { specStatus: "CG-DRAFT" })),
+    ]).then(done);
+  });
+
   // See https://github.com/w3c/respec/issues/653
   xit("should state that the spec is destined to become a note", function(
     done

commit 44775a642985101ae958e169d07ee6f2cc47833c

Fix bug in detecting wrong configuration for multiple WGs
Right now, respec complains if all of wg, wgUri, and wgPatentUri are arrays, when it should complain only if some but not all are
Bug was introduced in 0b706ce8ce3b524ba442bedd154dfb056503e131

src/w3c/headers.js
@@ -672,7 +672,7 @@ export function run(conf, doc, cb) {
   var wgPotentialArray = [conf.wg, conf.wgURI, conf.wgPatentURI];
   if (
     wgPotentialArray.some(item => Array.isArray(item)) &&
-    wgPotentialArray.every(item => Array.isArray(item))
+    !wgPotentialArray.every(item => Array.isArray(item))
   ) {
     pub(
       "error",