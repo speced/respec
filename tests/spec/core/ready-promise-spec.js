a64ceea5b4ded1eb4026f0270662853524c89451
Marcos Caceres <marcos@marcosc.com>
2017-08-13 17:23:59 +1000

    refactor(core/ready-promise-spec): ES6fy and simplify

--- tests/spec/core/ready-promise-spec.js
+++ tests/spec/core/ready-promise-spec.js
@@ -1,27 +1,17 @@
 "use strict";
-describe("respecIsReady promise", function() {
-  it("should settle when processing is done", function(done) {
-    var ops = makeStandardOps();
-    makeRSDoc(ops, function(doc) {
-      var props = new Set(Object.getOwnPropertyNames(doc));
-      expect(props.has("respecIsReady")).toBeTruthy();
-      expect(doc.respecIsReady instanceof doc.defaultView.Promise).toBeTruthy();
-      doc.respecIsReady.then(done);
-    });
-  });
+describe("respecIsReady promise", () => {
+  afterAll(flushIframes);
 
-  it("should resolve with the resulting respecConfig", function(done) {
-    var ops = makeStandardOps();
-    makeRSDoc(ops, function(doc) {
-      doc.respecIsReady
-        .then(function(resultingConfig) {
-          // previousPublishDate gets changed to a Date object by ReSpec,
-          // so not worth checking for equality.
-          delete resultingConfig.previousPublishDate;
-          delete ops.config.previousPublishDate;
-          expect(resultingConfig).toEqual(jasmine.objectContaining(ops.config));
-        })
-        .then(done);
-    });
+  it("resolve with the resulting respecConfig", async () => {
+    const ops = makeStandardOps();
+    const doc = await makeRSDoc(ops);
+    expect(doc.hasOwnProperty("respecIsReady")).toBe(true);
+    expect(doc.respecIsReady instanceof doc.defaultView.Promise).toBe(true);
+    const conf = await doc.respecIsReady;
+    // the following get changed to a Date objects by ReSpec,
+    // so not worth checking for equality.
+    delete ops.config.previousPublishDate;
+    delete ops.config.perEnd;
+    expect(conf).toEqual(jasmine.objectContaining(ops.config));
   });
 });
