54eeea85e5164758c56e3c60086b4f61122eca29
Marcos Caceres <marcos@marcosc.com>
2017-08-15 12:29:33 +1000

    Wait for idle moment to insert meta

--- src/core/seo.js
+++ src/core/seo.js
@@ -2,16 +2,33 @@
  * This Module adds a metatag description to the document, based on the
  * first paragraph of the abstract.
  */
+import "deps/regenerator";
+
+export const name = "core/seo";
+
 export async function run(conf, doc, cb) {
-  const metaDescription = doc.querySelector("#abstract p:first-of-type");
-  if (!metaDescription) {
-    return; // no abstract, so abort early
-  }
-  // This is not critical, so let's continue other processing first:
+  // This is not critical, so let's continue other processing first
   cb();
   await doc.respecIsReady;
-  const metaElem = doc.createElement("meta");
-  metaElem.name = "description";
-  metaElem.content = metaDescription.textContent;
-  doc.head.appendChild(metaElem);
+  const firstParagraph = doc.querySelector("#abstract p:first-of-type");
+  if (!firstParagraph) {
+    return; // no abstract, so nothing to do
+  }
+  const insertMetaDescription = makeDescriptionInserter(firstParagraph);
+  if (window.requestIdleCallback) {
+    window.requestIdleCallback(insertMetaDescription);
+  } else {
+    insertMetaDescription();
+  }
+}
+
+function makeDescriptionInserter(firstParagraph) {
+  return () => {
+    // Normalize whitespace: trim, remove new lines, tabs, etc.
+    const content = firstParagraph.textContent.replace(/\s+/, " ").trim();
+    const metaElem = doc.createElement("meta");
+    metaElem.name = "description";
+    metaElem.content = content;
+    doc.head.appendChild(metaElem);
+  };
 }
