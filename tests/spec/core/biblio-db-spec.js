fc78923b039c3353abc8f0c3bf74ac95c52a42aa
Marcos Caceres <marcos@marcosc.com>
2017-08-20 15:28:24 +1000

    refactor(tests): use async/await + es6

--- tests/spec/core/biblio-db-spec.js
+++ tests/spec/core/biblio-db-spec.js
@@ -44,117 +44,123 @@ describe("Core - biblioDB", () => {
       done();
     });
   });
+
   describe("ready getter", () => {
-    it("resolves with a IDB database", done => {
-      biblioDB.ready
-        .then(db => {
-          expect(db instanceof window.IDBDatabase).toBe(true);
-        })
-        .then(done);
+    it("resolves with a IDB database", async () => {
+      const db = await biblioDB.ready;
+      expect(db instanceof window.IDBDatabase).toBe(true);
     });
   });
+
   describe("add() method", () => {
-    it("rejects when adding bad types", done => {
-      biblioDB
-        .add("invalid", "bar")
-        .catch(err => expect(err instanceof TypeError).toBe(true))
-        .then(done);
+    it("rejects when adding bad types", async () => {
+      try {
+        await biblioDB.add("invalid", "bar");
+      } catch (err) {
+        expect(err instanceof TypeError).toBe(true);
+      }
     });
-    it("rejects when adding empty type", done => {
-      biblioDB
-        .add("", "ref")
-        .catch(err => expect(err instanceof TypeError).toBe(true))
-        .then(done);
+
+    it("rejects when adding empty type", async () => {
+      try {
+        await biblioDB.add("", "ref");
+      } catch (err) {
+        expect(err instanceof TypeError).toBe(true);
+      }
     });
-    it("adds single reference", done => {
-      biblioDB
-        .add("reference", {
-          authors: ["Test Author"],
-          href: "https://test/",
-          title: "test spec",
-          status: "Living Standard",
-          publisher: "W3C",
-          id: "test123",
-        })
-        .then(() => biblioDB.has("reference", "test123"))
-        .then(result => expect(result).toBe(true))
-        .then(done);
+
+    it("adds single reference", async () => {
+      await biblioDB.add("reference", {
+        authors: ["Test Author"],
+        href: "https://test/",
+        title: "test spec",
+        status: "Living Standard",
+        publisher: "W3C",
+        id: "test123",
+      });
+      const result = await biblioDB.has("reference", "test123");
+      expect(result).toBe(true);
     });
   });
+
   describe("addAll() method", () => {
-    it("adds both aliases and references", done => {
-      biblioDB
-        .addAll(data)
-        .then(() =>
-          Promise.all([
-            biblioDB.has("alias", "whatwg-dom"),
-            biblioDB.has("reference", "ADD-ALL-TEST"),
-            biblioDB.has("reference", "WHATWG-DOM"),
-          ])
-        )
-        .then(results => expect(results.every(v => v === true)).toBe(true))
-        .then(done);
+    it("adds both aliases and references", async () => {
+      await biblioDB.addAll(data);
+      const results = await Promise.all([
+        biblioDB.has("alias", "whatwg-dom"),
+        biblioDB.has("reference", "ADD-ALL-TEST"),
+        biblioDB.has("reference", "WHATWG-DOM"),
+      ]);
+      expect(results.every(v => v === true)).toBe(true);
     });
   });
+
   describe("get() method", () => {
-    it("rejects when getting invalid type", done => {
-      biblioDB
-        .get("invalid", "bar")
-        .catch(err => expect(err instanceof TypeError).toBe(true))
-        .then(done);
+    it("rejects when getting invalid type", async () => {
+      try {
+        await biblioDB.get("invalid", "bar");
+      } catch (err) {
+        expect(err instanceof TypeError).toBe(true);
+      }
     });
-    it("rejects when id is missing", done => {
-      biblioDB
-        .get("invalid")
-        .catch(err => expect(err instanceof TypeError).toBe(true))
-        .then(done);
+
+    it("rejects when id is missing", async () => {
+      try {
+        await biblioDB.get("invalid");
+      } catch (err) {
+        expect(err instanceof TypeError).toBe(true);
+      }
     });
-    it("retrieves a reference", done => {
-      biblioDB
-        .add("reference", {
-          href: "https://test/",
-          title: "PASS",
-          publisher: "W3C",
-          id: "get-ref-test",
-        })
-        .then(() => biblioDB.get("reference", "get-ref-test"))
-        .then(entry => expect(entry.title).toEqual("PASS"))
-        .then(done);
+
+    it("retrieves a reference", async () => {
+      await biblioDB.add("reference", {
+        href: "https://test/",
+        title: "PASS",
+        publisher: "W3C",
+        id: "get-ref-test",
+      });
+
+      const entry = await biblioDB.get("reference", "get-ref-test");
+      expect(entry.title).toEqual("PASS");
     });
-    it("retrieves an alias", done => {
-      biblioDB
-        .add("alias", {
-          id: "ALIAS-GET-TEST",
-          aliasOf: "PASS",
-        })
-        .then(() => biblioDB.get("alias", "ALIAS-GET-TEST"))
-        .then(entry => expect(entry.aliasOf).toEqual("PASS"))
-        .then(done);
+
+    it("retrieves an alias", async () => {
+      await biblioDB.add("alias", {
+        id: "ALIAS-GET-TEST",
+        aliasOf: "PASS",
+      });
+      const entry = await biblioDB.get("alias", "ALIAS-GET-TEST");
+      expect(entry.aliasOf).toEqual("PASS");
     });
-    it("returns null when it can't find an entry", done => {
-      Promise.all([
+
+    it("returns null when it can't find an entry", async () => {
+      const results = await Promise.all([
         biblioDB.get("reference", "does not exist"),
         biblioDB.get("alias", "does not exist"),
-      ])
-        .then(results => expect(results.every(v => v === null)).toBe(true))
-        .then(done);
+      ]);
+      expect(results.every(v => v === null)).toBe(true);
     });
   });
+
   describe("has() method", () => {
-    it("rejects on invalid type", done => {
-      biblioDB
-        .has("invalid", "bar")
-        .catch(err => expect(err instanceof TypeError).toBe(true))
-        .then(done);
+    it("rejects on invalid type", async () => {
+      try {
+        await biblioDB.has("invalid", "bar");
+      } catch (err) {
+        expect(err instanceof TypeError).toBe(true);
+      }
     });
-    it("rejects when id is missing", done => {
-      biblioDB
-        .has("invalid")
-        .catch(err => expect(err instanceof TypeError).toBe(true))
-        .then(done);
+
+    it("rejects when id is missing", async () => {
+      try {
+        await biblioDB.has("invalid");
+      } catch (err) {
+        expect(err instanceof TypeError).toBe(true);
+      }
     });
-    it("returns true when entries exist", done => {
-      Promise.all([
+
+    it("returns true when entries exist", async () => {
+      await Promise.all([
         biblioDB.add("reference", {
           id: "has-ref-test",
           title: "pass",
@@ -163,93 +169,91 @@ describe("Core - biblioDB", () => {
           id: "has-alias-test",
           aliasOf: "pass",
         }),
-      ])
-        .then(() =>
-          Promise.all([
-            biblioDB.has("reference", "has-ref-test"),
-            biblioDB.has("alias", "has-alias-test"),
-          ])
-        )
-        .then(result => expect(result.every(v => v === true)).toBe(true))
-        .then(done);
+      ]);
+      const results = await Promise.all([
+        biblioDB.has("reference", "has-ref-test"),
+        biblioDB.has("alias", "has-alias-test"),
+      ]);
+      expect(results.every(v => v === true)).toBe(true);
     });
-    it("returns false when entries don't exist", done => {
-      Promise.all([
+
+    it("returns false when entries don't exist", async () => {
+      const results = await Promise.all([
         biblioDB.has("reference", "does not exist"),
         biblioDB.has("alias", "does not exist"),
-      ])
-        .then(result => expect(result.every(v => v === false)).toBe(true))
-        .then(done);
+      ]);
+      expect(results.every(v => v === false)).toBe(true);
     });
   });
+
   describe("isAlias() method", () => {
-    it("rejects if passed a bad id", done => {
+    it("rejects if passed a bad id", async () => {
       var p1 = biblioDB.isAlias();
       var p2 = biblioDB.isAlias(null);
       var p3 = biblioDB.isAlias("");
-      Promise.all([
+      await Promise.all([
         p1.catch(err => expect(err instanceof TypeError).toBe(true)),
         p2.catch(err => expect(err instanceof TypeError).toBe(true)),
         p3.catch(err => expect(err instanceof TypeError).toBe(true)),
-      ]).then(done);
+      ]);
     });
-    it("returns true when it is an alias", done => {
-      biblioDB
-        .add("alias", { aliasOf: "isAlias", id: "test-isAlias-pass" })
-        .then(() => biblioDB.isAlias("test-isAlias-pass"))
-        .then(result => expect(result).toBe(true))
-        .then(done);
+
+    it("returns true when it is an alias", async () => {
+      await biblioDB.add("alias", {
+        aliasOf: "isAlias",
+        id: "test-isAlias-pass",
+      });
+      const result = await biblioDB.isAlias("test-isAlias-pass");
+      expect(result).toBe(true);
     });
-    it("returns false when it is not an alias", done => {
-      biblioDB
-        .isAlias("not an alias")
-        .then(result => expect(result).toBe(false))
-        .then(done);
+
+    it("returns false when it is not an alias", async () => {
+      const result = await biblioDB.isAlias("not an alias");
+      expect(result).toBe(false);
     });
   });
+
   describe("find() method", () => {
-    it("rejects if passed a bad id", done => {
+    it("rejects if passed a bad id", async () => {
       var p1 = biblioDB.find();
       var p2 = biblioDB.find(null);
       var p3 = biblioDB.find("");
-      Promise.all([
+      await Promise.all([
         p1.catch(err => expect(err instanceof TypeError).toBe(true)),
         p2.catch(err => expect(err instanceof TypeError).toBe(true)),
         p3.catch(err => expect(err instanceof TypeError).toBe(true)),
-      ]).then(done);
+      ]);
     });
-    it("finds a references and resolves aliases", done => {
-      biblioDB
-        .addAll(data)
-        .then(() => biblioDB.find("DAHU"))
-        .then(result => expect(result.id).toEqual("DAHUT"))
-        .then(
-          () => biblioDB.find("whatwg-dom") // alias
-        )
-        .then(result => expect(result.title).toEqual("DOM Standard"))
-        .then(done);
+
+    it("finds a references and resolves aliases", async () => {
+      await biblioDB.addAll(data);
+      const r1 = await biblioDB.find("DAHU");
+      expect(r1.id).toEqual("DAHUT");
+      const r2 = await biblioDB.find("whatwg-dom"); // alias
+      expect(r2.title).toEqual("DOM Standard");
     });
   });
+
   describe("resolveAlias() method", () => {
-    it("rejects if passed a bad id", done => {
+    it("rejects if passed a bad id", async () => {
       var p1 = biblioDB.resolveAlias();
       var p2 = biblioDB.resolveAlias(null);
       var p3 = biblioDB.resolveAlias("");
-      Promise.all([
+      await Promise.all([
         p1.catch(err => expect(err instanceof TypeError).toBe(true)),
         p2.catch(err => expect(err instanceof TypeError).toBe(true)),
         p3.catch(err => expect(err instanceof TypeError).toBe(true)),
-      ]).then(done);
+      ]);
     });
-    it("resolves known aliases or return null when alias is unknown", done => {
-      biblioDB
-        .addAll(data)
-        .then(() => biblioDB.resolveAlias("whatwg-dom"))
-        .then(result => expect(result).toEqual("WHATWG-DOM"))
-        .then(() => biblioDB.resolveAlias("does not exist"))
-        .then(result => expect(result).toBe(null))
-        .then(done);
+
+    it("resolves known aliases or return null when alias is unknown", async () => {
+      await biblioDB.addAll(data);
+      const alias = await biblioDB.resolveAlias("whatwg-dom");
+      expect(alias).toEqual("WHATWG-DOM");
+      const noAlias = await biblioDB.resolveAlias("does not exist");
+      expect(noAlias).toBe(null);
     });
   });
+
   describe("close() method", () => {});
 });
--- tests/spec/core/highlight-spec.js
+++ tests/spec/core/highlight-spec.js
@@ -1,13 +1,13 @@
 "use strict";
-describe("Core — Highlight", function() {
+describe("Core — Highlight", () => {
   afterAll(flushIframes);
 
-  it("shouldn't highlight idl blocks", done => {
+  it("shouldn't highlight idl blocks", async () => {
     var ops = {
       config: makeBasicConfig(),
       body:
         makeDefaultBody() +
-          `
+        `
         <section><pre class=idl>
           [Constructor]interface Dahut : Mammal {
             const unsigned short DEXTROGYROUS = 1;
@@ -15,19 +15,18 @@ describe("Core — Highlight", function() {
           };</pre>
         </section>`,
     };
-    makeRSDoc(ops, function(doc) {
-      var pre = doc.querySelector("pre");
-      expect(pre.classList.contains("hljs")).toBeFalsy();
-      expect(pre.querySelectorAll("span[class^=hljs-]").length).toBe(0);
-    }).then(done);
+    const doc = await makeRSDoc(ops);
+    var pre = doc.querySelector("pre");
+    expect(pre.classList.contains("hljs")).toBeFalsy();
+    expect(pre.querySelectorAll("span[class^=hljs-]").length).toBe(0);
   });
 
-  it("should automatically highlight", done => {
+  it("automatically highlights", async () => {
     var ops = {
       config: makeBasicConfig(),
       body:
         makeDefaultBody() +
-          `<section>
+        `<section>
           <pre class=example>
             function foo() {
               alert('foo');
@@ -35,21 +34,20 @@ describe("Core — Highlight", function() {
           </pre>
         </section>`,
     };
-    makeRSDoc(ops, function(doc) {
-      var pre = doc.querySelector("div.example pre");
-      expect(pre.classList.contains("hljs")).toBeTruthy();
-      expect(pre.querySelectorAll("span[class^=hljs-]").length).toBeGreaterThan(
-        0
-      );
-    }).then(done);
+    const doc = await makeRSDoc(ops);
+    var pre = doc.querySelector("div.example pre");
+    expect(pre.classList.contains("hljs")).toBeTruthy();
+    expect(pre.querySelectorAll("span[class^=hljs-]").length).toBeGreaterThan(
+      0
+    );
   });
 
-  it("shouldn't highlight pre elements when told not to", done => {
+  it("shouldn't highlight pre elements when told not to", async () => {
     var ops = {
       config: makeBasicConfig(),
       body:
         makeDefaultBody() +
-          `<section>
+        `<section>
           <pre class='nohighlight example'>
             function foo() {
               alert('foo');
@@ -57,19 +55,18 @@ describe("Core — Highlight", function() {
           </pre>
         </section>`,
     };
-    makeRSDoc(ops, function(doc) {
-      var pre = doc.querySelector("div.example pre");
-      expect(pre.classList.contains("nohighlight")).toBeTruthy();
-      expect(pre.querySelectorAll("span[class^=hljs-]").length).toBe(0);
-    }).then(done);
+    const doc = await makeRSDoc(ops);
+    var pre = doc.querySelector("div.example pre");
+    expect(pre.classList.contains("nohighlight")).toBeTruthy();
+    expect(pre.querySelectorAll("span[class^=hljs-]").length).toBe(0);
   });
 
-  it("should respect the noHighlightCSS by not highlighting anything", done => {
+  it("respects the noHighlightCSS by not highlighting anything", async () => {
     var ops = {
       config: Object.assign(makeBasicConfig(), { noHighlightCSS: true }),
       body:
         makeDefaultBody() +
-          `<section>
+        `<section>
           <pre id="test">
             function foo() {
               alert('foo');
@@ -77,9 +74,8 @@ describe("Core — Highlight", function() {
           </pre>
         </section>`,
     };
-    makeRSDoc(ops, function(doc) {
-      var pre = doc.querySelector("#test");
-      expect(pre.querySelectorAll("span[class^=hljs-]").length).toBe(0);
-    }).then(done);
+    const doc = await makeRSDoc(ops);
+    var pre = doc.querySelector("#test");
+    expect(pre.querySelectorAll("span[class^=hljs-]").length).toBe(0);
   });
 });
