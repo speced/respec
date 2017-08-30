d422fb40233de18db348a94df1ab6e268363b303
dhvenema <DVenema@geonovum.nl>
2017-08-30 12:45:39 +0200

    fix seo-error and add media PDF print

--- src/geonovum/seo.js
+++ src/geonovum/seo.js
@@ -13,42 +13,44 @@ export function run(conf, doc, cb) {
       conf.shortName +
       "/"
     : null;
-  switch (conf.canonicalURI.toLowerCase()) {
-    case "wv":
-      if (conf.edDraftURI) {
-        conf.canonicalURI = new URL(conf.edDraftURI, doc.location).href;
-      } else {
-        pub(
-          "warn",
-          "Canonical URI staat op WV, " +
-            "maar er is geen edDraftURI geconfigureerd"
-        );
-        conf.canonicalURI = null;
-      }
-      break;
-    case "def":
-      if (trLatestUri) {
-        conf.canonicalURI = trLatestUri;
-      } else {
-        pub(
-          "warn",
-          "Canonical URI staat op DEF" +
-            "maar er is geen shortName geconfigureerd"
-        );
-        conf.canonicalURI = null;
-      }
-      break;
-    default:
-      if (conf.canonicalURI) {
-        try {
-          conf.canonicalURI = new URL(conf.canonicalURI, doc.location).href;
-        } catch (err) {
-          pub("warn", "CanonicalURI is an invalid URL: " + err.message);
+  if (conf.canonicalURI) {
+    switch (conf.canonicalURI.toLowerCase()) {
+      case "WV":
+        if (conf.edDraftURI) {
+          conf.canonicalURI = new URL(conf.edDraftURI, doc.location).href;
+        } else {
+          pub(
+            "warn",
+            "Canonical URI staat op WV, " +
+              "maar er is geen edDraftURI geconfigureerd"
+          );
           conf.canonicalURI = null;
         }
-      } else if (trLatestUri) {
-        conf.canonicalURI = trLatestUri;
-      }
+        break;
+      case "DEF":
+        if (trLatestUri) {
+          conf.canonicalURI = trLatestUri;
+        } else {
+          pub(
+            "warn",
+            "Canonical URI staat op DEF" +
+              "maar er is geen shortName geconfigureerd"
+          );
+          conf.canonicalURI = null;
+        }
+        break;
+      default:
+        if (conf.canonicalURI) {
+          try {
+            conf.canonicalURI = new URL(conf.canonicalURI, doc.location).href;
+          } catch (err) {
+            pub("warn", "CanonicalURI is an invalid URL: " + err.message);
+            conf.canonicalURI = null;
+          }
+        } else if (trLatestUri) {
+          conf.canonicalURI = trLatestUri;
+        }
+    }
   }
   if (conf.canonicalURI) {
     var linkElem = doc.createElement("link");
