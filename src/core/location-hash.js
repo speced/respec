91fa7358dd0d3964d58ad93b2ee75e4e7781ba07
Marcos Caceres <marcos@marcosc.com>
2017-10-07 17:22:31 +1100

    feat(core/location-hash): recover from legacy hash

--- src/core/location-hash.js
+++ src/core/location-hash.js
@@ -9,13 +9,31 @@ export function run(conf, doc, cb) {
   // Added message for legacy compat with Aria specs
   // See https://github.com/w3c/respec/issues/793
   pub("start", "core/location-hash");
-  var hash = window.location.hash;
 
+  let hash = "";
+  try {
+    hash = decodeURIComponent(window.location.hash).substr(1);
+  } catch (err) {
+    hash = "";
+  }
   // Only scroll to the hash if the document hasn't been scrolled yet
   // this ensures that a page refresh maintains the scroll position
-  if (hash && !window.pageYOffset) {
-    window.location.hash = "";
-    window.location.hash = hash;
+  if (!hash && !window.pageYOffset) {
+    return cb();
   }
+  // Allow some degree of recovery for legacy fragments format.
+  // See https://github.com/w3c/respec/issues/1353
+  const hasLink = !!doc.getElementById(hash);
+  const isLegacyFrag = /\W/.test(hash);
+  if (!hasLink && isLegacyFrag) {
+    const id = hash
+      .replace(/[\W]+/gim, "-")
+      .replace(/^-+/, "")
+      .replace(/-+$/, "");
+    if (document.getElementById(id)) {
+      hash = id;
+    }
+  }
+  window.location.hash = `#${hash}`;
   cb();
 }
