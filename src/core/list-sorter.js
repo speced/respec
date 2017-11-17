6b2561657291147a760ff690822071d2f544a827
Marcos Cáceres <marcos@marcosc.com>
2017-11-17 13:01:28 +1100

    feat(core/list-sorter): teach ReSpec to sort OL + UL

--- src/core/list-sorter.js
+++ src/core/list-sorter.js
@@ -6,50 +6,92 @@ function makeSorter(direction) {
     return direction === "ascending" ? a.localeCompare(b) : b.localeCompare(a);
   };
 }
+/**
+ * Shallow sort list items in OL, and UL elements.
+ *
+ * @param {HTMLUListElement} elem
+ * @returns {DocumentFragment}
+ */
+export function sortListItems(elem, dir) {
+  const elements = getDirectDescendents(elem, "li");
+  const sortedElements = elements.sort(makeSorter(dir)).reduce((frag, elem) => {
+    frag.appendChild(elem);
+    return frag;
+  }, document.createDocumentFragment());
+  return sortedElements;
+}
 
-function sortDefinitionTerms(dl) {
-  const dir = dl.dataset.sort || "ascending";
-  const sortedElements = Array.from(dl.querySelectorAll("dt"))
-    .sort(makeSorter(dir))
-    .reduce((frag, elem) => {
-      const { nodeType, nodeName } = elem;
-      const children = document.createDocumentFragment();
-      let { nextSibling: next } = elem;
-      while (next) {
-        if (!next.nextSibling) {
-          break;
-        }
-        children.appendChild(next.cloneNode(true));
-        const { nodeType: nextType, nodeName: nextName } = next.nextSibling;
-        const isSameType = nextType === nodeType && nextName === nodeName;
-        if (isSameType) {
-          break;
-        }
-        next = next.nextSibling;
+function getDirectDescendents(elem, wantedDescendentName) {
+  let elements;
+  try {
+    elements = elem.querySelectorAll(`:scope > ${wantedDescendentName}`);
+  } catch (err) {
+    let tempId = "";
+    // We give a temporary id, to overcome lack of ":scope" support in Edge.
+    if (!elem.id) {
+      tempId = `temp-${Math.random()}`;
+      elem.id = tempId;
+    }
+    const query = `#${elem.id} > ${wantedDescendentName}`;
+    elements = elem.parentElement.querySelectorAll(query);
+    if (tempId) {
+      elem.id = "";
+    }
+  }
+  return [...elements];
+}
+
+/**
+ * Shallow sort a definition list based on its definition terms (dt) elements.
+ *
+ * @param {HTMLDListElement} dl
+ * @returns {DocumentFragment}
+ */
+export function sortDefinitionTerms(dl, dir) {
+  const elements = getDirectDescendents(dl, "dt");
+  const sortedElements = elements.sort(makeSorter(dir)).reduce((frag, elem) => {
+    const { nodeType, nodeName } = elem;
+    const children = document.createDocumentFragment();
+    let { nextSibling: next } = elem;
+    while (next) {
+      if (!next.nextSibling) {
+        break;
       }
-      children.prepend(elem.cloneNode(true));
-      frag.appendChild(children);
-      return frag;
-    }, document.createDocumentFragment());
+      children.appendChild(next.cloneNode(true));
+      const { nodeType: nextType, nodeName: nextName } = next.nextSibling;
+      const isSameType = nextType === nodeType && nextName === nodeName;
+      if (isSameType) {
+        break;
+      }
+      next = next.nextSibling;
+    }
+    children.prepend(elem.cloneNode(true));
+    frag.appendChild(children);
+    return frag;
+  }, document.createDocumentFragment());
   return sortedElements;
 }
 
 export function run(conf, doc, cb) {
-  const thingsToSort = Array.from(document.querySelectorAll("[data-sort]"));
-  for (const parent of thingsToSort) {
+  for (const elem of document.querySelectorAll("[data-sort]")) {
     let sortedElems;
-    switch (parent.localName) {
+    const dir = elem.dataset.sort || "ascending";
+    switch (elem.localName) {
       case "dl":
-        sortedElems = sortDefinitionTerms(parent);
+        sortedElems = sortDefinitionTerms(elem, dir);
+        break;
+      case "ol":
+      case "ul":
+        sortedElems = sortListItems(elem, dir);
         break;
       default:
-        pub("warning", `ReSpec can't sort ${parent.localName} elements.`);
+        pub("warning", `ReSpec can't sort ${elem.localName} elements.`);
     }
     if (sortedElems) {
       const range = document.createRange();
-      range.selectNodeContents(parent);
+      range.selectNodeContents(elem);
       range.deleteContents();
-      parent.appendChild(sortedElems);
+      elem.appendChild(sortedElems);
     }
   }
   cb();
