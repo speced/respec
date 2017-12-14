6eebfa5543b5ca5360051ac40e4e9eb3f27c8d61
Marcos Cáceres <marcos@marcosc.com>
2017-12-14 10:15:08 -0600

    refactor(w3c/headers): prettier

--- src/w3c/headers.js
+++ src/w3c/headers.js
@@ -354,7 +354,8 @@ const licenses = {
   "w3c-software-doc": {
     name: "W3C Software and Document Notice and License",
     short: "W3C Software and Document",
-    url: "https://www.w3.org/Consortium/Legal/2015/copyright-software-and-document",
+    url:
+      "https://www.w3.org/Consortium/Legal/2015/copyright-software-and-document",
   },
   "cc-by": {
     name: "Creative Commons Attribution 4.0 International Public License",
@@ -558,12 +559,14 @@ export function run(conf, doc, cb) {
   conf.alternatesHTML =
     conf.alternateFormats &&
     joinAnd(conf.alternateFormats, function(alt) {
-      var optional = alt.hasOwnProperty("lang") && alt.lang
-        ? " hreflang='" + alt.lang + "'"
-        : "";
-      optional += alt.hasOwnProperty("type") && alt.type
-        ? " type='" + alt.type + "'"
-        : "";
+      var optional =
+        alt.hasOwnProperty("lang") && alt.lang
+          ? " hreflang='" + alt.lang + "'"
+          : "";
+      optional +=
+        alt.hasOwnProperty("type") && alt.type
+          ? " type='" + alt.type + "'"
+          : "";
       return (
         "<a rel='alternate' href='" +
         alt.uri +
@@ -662,7 +665,9 @@ export function run(conf, doc, cb) {
   var bp;
   if (conf.isCGBG) bp = cgbgHeadersTmpl(conf);
   else bp = headersTmpl(conf);
-  $("body", doc).prepend($(bp)).addClass("h-entry");
+  $("body", doc)
+    .prepend($(bp))
+    .addClass("h-entry");
 
   // handle SotD
   var sotd =
@@ -745,8 +750,8 @@ export function run(conf, doc, cb) {
   conf.recNotExpected = conf.recNotExpected
     ? true
     : !conf.isRecTrack &&
-        conf.maturity == "WD" &&
-        conf.specStatus !== "FPWD-NOTE";
+      conf.maturity == "WD" &&
+      conf.specStatus !== "FPWD-NOTE";
   if (conf.isIGNote && !conf.charterDisclosureURI)
     pub(
       "error",
