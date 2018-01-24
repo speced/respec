2a83cbb3f8630fdda429d802ecf090ea2fb24cc4
Marcos Cáceres <marcos@marcosc.com>
2018-01-24 12:06:41 +1100

    ally: don't announce body changed

--- src/core/aria.js
+++ src/core/aria.js
@@ -6,13 +6,11 @@ sub(
   "end-all",
   () => {
     document.body.setAttribute("aria-busy", "false");
-    document.body.removeAttribute("aria-live");
   },
   { once: true }
 );
 
 export function run(conf, doc, cb) {
   doc.body.setAttribute("aria-busy", "true");
-  doc.body.setAttribute("aria-live", "polite");
   cb();
 }
