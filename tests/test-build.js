5fd67f917149e4443497b1c3f183ecff474c6926
Kagami Sascha Rosylight <saschaplas@outlook.com>
2018-02-03 12:01:43 +0900

    refactor: Migrate to ES2017 async functions (#1471)
    * migrate to ES2017 async functions
    * remove a remaining require

--- tests/test-build.js
+++ tests/test-build.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env mocha
 
 "use strict";
-const async = require("marcosc-async");
 const colors = require("colors");
 const fsp = require("fs-extra");
 const path = require("path");
@@ -20,11 +19,9 @@ colors.setTheme({
   warn: "yellow",
 });
 
-function checkIfFileExists(filePath) {
-  return async.task(function*() {
-    const stats = yield fsp.lstat(filePath);
-    return stats.isFile();
-  });
+async function checkIfFileExists(filePath) {
+  const stats = await fsp.lstat(filePath);
+  return stats.isFile();
 }
 
 describe("builder (tool)", function() {
@@ -37,22 +34,22 @@ describe("builder (tool)", function() {
   const latest = path.join(__dirname, "../builds/respec-w3c-common.js");
   it(
     "should have built default respec",
-    async(function*() {
-      yield Builder.build({ name: "w3c-common" });
-      expect(yield checkIfFileExists(latest)).to.equal(true);
-      expect(yield checkIfFileExists(latestMap)).to.equal(true);
-    })
+    async() => {
+      await Builder.build({ name: "w3c-common" });
+      expect(await checkIfFileExists(latest)).to.equal(true);
+      expect(await checkIfFileExists(latestMap)).to.equal(true);
+    }
   );
 
   describe("respec-w3c-common.build.js", function() {
     it(
       "should include the link to the sourcemap",
-      async(function*() {
-        var source = yield fsp.readFile(latest, "utf-8");
+      async() => {
+        const source = await fsp.readFile(latest, "utf-8");
         expect(source.search("respec-w3c-common.build.js.map")).to.not.equal(
           -1
         );
-      })
+      }
     );
   });
 });
