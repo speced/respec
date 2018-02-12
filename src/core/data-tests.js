d7585602d3f738a0eeac3014e83c34cdf34a5012
Marcos Cáceres <marcos+github@marcosc.com>
2018-02-12 14:26:31 +1100

    Style cleanup

--- src/core/data-tests.js
+++ src/core/data-tests.js
@@ -23,41 +23,50 @@ export const name = "core/data-tests";
 const lang = defaultLang in l10n ? defaultLang : "en";
 
 function toListItem(href) {
-  
+  const emojiList = document.createElement("span");
   const [testFile] = new URL(href).pathname.split("/").reverse();
-  
   const testParts = testFile.split(".");
-  const [testFileName] = testParts;
-  
-  const isSecureTest = testParts.find(part => part === "https");
-  const isManualTest = testFileName.split(".").join("-").split("-").find(part => part === "manual");
-  
-  let emojiList = document.createElement("span");
+  let [testFileName] = testParts;
 
+  const isSecureTest = testParts.find(part => part === "https");
   if (isSecureTest) {
-    const requiresConnectionEmoji = hyperHTML.bind(document.createElement("span"))` 🔒 `;
-    requiresConnectionEmoji.setAttribute("aria-label","requires a secure connection");
-    
-    testFileName.replace(".https","");
-    emojiList.append(requiresConnectionEmoji)
+    const requiresConnectionEmoji = hyperHTML.bind(
+      document.createElement("span")
+    )` 🔒 `;
+    requiresConnectionEmoji.setAttribute(
+      "aria-label",
+      "requires a secure connection"
+    );
+    requiresConnectionEmoji.setAttribute("title", "Test requires HTTPS");
+    testFileName = testFileName.replace(".https", "");
+    emojiList.append(requiresConnectionEmoji);
   }
-  
-  if(isManualTest) {
-  
-    const manualPerformEmoji = hyperHTML.bind(document.createElement("span"))` 💪 `;
-    manualPerformEmoji.setAttribute("aria-label","the test must be run manually");
-  
-    testFileName.replace("-manual","");
-    emojiList.append(manualPerformEmoji)
+
+  const isManualTest = testFileName
+    .split(".")
+    .join("-")
+    .split("-")
+    .find(part => part === "manual");
+  if (isManualTest) {
+    const manualPerformEmoji = hyperHTML.bind(
+      document.createElement("span")
+    )` 💪 `;
+    manualPerformEmoji.setAttribute(
+      "aria-label",
+      "the test must be run manually"
+    );
+    manualPerformEmoji.setAttribute("title", "Manual test");
+    testFileName = testFileName.replace("-manual", "");
+    emojiList.append(manualPerformEmoji);
   }
-  
+
   const testList = hyperHTML.bind(document.createElement("li"))`
     <a href="${href}">
       ${testFileName}
     </a>
   `;
-  testList.append(emojiList)
-  return testList
+  testList.append(emojiList);
+  return testList;
 }
 
 export function run(conf, doc, cb) {
