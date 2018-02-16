1869a2031d2a6434e02d6ab22d558d8443458f32
Kagami Sascha Rosylight <saschanaz@outlook.com>
2018-02-16 15:26:36 +0900

    further refactoring for show-link

--- src/w3c/templates/show-link.js
+++ src/w3c/templates/show-link.js
@@ -1,34 +1,21 @@
+const html = hyperHTML;
+
 export default link => {
   if (!link.key) {
     return;
   }
-  const html = hyperHTML;
   return html`
-    <dt class="${link.class ? [link.class] : null}">${link.key}:</dt>
-    ${link.data ? html`
-      ${link.data.map(item => html`
-        ${item.value ? html`
-          <dd class="${item.class ? [item.class] : null}">
-            ${item.href ? html`<a href="${item.href}">${item.value}</a>` : ""}
-          </dd>
-        ` : html`
-          ${item.href ? html`
-            <dd><a href="${item.href}">${item.href}</a></dd>
-          ` : ""}
-        `}
-      `)}
-    ` : html`
-      ${link.value ? html`
-        <dd class="${link.class ? [link.class] : null}">
-          ${link.href ? html`<a href="${link.href}">${link.value}</a>` : ""}
-        </dd>
-      ` : html`
-        ${link.href ? html`
-          <dd class="${link.class ? [link.class] : null}">
-            <a href="${link.href}">${link.href}</a>
-          </dd>
-        ` : ""}
-      `}
-    `}
+    <dt class="${link.class ? link.class : null}">${link.key}:</dt>
+    ${link.data ? link.data.map(showLinkData) : showLinkData(link)}
   `;
+}
+
+function showLinkData(data) {
+  return html`
+    <dd class="${data.class ? data.class : null}">
+      ${data.href ? html`
+        <a href="${data.href}">${data.value || data.href}</a>
+      ` : ""}
+    </dd>
+  `
 }
