48455196b49c5732c0531a3ace16628f828ff34f
Kagami Sascha Rosylight <saschanaz@outlook.com>
2018-02-28 22:40:05 +0900

    refactor(core/contrib): use ES2015+

--- src/core/contrib.js
+++ src/core/contrib.js
@@ -10,107 +10,90 @@ import { pub } from "core/pubsubhub";
 export const name = "core/contrib";
 
 function prop(prop) {
-  return function(o) {
-    return o[prop];
-  };
+  return o => o[prop];
 }
 
 function findUsers(...thingsToFind) {
-  var users = {};
-  thingsToFind.forEach(function(things) {
-    things.forEach(function(thing) {
+  const users = new Set();
+  for (const things of thingsToFind) {
+    for (const thing of things) {
       if (thing.user) {
-        users[thing.user.url] = true;
+        users.add(thing.user.url);
       }
-    });
-  });
-  return Object.keys(users);
+    }
+  }
+  return [...users];
 }
 
 function join(things) {
   if (!things.length) {
     return "";
   }
-  things = things.slice(0);
-  var last = things.pop();
-  var length = things.length;
-  if (length === 0) {
+  const length = things.length;
+  const last = things[length - 1];
+  if (length === 1) {
     return last;
   }
-  if (length === 1) {
-    return things[0] + " and " + last;
+  if (length === 2) {
+    return `${things[0]} and ${last}`;
   }
-  return things.join(", ") + ", and " + last;
+  return `${things.join(", ")}, and ${last}`;
 }
 
-function toHTML(urls, editors, element) {
-  return $.when
-    .apply(
-      $,
-      urls.map(function(url) {
-        return ghFetch(url);
-      })
-    )
-    .then(function(...args) {
-      var names = args
-        .map(function(user) {
-          user = user[0];
-          return user.name || user.login;
-        })
-        .filter(function(name) {
-          return editors.indexOf(name) < 0;
-        });
-      names.sort(function(a, b) {
-        return a.toLowerCase().localeCompare(b.toLowerCase());
-      });
-      $(element).html(join(names)).attr("id", null);
-    });
+async function toHTML(urls, editors, element) {
+  const args = await Promise.all(...urls.map(ghFetch));
+  const names = args
+    .map(user => user[0].name || user[0].login)
+    .filter(name => !editors.includes(name))
+    .sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+  element.textContent = join(names);
+  element.id = null;
 }
 
 export async function run(conf, doc, cb) {
-  var $commenters = doc.querySelector("#gh-commenters");
-  var $contributors = doc.querySelector("#gh-contributors");
+  const ghCommenters = doc.getElementById("gh-commenters");
+  const ghContributors = doc.getElementById("gh-contributors");
 
-  if (!$commenters && !$contributors) {
+  if (!ghCommenters && !ghContributors) {
     return cb();
   }
 
   if (!conf.githubAPI) {
-    var elements = [];
-    if ($commenters) elements.push("#" + $commenters.id);
-    if ($contributors) elements.push("#" + $contributors.id);
+    const elements = [];
+    if (ghCommenters) elements.push("#" + ghCommenters.id);
+    if (ghContributors) elements.push("#" + ghContributors.id);
     pub(
       "error",
-      "Requested list of contributors and/or commenters from GitHub (" +
-        elements.join(" and ") +
-        ") but config.githubAPI is not set."
+      `Requested list of contributors and/or commenters from GitHub (${
+        elements.join(" and ")
+      }) but config.githubAPI is not set.`
     );
     cb();
     return;
   }
 
-  ghFetch(conf.githubAPI)
-    .then(function(json) {
-      return $.when(
-        fetchIndex(json.issues_url),
-        fetchIndex(json.issue_comment_url),
-        fetchIndex(json.contributors_url)
-      );
-    })
-    .then(function(issues, comments, contributors) {
-      var editors = respecConfig.editors.map(prop("name"));
-      var commenters = findUsers(issues, comments);
-      contributors = contributors.map(prop("url"));
-      return $.when(
-        toHTML(commenters, editors, $commenters),
-        toHTML(contributors, editors, $contributors)
-      );
-    })
-    .then(cb, function(error) {
-      pub(
-        "error",
-        "Error loading contributors and/or commenters from  Error: " + error
-      );
-      cb();
-    });
+  const json = await ghFetch(conf.githubAPI);
+  const [issues, comments, contributors] = await Promise.all(
+    fetchIndex(json.issues_url),
+    fetchIndex(json.issue_comment_url),
+    fetchIndex(json.contributors_url)
+  );
+  const editors = respecConfig.editors.map(prop("name"));
+  const commenters = findUsers(issues, comments);
+  contributors = contributors.map(prop("url"));
+  try {
+    return Promise.all(
+      toHTML(commenters, editors, ghCommenters),
+      toHTML(contributors, editors, ghContributors)
+    );
+  }
+  catch (error) {
+    pub(
+      "error",
+      "Error loading contributors and/or commenters from  Error: " + error
+    );
+  }
+  finally {
+    cb();
+  }
 }
