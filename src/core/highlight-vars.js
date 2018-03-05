f88b7e10a331a319a5a5888d3afed3c0c72836fa
Sid Vishnoi <sidvishnoi8@gmail.com>
2018-03-05 15:01:28 +0530

    Make changes as per review
    Reduce CSS clutter
    Move `var` style to separate css file
    Clean JS
    Remove highlight styles on export

--- src/core/highlight-vars.js
+++ src/core/highlight-vars.js
@@ -1,13 +1,18 @@
 // Module core/highlight-vars
-// Highlights occurrences of a <var> in an algorithm on click
-// if a <var> is set only once (unused/undeclared), it's highlighted as "var.bug"
+// Highlights occurrences of a <var> within a .algorithm on click
+// if a <var> is set only once (unused/undeclared), it's highlighted with class ".bug"
+
+import hlVars from "deps/text!core/css/var.css";
+const styleElement = document.createElement("style");
+styleElement.textContent = hlVars;
+styleElement.classList.add("removeOnSave");
+document.head.appendChild(styleElement);
 
 export const name = "core/highlight-vars";
 
 export function run(conf, doc, cb) {
-  const vars = [...document.querySelectorAll(".algorithm var")];
-  for (let i = 0, l = vars.length; i < l; ++i) {
-    vars[i].addEventListener("click", highlightVars);
+  for (const elem of document.querySelectorAll(".algorithm var")) {
+    elem.addEventListener("click", highlightVars);
   }
   cb();
 }
@@ -15,8 +20,10 @@ export function run(conf, doc, cb) {
 function highlightVars({ target }) {
   const value = target.innerText;
   const parent = target.closest(".algorithm");
-  const toHightlight = [...parent.querySelectorAll("var")]
-    .filter(el => el.innerText === value);
-  const classToAdd = toHightlight.length === 1 ? "bug" : "active";
-  toHightlight.map(el => el.classList.toggle(classToAdd));
+  [...parent.querySelectorAll("var")]
+    .filter(el => el.innerText === value)
+    .forEach((el, _, {length}) => {
+      const classToAdd = length === 1 ? "bug" : "active";
+      el.classList.toggle(classToAdd);
+    });
 }
