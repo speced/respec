02137cc056f22c32d5c790d24d2a7cd2ee3b29c4
Sid Vishnoi <sidvishnoi8@gmail.com>
2018-03-16 16:34:22 +0530

    Make variable highlight configurable
    Add `beforesave` clean up
    Use Map for HL_COLORS
    Less destructive highlight removal
    Remove `respec-bug`
    Minor fixes

--- src/core/highlight-vars.js
+++ src/core/highlight-vars.js
@@ -1,31 +1,43 @@
 // Module core/highlight-vars
 // Highlights occurrences of a <var> within a section on click
-// if a <var> is set only once (unused/undeclared), it's highlighted with class ".bug"
+// Set `conf.highlightVars = true` to enable
 // removes highlights from <var> if clicked anywhere else
 // all is done while keeping in mind that exported html stays clean (removeHighlight)
 
+import { sub } from "core/pubsubhub";
 import hlVars from "deps/text!core/css/var.css";
-const styleElement = document.createElement("style");
-styleElement.textContent = hlVars;
-styleElement.classList.add("removeOnSave");
-document.head.appendChild(styleElement);
 
 export const name = "core/highlight-vars";
 
 export function run(conf, doc, cb) {
+  if (!conf.highlightVars) {
+    return cb();
+  }
+  const styleElement = document.createElement("style");
+  styleElement.textContent = hlVars;
+  styleElement.classList.add("removeOnSave");
+  document.head.appendChild(styleElement);
+
   document.addEventListener("click", initHighlight);
+
+  // remove highlights, cleanup empty class/style attributes
+  sub("beforesave", (outputDoc) => {
+    [...outputDoc.querySelectorAll("var.respec-active")]
+      .forEach(removeHighlight);
+  });
   cb();
 }
 
 // availability of highlight colors.
-const HL_COLORS = {};
-[ // background[,color]
-  "yellow",
-  "#726012,#fff",
-  "#c78e00",
-  "#3949ab,#fff",
-  "#F720E5",
-].forEach(color => HL_COLORS[color] = true); // all colors are initially available
+const HL_COLORS = new Map([
+  // [ background:String, available:Boolean ]
+  // or [ background,color:String, available:Boolean ]
+  ["yellow", true],
+  ["#726012,#fff", true],
+  ["#c78e00", true],
+  ["#3949ab,#fff", true],
+  ["#F720E5", true],
+]);
 
 function getHighlightColor(target) {
   // return current colors if applicable
@@ -34,40 +46,40 @@ function getHighlightColor(target) {
   if (bg) return `${bg}${col ? `,${col}` : ""}`;
 
   // first color preference
-  if (HL_COLORS["yellow"] === true) return "yellow";
+  if (HL_COLORS.get("yellow") === true) return "yellow";
 
   // otherwise get some other available color
-  return Object.keys(HL_COLORS).filter(c => HL_COLORS[c] === true).pop()
+  return [...HL_COLORS.keys()].find(c => HL_COLORS.get(c) === true)
     || "yellow";
 }
 
 function removeHighlight(el) {
   // done so that only the respec classes are removed
   el.classList.remove("respec-active");
-  el.classList.remove("respec-bug");
-  el.setAttribute("style", "");
-  el.removeAttribute("style");
-  if (!el.classList.value.trim()) el.removeAttribute("class");
+  el.style.removeProperty("--respec-background-color");
+  el.style.removeProperty("--respec-color");
+  // clean up empty class/style attributes so they don't come in export
+  if (!el.getAttribute("style")) el.removeAttribute("style");
+  if (!el.getAttribute("class")) el.removeAttribute("class");
 }
 
 function highlightVars(target) {
-  const innerText = target.innerText;
+  const { innerText } = target;
   const parent = target.closest("section");
   const highlightColor = getHighlightColor(target);
 
-  const highlightVar = (elem, _, { length }) => {
+  function highlightVar(elem) {
     if (elem.classList.contains("respec-active")) {
-      HL_COLORS[highlightColor] = true;
+      HL_COLORS.set(highlightColor, true);
       removeHighlight(elem);
     } else {
-      HL_COLORS[highlightColor] = false;
-      elem.classList.add("respec-active");
-      if (length === 1) elem.classList.add("respec-bug");
       const [ background, color ] = highlightColor.split(",");
+      HL_COLORS.set(highlightColor, false);
+      elem.classList.add("respec-active");
       elem.style.setProperty("--respec-background-color", background);
       if (color) elem.style.setProperty("--respec-color", color);
     }
-  };
+  }
 
   [...parent.querySelectorAll("var")]
     .filter(el => el.innerText === innerText)
@@ -75,9 +87,9 @@ function highlightVars(target) {
 }
 
 function initHighlight({ target }) {
-  if (target.tagName === "VAR") return highlightVars(target);
+  if (target.localName === "var") return highlightVars(target);
   // else, remove highlight
   [...document.querySelectorAll("var.respec-active")].forEach(removeHighlight);
   // make all colors available
-  Object.keys(HL_COLORS).forEach(color => HL_COLORS[color] = true);
+  HL_COLORS.forEach((_, color) => HL_COLORS.set(color, true));
 }
