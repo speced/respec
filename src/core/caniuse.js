efb7b5acb9fcb695644cdd1024a142fc65f66033
Sid Vishnoi <sidvishnoi8@gmail.com>
2018-03-21 20:06:11 +0530

    Update caniuse

--- src/core/caniuse.js
+++ src/core/caniuse.js
@@ -20,28 +20,48 @@ import { pub } from "core/pubsubhub";
 import "deps/hyperhtml";
 import caniuseCss from "deps/text!core/css/caniuse.css";
 
+const BROWSERS = new Map([ // browser name dictionary
+  ["chrome", "Chrome"],
+  ["firefox", "Firefox"],
+  ["ie", "IE"],
+  ["edge", "Edge"],
+  ["android", "Android"],
+  ["safari", "Safari"],
+  ["opera", "Opera"],
+  ["bb", "Blackberry"],
+  ["and_uc", "UC (Android)"],
+  ["and_ff", "Firefox (Android)"],
+]);
+
 export const name = "core/caniuse";
 
-export async function run(conf) {
+export async function run(conf, doc, cb) {
+  cb(); // carry on with critical plugins
   if (!conf.caniuse) {
     return;
   }
-  // Opportunistically insert the style into the head to reduce FOUC.
-  const codeStyle = document.createElement("style");
-  codeStyle.textContent = caniuseCss;
-  document.head.appendChild(codeStyle);
-
   normalizeConf(conf);
   const { caniuse } = conf;
-  const cache = await new IDBCache("respec-caniuse", ["caniuse"]);
-  if (caniuse.feature) {
-    canIUse(
-      caniuse.feature,
-      document.querySelector(".head dl").lastChild,
-      cache,
-      caniuse
-    );
+  if (!caniuse.feature) {
+    return; // no feature to show
   }
+  // Opportunistically insert the style into the head to reduce FOUC.
+  document.head.appendChild(hyperHTML `<style>${caniuseCss}</style>`);
+
+  const parent = document.querySelector(".head dl");
+  parent.appendChild(hyperHTML`
+    <dt class="caniuse-title" id="${`caniuse-${caniuse.feature}`}">
+      Can I Use this API?
+    </dt>`);
+  const placeholder = parent.appendChild(hyperHTML`
+    <dd class="caniuse-stats">fetching data from caniuse.com...</dd>`);
+
+  const cache = new IDBCache("respec-caniuse", ["caniuse"]);
+
+  hyperHTML.bind(placeholder)`${{
+    any: canIUse(caniuse, cache),
+    placeholder: "fetching data from caniuse.com...",
+  }}`;
 }
 
 /**
@@ -60,34 +80,36 @@ function normalizeConf(conf) {
     return;
   }
   if (Array.isArray(conf.caniuse.browsers)) {
-    conf.caniuse.browsers = conf.caniuse.browsers.map(b => b.toLowerCase());
+    conf.caniuse.browsers = conf.caniuse.browsers
+      .map(b => b.toLowerCase())
+      .filter(isValidBrowser);
   } else if (conf.caniuse.browsers !== "ALL") {
     conf.caniuse.browsers = DEFAULTS.browsers;
   }
   if (conf.caniuse.maxAge === undefined) conf.caniuse.maxAge = DEFAULTS.maxAge;
   if (!conf.caniuse.versions) conf.caniuse.versions = DEFAULTS.versions;
+
+  function isValidBrowser(browser) {
+    if (BROWSERS.has(browser)) {
+      return true;
+    }
+    pub("warn", `Ignoring invalid browser "\`${browser}\`" in \`conf.caniuse.browsers\``);
+    return false;
+  }
 }
 
 /**
- * main canIUse function
- * @param  {String} key              which api to look for
- * @param  {Node} refNode            adds table after refNode
- * @param {IDBCache} cache           IDB cache
+ * promises to get content for canIUse table
  * @param {Object} conf              normalized respecConfig.caniuse
  */
-async function canIUse(key, refNode, cache, conf) {
-  const title = hyperHTML`
-    <dt class="caniuse-title" id="${`caniuse-${key}`}">
-      Can I Use this API? (${key})
-    </dt>`;
-
-  const contentPromise = new Promise(async (resolve) => {
+async function canIUse(conf, cache) {
+  return new Promise(async (resolve) => {
     // use data from cache data if valid and render
     try {
-      const cachedStats = await cache.get(key);
+      const cachedStats = await cache.get(conf.feature);
       if (cachedStats
         && new Date() - new Date(cachedStats.cacheTime) < conf.maxAge) {
-        resolve(getTableHtml(key, cachedStats.stats, conf));
+        resolve(createTableHTML(conf.feature, cachedStats.stats, conf));
       }
     } catch (err) {
       console.error(err);
@@ -95,34 +117,26 @@ async function canIUse(key, refNode, cache, conf) {
 
     // otherwise fetch new data, cache and render
     const url = conf.apiURL
-      ? conf.apiURL.replace("{FEATURE}", key)
-      : `https://raw.githubusercontent.com/Fyrd/caniuse/master/features-json/${key}.json`;
+      ? conf.apiURL.replace("{FEATURE}", conf.feature)
+      : `https://raw.githubusercontent.com/Fyrd/caniuse/master/features-json/${conf.feature}.json`;
 
     try {
       const json = await getJson(url);
-      cache.set(key, { stats: json.stats, cacheTime: new Date() })
+      cache.set(conf.feature, { stats: json.stats, cacheTime: new Date() })
         .catch(err => console.error("Failed to cache caniuse data.", err));
-      resolve(getTableHtml(key, json.stats, conf));
+      resolve(createTableHTML(conf.feature, json.stats, conf));
     } catch (err) {
-      const permalink = `http://caniuse.com/#feat=${key}`;
+      const permalink = `http://caniuse.com/#feat=${conf.feature}`;
       resolve(hyperHTML`
         Error [core/caniuse]: ${err.message}.
         <br>Please check directly on <a href="${permalink}">${permalink}</a>.`);
     }
   });
-
-  const content = hyperHTML`
-    <dd class="caniuse-stats">${{
-      any: contentPromise,
-      placeholder: "fetching data from caniuse.com...",
-    }}</dd>`;
-
-  refNode.parentNode.insertBefore(content, refNode.nextSibling);
-  refNode.parentNode.insertBefore(title, refNode.nextSibling);
 }
 
 // TODO: replace with fetch in core/github ?
 async function getJson(url) {
+  await new Promise(resolve => setTimeout(resolve, 4000)); // DEV only. remove before upload
   const response = await window.fetch(url);
   if (!response.ok) {
     if (response.status === 404) {
@@ -136,46 +150,26 @@ async function getJson(url) {
   return await response.json();
 }
 
-const BROWSERS = { // browser name dictionary
-  chrome: "Chrome",
-  firefox: "Firefox",
-  ie: "IE",
-  edge: "Edge",
-  android: "Android",
-  safari: "Safari",
-  opera: "Opera",
-  bb: "Blackberry",
-  and_uc: "UC (Android)",
-  and_ff: "Firefox (Android)",
-};
-
 /**
  * get HTML element for the canIUse support table
  * @param  {String} key       API name
  * @param  {Object} stats     CanIUse API results
  * @param  {Object} conf      respecConfig.caniuse
  */
-function getTableHtml(key, stats, conf) {
+function createTableHTML(key, stats, conf) {
   let browsers = conf.browsers;
   if (conf.browsers === "ALL") {
     browsers = Object.keys(stats);
   }
 
-  const validBrowsers = browsers.filter(b => b in stats);
-  if (validBrowsers.length !== browsers.length) {
-    pub("warn", `Unsupported value(s) in \`conf.caniuse.browsers\`.
-    The following were ignored: \`[${browsers.filter(b => !(b in stats))}]\``);
-  }
-
   // render the support table
   return hyperHTML`
-      ${validBrowsers.map(browser =>
+    ${browsers.map(browser =>
       addBrowser(browser, conf.versions, stats[browser])
     )}
-      <a href="${`http://caniuse.com/#feat=${key}`}"
-        title="Get details at caniuse.com">
-        More info
-      </a>`;
+    <a href="${`http://caniuse.com/#feat=${key}`}"
+      title="Get details at caniuse.com">More info
+    </a>`;
 
   /**
    * add a browser and it's support to table
@@ -200,7 +194,7 @@ function getTableHtml(key, stats, conf) {
     return hyperHTML`
       <ul class="caniuse-browser">
         <li class="${`caniuse-cell ${getSupport(browserVersions[0])}`}">
-          ${BROWSERS[browser] || browser} ${browserVersions[0]}
+          ${BROWSERS.get(browser) || browser} ${browserVersions[0]}
         </li>
         <li class="caniuse-col">
           <ul>
