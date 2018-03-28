4a5d0d0db503a7a50a9f062ee1c450b8207649d7
Soumya Himanish Mohapatra <shmiitian@gmail.com>
2018-03-28 11:32:52 +0530

    new changes

--- src/core/figures.js
+++ src/core/figures.js
@@ -76,7 +76,10 @@ export function run(conf, doc, cb) {
     id = id.substring(1);
     if (figMap[id]) {
       $a.addClass("fig-ref");
-      if ($a.html() === "") $a.append(figMap[id].slice(0, 2).clone());
+      if ($a.html() === "") {
+        const $shortFigDescriptor = figMap[id].slice(0, 2).clone();
+        $a.append($shortFigDescriptor);
+      }
     }
   });
 
