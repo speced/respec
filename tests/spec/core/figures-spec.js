2eace7b2e953a7392b954453ea9e5f2b026a1961
Soumya Himanish Mohapatra <shmiitian@gmail.com>
2018-03-29 07:33:53 +0530

    new new new changes

--- tests/spec/core/figures-spec.js
+++ tests/spec/core/figures-spec.js
@@ -24,38 +24,38 @@ describe("Core - Figures", function() {
   });
 
   it("creates autolinks from the anchor to the figure", async () => {
-    const opsEn = {
+    const ops = {
       config: makeBasicConfig(),
       body:
       makeDefaultBody() +
-      "<figure id='figEn'> <img src='img' alt=''>" +
+      "<figure id='fig'> <img src='img' alt=''>" +
       "<figcaption>test figure caption</figcaption>" +
       "</figure>" +
-      "<a id='anchor-fig-en' href='#figEn'></a>"
+      "<a id='anchor-fig' href='#fig'></a>"
     };
+    const doc = await makeRSDoc(ops);
+    const anchorFig = doc.getElementById("anchor-fig");
+    expect(anchorFig.innerText).toEqual("Figure 1");
+    expect(anchorFig.title).toEqual("test figure caption");
+  });
 
-    const opsJa = {
+  it("localizes the anchor of figure", async () => {
+    const ops = {
       config: makeBasicConfig(),
       htmlAttrs: {
         lang: "ja",
       },
       body:
       makeDefaultBody() +
-      "<figure id='figJa'> <img src='img' alt=''>" +
+      "<figure id='fig'> <img src='img' alt=''>" +
       "<figcaption>漢字と仮名のサイズの示し方</figcaption>" +
       "</figure>" +
-      "<a id='anchor-fig-ja' href='#figJa'></a>"
+      "<a id='anchor-fig' href='#fig'></a>"
     };
-
-    const docEn = await makeRSDoc(opsEn);
-    const anchorFigEn = docEn.getElementById("anchor-fig-en");
-    expect(anchorFigEn.innerText).toEqual("Figure 1");
-    expect(anchorFigEn.title).toEqual("test figure caption");
-
-    const docJa = await makeRSDoc(opsJa);
-    const anchorFigJa = docJa.getElementById("anchor-fig-ja");
-    expect(anchorFigJa.innerText).toEqual("図1");
-    expect(anchorFigJa.title).toEqual("漢字と仮名のサイズの示し方");
+    const doc = await makeRSDoc(ops);
+    const anchorFig = doc.getElementById("anchor-fig");
+    expect(anchorFig.innerText).toEqual("図1");
+    expect(anchorFig.title).toEqual("漢字と仮名のサイズの示し方");
   });
 
   it("generates table of figures", async () => {
