7b8bb7bc7a2235f372acc3a56426b19adb3c7e2a
Sid Vishnoi <sidvishnoi8@gmail.com>
2018-04-02 17:46:00 +0530

    Update tests (remove setTimeout), remove placeholder test

--- tests/spec/core/caniuse-spec.js
+++ tests/spec/core/caniuse-spec.js
@@ -2,17 +2,6 @@
 describe("Core — Can I Use", function () {
   afterAll(flushIframes);
 
-  it("adds proper placeholders", async () => {
-    const ops = makeStandardOps({ caniuse: "FEATURE" });
-    const doc = await makeRSDoc(ops);
-
-    const title = doc.getElementById("caniuse-FEATURE");
-    const placeholder = title.nextElementSibling;
-    expect(title.textContent).toBe("Can I Use this API?");
-    expect(placeholder.textContent).toBe("Fetching data from caniuse.com...");
-    expect(placeholder.localName).toBe("dd");
-  });
-
   it("uses meaningful defaults", async () => {
     const ops = makeStandardOps({ caniuse: "FEATURE" });
     const doc = await makeRSDoc(ops);
@@ -64,7 +53,7 @@ describe("Core — Can I Use", function () {
     // TODO: check for `pub` warnings
   });
 
-  it("shows caniuse.com link on error", async (done) => {
+  it("shows caniuse.com link on error", async () => {
     const ops = makeStandardOps({ caniuse: {
         feature: "FEATURE",
         apiURL: "DOES-NOT-EXIST",
@@ -72,15 +61,12 @@ describe("Core — Can I Use", function () {
     });
     const doc = await makeRSDoc(ops);
 
-    setTimeout(() => {
-      const link = doc.querySelector(".caniuse-stats a");
-      expect(link.textContent).toBe("caniuse.com");
-      expect(link.href).toBe("https://caniuse.com/#feat=FEATURE");
-      done();
-    }, 50);
+    const link = doc.querySelector(".caniuse-stats a");
+    expect(link.textContent).toBe("caniuse.com");
+    expect(link.href).toBe("https://caniuse.com/#feat=FEATURE");
   });
 
-  fit("shows caniuse browser support table", async (done) => {
+  it("shows caniuse browser support table", async () => {
     const ops = makeStandardOps({
       caniuse: {
         feature: "FEATURE",
@@ -91,43 +77,39 @@ describe("Core — Can I Use", function () {
     });
     const doc = await makeRSDoc(ops);
 
-    setTimeout(() => {
-      const stats = doc.querySelector(".caniuse-stats");
+    const stats = doc.querySelector(".caniuse-stats");
 
-      const moreInfoLink = stats.querySelector("a");
-      expect(moreInfoLink.href).toBe("https://caniuse.com/#feat=FEATURE");
-      expect(moreInfoLink.textContent.trim()).toBe("More info");
+    const moreInfoLink = stats.querySelector("a");
+    expect(moreInfoLink.href).toBe("https://caniuse.com/#feat=FEATURE");
+    expect(moreInfoLink.textContent.trim()).toBe("More info");
 
-      const browsers = stats.querySelectorAll("ul.caniuse-browser");
-      expect(browsers.length).toBe(2);
-      const [firefox, chrome] = browsers;
+    const browsers = stats.querySelectorAll("ul.caniuse-browser");
+    expect(browsers.length).toBe(2);
+    const [firefox, chrome] = browsers;
 
-      const chromeVersions = chrome.querySelectorAll("li.caniuse-cell");
-      expect(chromeVersions.length).toBe(3);
+    const chromeVersions = chrome.querySelectorAll("li.caniuse-cell");
+    expect(chromeVersions.length).toBe(3);
 
-      const firefoxVersions = firefox.querySelectorAll("li.caniuse-cell");
-      expect(firefoxVersions.length).toBe(5);
+    const firefoxVersions = firefox.querySelectorAll("li.caniuse-cell");
+    expect(firefoxVersions.length).toBe(5);
 
-      // eslint-disable-next-line no-unused-vars
-      const [label, hiddenVersion, _] = firefoxVersions;
+    // eslint-disable-next-line no-unused-vars
+    const [label, hiddenVersion, _] = firefoxVersions;
 
-      expect(label.textContent.trim()).toBe("Firefox 61");
-      expect(label.classList.value).toBe("caniuse-cell y");
+    expect(label.textContent.trim()).toBe("Firefox 61");
+    expect(label.classList.value).toBe("caniuse-cell y");
 
-      expect(hiddenVersion.textContent.trim()).toBe("60");
-      expect(hiddenVersion.classList.value).toBe("caniuse-cell n d");
+    expect(hiddenVersion.textContent.trim()).toBe("60");
+    expect(hiddenVersion.classList.value).toBe("caniuse-cell n d");
 
-      // test hover effects
-      let style = getComputedStyle(hiddenVersion);
-      expect(style.getPropertyValue("display")).toBe("none");
+    // test hover effects
+    let style = getComputedStyle(hiddenVersion);
+    expect(style.getPropertyValue("display")).toBe("none");
 
-      // add hover class as can't trigger a hover (https://stackoverflow.com/a/17226753/3367669)
+    // add hover class as can't trigger a hover (https://stackoverflow.com/a/17226753/3367669)
 
-      firefox.classList.add("hover");
-      style = getComputedStyle(hiddenVersion);
-      expect(style.getPropertyValue("display")).toBe("block");
-
-      done();
-  }, 50);
+    firefox.classList.add("hover");
+    style = getComputedStyle(hiddenVersion);
+    expect(style.getPropertyValue("display")).toBe("block");
   });
 });
