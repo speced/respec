823bb7de950f21afeb40856b8c6804afcf6822ae
Sid Vishnoi <sidvishnoi8@gmail.com>
2018-05-02 14:10:43 +0530

    Prettier, remove fdescribe, nits

--- tests/spec/core/exporter-spec.js
+++ tests/spec/core/exporter-spec.js
@@ -1,24 +1,23 @@
 "use strict";
 
-fdescribe("Core - exporter", () => {
+describe("Core - exporter", () => {
   afterAll(flushIframes);
 
   async function getExportedDoc(ops) {
-    const rsDocToDataURL = await new Promise(resolve => {
-      require(["core/exporter"], ({rsDocToDataURL: _rsDocToDataURL}) => {
-        resolve(_rsDocToDataURL);
-      });
+    const { rsDocToDataURL } = await new Promise(resolve => {
+      require(["core/exporter"], resolve);
     });
     const doc = await makeRSDoc(ops);
     await doc.respecIsReady;
     const parser = new DOMParser();
-    const docString = decodeURIComponent(rsDocToDataURL("text/html", doc))
-      .replace("data:text/html;charset=utf-8,", "");
+    const docString = decodeURIComponent(
+      rsDocToDataURL("text/html", doc)
+    ).replace("data:text/html;charset=utf-8,", "");
     const exportedDoc = parser.parseFromString(docString, "text/html");
     return exportedDoc;
   }
 
-  it("should remove .removeOnSave elements", async () => {
+  it("removes .removeOnSave elements", async () => {
     const ops = makeStandardOps();
     ops.body = `<div class="removeOnSave" id="this-should-be-removed">this should be removed</div>`;
     const doc = await getExportedDoc(ops);
@@ -27,22 +26,20 @@ fdescribe("Core - exporter", () => {
     expect(doc.querySelectorAll(".removeOnSave").length).toBe(0);
   });
 
-  it("should cleanup hyperHTML comments", async () => {
+  it("cleans up hyperHTML comments", async () => {
     const ops = makeStandardOps();
     ops.body = `<div><!--_hyper: LEAVE;-->PASS<!-- STAY --></div>`;
     const doc = await getExportedDoc(ops);
 
-    const walker = document.createTreeWalker(
-      doc.body,
-      NodeFilter.SHOW_COMMENT,
-    );
+    const walker = document.createTreeWalker(doc.body, NodeFilter.SHOW_COMMENT);
     const comments = [];
     while (walker.nextNode()) {
       comments.push(walker.currentNode);
     }
 
-    const hyperComments = comments.filter(
-      comment => comment.textContent.startsWith("_hyper"));
+    const hyperComments = comments.filter(comment =>
+      comment.textContent.startsWith("_hyper")
+    );
     expect(hyperComments.length).toBe(0);
 
     expect(comments.length).toBe(1);
