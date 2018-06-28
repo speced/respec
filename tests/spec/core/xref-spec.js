0bb0d5f8307352ad0ff87cd58d8bdd5784ab483c
Sid Vishnoi <sidvishnoi8@gmail.com>
2018-06-28 22:55:21 +0530

    Add tests

---
+++ tests/spec/core/xref-spec.js
@@ -0,0 +1,27 @@
+"use strict";
+describe("Core — xref", () => {
+  afterAll(flushIframes);
+
+  const apiURL = new URL(`${window.location.origin}/tests/data/xref.json`);
+
+  it("does nothing if xref is not enabled", async () => {
+    const body = `<a id="external-link">EventHandler</a>`;
+    const ops = makeStandardOps(null, body);
+    const doc = await makeRSDoc(ops);
+
+    const link = doc.getElementById("external-link");
+    expect(link.getAttribute("href")).toBeFalsy();
+  });
+
+  it("adds link to unique <a> terms", async () => {
+    const body = `<a id="external-link">event handler</a>`;
+    const config = { xref: { url: apiURL } };
+    const ops = makeStandardOps(config, body);
+    const doc = await makeRSDoc(ops);
+
+    const link = doc.getElementById("external-link");
+    expect(link.href).toEqual(
+      "https://html.spec.whatwg.org/multipage/webappapis.html#event-handlers"
+    );
+  });
+});
