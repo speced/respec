b7205e30e6cf273cf9061aa2bae8f5d92d56c67d
Kagami Sascha Rosylight <saschanaz@outlook.com>
2018-07-16 22:32:17 +0900

    Firefox 61 supports trimEnd

--- src/core/webidl.js
+++ src/core/webidl.js
@@ -294,7 +294,7 @@ function unindentMarkup(text) {
   if (!text) {
     return text;
   }
-  // TODO: use trimEnd when Edge and Firefox support it
+  // TODO: use trimEnd when Edge supports it
   const lines = text.trimRight().split("\n");
   while (lines.length && !lines[0].trim()) {
     lines.shift();
