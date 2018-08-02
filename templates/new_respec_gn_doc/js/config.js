064ffcdb6d60c5628184c9bb0731dfc83bc5f58e
Thijs Brentjens <thijs@brentjensgeoict.nl>
2018-08-02 14:16:23 +0200

    template uitbeiden: emailComments, license en wat datum/versie opties verder beschrijven

--- templates/new_respec_gn_doc/js/config.js
+++ templates/new_respec_gn_doc/js/config.js
@@ -3,8 +3,25 @@ var respecConfig = {
   specStatus: "GN-WV",
   //voor specType mogelijkheden zie https://github.com/Geonovum/respec/wiki/specType
   specType: "HR",
+  // Standaard emailadres bij een consultatieversie: geo-standaarden@geonovum.nl
+  // Geef bij emailComments een ander emailadres op als dat gewenst is.
+  emailComments: 'geo-standaarden@geonovum.nl',
   //voor pubDomain mogelijkheden zie https://github.com/Geonovum/respec/wiki/pubDomain
   pubDomain: "...",
+  // Voor publishDate zie https://github.com/w3c/respec/wiki/publishDate
+  // Als er geen publishDate is opgegeven, genereert ReSpec de dataum o.b.v. de laatste wijzigingen
+  // Belangrijk: als publishDate niet opgegeven is, wordt bij de link "Laatst gepubliceerde versie" "geen" gezet. Anders wordt een link opgebouwd voor de laatste versie, met het formaat:
+  // https://docs.geostandaarden.nl/{pubDomain}/{shortName}
+  publishDate: "2017-12-22",
+  // Als er een eerder gepubliceerde versie is, geef dan de datum daarvan op
+  previousPublishDate: "2017-05-31",
+  // Zie https://github.com/w3c/respec/wiki/previousMaturity. Dit moet een
+  // Als previousMaturity niet opgegeven wprdt, maar previousPublishDate wel, dan wordt dezelfde status gebruikt als bij specStatus
+  previousMaturity: "GN-VV",
+  // licentie. Kiezen uit: 
+  license: 'cc-by-nd',
+  // logos: [], // Geef een lege array op als er geen Geonovum logo moet staan
+  doJsonLd: true,
   editors: [
     {
       name: "...",
@@ -17,11 +34,11 @@ var respecConfig = {
       companyURL: "http://www.geonovum.nl/",
     },
   ],
-  //shortName = korte naam voor in de url van het document, gebruik alleen letters, cijfers en eventueel '-'
+  // shortName = korte naam voor in de url van het document, gebruik alleen letters, cijfers en eventueel '-'
   shortName: "...",
-  //url van de github repository waar je in werkt
+  // url van de github repository waar je in werkt
   github: "https://github.com/Geonovum/...",
-  //url van de issue lijst in de github repository waar je in werkt
+  // url van de issue lijst in de github repository waar je in werkt
   issueBase: "https://github.com/Geonovum/.../issues/",
   // in localBiblio kun je bibliografische items opnemen. In de tekst kun je dan tussen blokhaken verwijzen naar de korte naam bv [NEN3610].
   localBiblio: {
