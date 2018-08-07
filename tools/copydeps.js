55c749787881c44c8982b9fb457a5fd1cd722b99
Kagami Sascha Rosylight <saschanaz@outlook.com>
2018-08-07 21:04:51 +0900

    use fsp.remove

--- tools/copydeps.js
+++ tools/copydeps.js
@@ -25,35 +25,6 @@ const srcDesMap = new Map([
   ["./node_modules/idb-keyval/dist/idb-keyval-amd.min.js", "./js/deps/idb.js"],
 ]);
 
-function makePathResolver(base) {
-  return file => toFullPath(file, base);
-}
-
-// simulate rm
-async function rm(...files) {
-  for (const file of files) {
-    const fullPath = toFullPath(file);
-    const resolveToThisPath = makePathResolver(fullPath);
-    let lstat;
-    try {
-      lstat = await stat(fullPath);
-    } catch (err) {
-      if (err.code !== "ENOENT") {
-        throw new Error(err.message);
-      }
-      console.warn("File not found: " + fullPath);
-      continue;
-    }
-    if (lstat.isDirectory()) {
-      const innerFiles = await readdir(fullPath);
-      const paths = innerFiles.map(resolveToThisPath);
-      await rm(...paths);
-      continue;
-    }
-    await unlink(fullPath);
-  }
-}
-
 async function cp(source, dest) {
   const baseName = path.basename(source);
   const actualDestination = path.extname(dest)
@@ -72,9 +43,14 @@ async function copyDeps() {
 }
 
 // Delete dependent files
-rm("./js/deps/", "./js/core/css/github.css")
-  .then(copyDeps)
-  .catch(err => {
+(async () => {
+  try {
+    await fsp.remove("./js/deps/");
+    await fsp.remove("./js/core/css/github.css");
+    await copyDeps();
+  }
+  catch (err) {
     console.error(err.stack);
     process.exit(1);
-  });
+  }
+})();
