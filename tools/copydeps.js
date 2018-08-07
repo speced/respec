5ae2e44c57392e626d7a5a71e2ac89af2d42843e
Kagami Sascha Rosylight <saschanaz@outlook.com>
2018-08-07 22:08:07 +0900

    copyDeprecated

--- tools/copydeps.js
+++ tools/copydeps.js
@@ -8,7 +8,6 @@ const fsp = require("fs-extra");
 
 const srcDesMap = [
   ["./node_modules/clipboard/dist/clipboard.js", "./js/deps/"],
-  ["./node_modules/domReady/domReady.js", "./js/deps/"],
   [
     "./node_modules/handlebars/dist/handlebars.runtime.js",
     "./js/deps/handlebars.js",
@@ -25,6 +24,10 @@ const srcDesMap = [
   ["./node_modules/idb-keyval/dist/idb-keyval-amd.min.js", "./js/deps/idb.js"],
 ];
 
+const deprecated = [
+  ["./node_modules/domReady/domReady.js", "Use standard DOMContentLoaded and document.readyState instead."]
+];
+
 async function cp(source, dest) {
   const baseName = path.basename(source);
   const actualDestination = path.extname(dest)
@@ -39,12 +42,25 @@ async function copyDeps() {
   await Promise.all(copyPromises);
 }
 
+async function copyDeprecated() {
+  const promises = deprecated.map(async ([dep, guide]) => {
+    const basename = path.basename(dep, ".js");
+    await cp(dep, `./js/deps/_${basename}.js`);
+
+    const message = `The dependency \`deps/${basename}\` is deprecated. ${guide}`;
+    const wrapper = `define(["deps/_${basename}"], dep => { console.warn("${message}"); return dep; });`;
+    await fsp.writeFile(`./js/deps/${basename}.js`, wrapper);
+  });
+  await Promise.all(promises);
+}
+
 // Delete dependent files
 (async () => {
   try {
     await fsp.remove("./js/deps/");
     await fsp.remove("./js/core/css/github.css");
     await copyDeps();
+    await copyDeprecated();
   }
   catch (err) {
     console.error(err.stack);
