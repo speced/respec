15241f6af7e918fdc0761165770d6bc54022e4da
Thijs Brentjens <thijs@brentjensgeoict.nl>
2018-09-17 13:15:17 +0200

    url-search-params no longer a dependency (see develop brnch of w3c base)

--- js/profile-geonovum.js
+++ js/profile-geonovum.js
@@ -23,7 +23,7 @@ require.config({
     "handlebars.runtime": "deps/handlebars",
     "deps/highlight": "https://www.w3.org/Tools/respec/respec-highlight",
   },
-  deps: ["deps/hyperhtml", "deps/url-search-params", "geonovum/deps/leaflet"],
+  deps: ["deps/hyperhtml", "geonovum/deps/leaflet"],
 });
 
 define(
