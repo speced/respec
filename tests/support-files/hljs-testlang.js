34107ea241d01fd7035de466a4c0a4c04426c88d
Marcos Cáceres <marcos@marcosc.com>
2018-10-03 01:52:26 +1000

    feat(respec-worker): dynamically load highlight.js languages (#1835)

---
+++ tests/support-files/hljs-testlang.js
@@ -0,0 +1,14 @@
+var module = module ? module : {}; // eslint-disable-line
+
+function testLang() {
+  return {
+    aliases: ["test"],
+    keywords: ["funkyFunction"],
+  };
+}
+
+module.exports = function(hljs) {
+  hljs.registerLanguage("funkytest", testLang);
+};
+
+module.exports.definer = testLang;
