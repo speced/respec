da594db0112fd24761f656d0773f6210291320a3
Kagami Sascha Rosylight <saschanaz@outlook.com>
2018-12-15 23:17:22 +0900

    add tests

--- tests/spec/core/dfn-finder-spec.js
+++ tests/spec/core/dfn-finder-spec.js
@@ -23,4 +23,29 @@ describe("Core — Definition finder", () => {
     expect(dfn.dataset.lt).toBe("bar()|bar|foo.bar()|foo.bar");
     expect(dfn.classList.contains("respec-offending-element")).toBeFalsy();
   });
+
+  it("should enumerate operation alternative names", async () => {
+    const bodyText = `
+      <section data-dfn-for="Foo">
+        <h2>Test section</h2>
+        <pre class="idl">
+          [Exposed=Window]
+          interface Foo {
+            void bar();
+            void baz();
+          };
+        </pre>
+        <dfn id="bar">bar</dfn>
+        <dfn id="baz">baz()</dfn>
+      </section>`;
+    const ops = {
+      config: makeBasicConfig(),
+      body: makeDefaultBody() + bodyText,
+    };
+    const doc = await makeRSDoc(ops);
+    const bar = doc.getElementById("bar");
+    expect(bar.dataset.lt).toBe("foo.bar()|foo.bar|bar()|bar");
+    const baz = doc.getElementById("baz");
+    expect(baz.dataset.lt).toBe("foo.baz()|foo.baz|baz()|baz");
+  });
 });
