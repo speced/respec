b424b5d46e6828696982c41159a7c61a83da747a
Kagami Sascha Rosylight <saschanaz@outlook.com>
2018-12-22 19:29:15 +0900

    test: type check by `npm run lint`  (#1972)
    * test: type check by `npm run lint`
    
    * install typescript
    
    * remove redundant deps field

---
+++ src/type-helper.d.ts
@@ -0,0 +1,11 @@
+interface Element {
+  /*
+   * This signature helps JSDoc to assert types in a separate line:
+   * / @type {HTMLElement} /
+   * const parent = child.closest(selector);
+   * 
+   * Rather than:
+   * const parent = / @type {HTMLElement} / (child.closest(selector));
+   */
+  closest<T extends Element>(selector: string): T | null;
+}
