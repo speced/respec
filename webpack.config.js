9a9f81af07635498e868544ddcd25c4072ab2ea0
Kagami Sascha Rosylight <saschanaz@outlook.com>
2018-12-25 20:55:54 +0900

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const webpack = require('webpack');
+
+module.exports = {
+  mode: "production",
+  entry: "./js/profile-w3c-common.js",
+  output: {
+    path: path.resolve(__dirname, "builds"),
+    filename: "respec-w3c-common.js"
+  },
+  module: {
+    rules: [
+      {
+        // shortcut.js uses global scope
+        test: require.resolve('./js/shortcut.js'),
+        use: 'exports-loader?shortcut'
+      }
+    ]
+  },
+  resolveLoader: {
+    // to import texts via e.g. "text!./css/webidl.css"
+    alias: { "text": "raw-loader" }
+  },
+  devtool: 'source-map',
+  plugins: [
+    // prevents generating empty 1.respec-w3c-common.js
+    new webpack.optimize.LimitChunkCountPlugin({
+      maxChunks: 1,
+    })
+  ]
+}
