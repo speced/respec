da36d9994a661af5301fbbda6644ba78b22dd0c8
Kagami Sascha Rosylight <saschanaz@outlook.com>
2019-01-01 19:13:43 +0900

    partial fixes in core/utils

--- src/core/utils.js
+++ src/core/utils.js
@@ -127,7 +127,7 @@ export function calculateLeftPad(text) {
  * @param {URL|String} opts.href The URL for the resource or origin.
  * @param {String} [opts.corsMode] Optional, the CORS mode to use (see HTML spec).
  * @param {String} [opts.as] Optional, fetch destination type (see fetchDestinations).
- * @param {Bool} [opts.dontRemove] If the hint should remain in the spec after processing.
+ * @param {boolean} [opts.dontRemove] If the hint should remain in the spec after processing.
  * @return {HTMLLinkElement} A link element ready to use.
  */
 export function createResourceHint(opts) {
@@ -137,7 +137,7 @@ export function createResourceHint(opts) {
   if (!resourceHints.has(opts.hint)) {
     throw new TypeError("Invalid resources hint");
   }
-  const url = new URL(opts.href, document.location);
+  const url = new URL(opts.href.toString(), location.href);
   const linkElem = document.createElement("link");
   let href = url.href;
   linkElem.rel = opts.hint;
@@ -176,6 +176,10 @@ export function normalizePadding(text = "") {
     return "\n";
   }
 
+  /**
+   * @param {Node} node 
+   * @return {node is Text}
+   */
   function isTextNode(node) {
     return node !== null && node.nodeType === Node.TEXT_NODE;
   }
@@ -327,7 +331,7 @@ export function showInlineError(elems, msg, title) {
  * Adds error class to each element while emitting a warning
  * @param {Element} elem
  * @param {String} msg message to show in warning
- * @param {String} title error message to add on each element
+ * @param {String=} title error message to add on each element
  */
 function markAsOffending(elem, msg, title) {
   elem.classList.add("respec-offending-element");
@@ -524,7 +528,7 @@ export function runTransforms(content, flist) {
  * Cached request handler
  * @param {Request} request
  * @param {Object} maxAge cache expiration duration in ms. defaults to 24 hours (86400000 ms)
- * @return {Response}
+ * @return {Promise<Response>}
  *  if a cached response is available and it's not stale, return it
  *  else: request from network, cache and return fresh response.
  *    If network fails, return a stale cached version if exists (else throw)
@@ -568,7 +572,7 @@ export async function fetchAndCache(request, maxAge = 86400000) {
     const clonedResponse = response.clone();
     const customHeaders = new Headers(response.headers);
     const expiryDate = new Date(Date.now() + maxAge);
-    customHeaders.set("Expires", expiryDate);
+    customHeaders.set("Expires", expiryDate.toString());
     const cacheResponse = new Response(await clonedResponse.blob(), {
       headers: customHeaders,
     });
@@ -657,13 +661,12 @@ export function getTextNodes(el, exclusions = []) {
   const nodeIterator = document.createNodeIterator(
     el,
     NodeFilter.SHOW_TEXT,
-    { acceptNode },
-    false
+    { acceptNode }
   );
   const textNodes = [];
   let node;
   while ((node = nodeIterator.nextNode())) {
-    textNodes.push(node);
+    textNodes.push(/** @type {Text} */(node));
   }
   return textNodes;
 }
