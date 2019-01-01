953c8a1db0bbef149da81329bc4e57ffb47c41b9
Swapnilr1 <swapnilrustagi97792@gmail.com>
2019-01-01 18:18:52 +0530

    Support `Assert:` in algorithms

---
+++ src/core/algorithms.js
@@ -0,0 +1,20 @@
+/**
+Currently used only for adding 'assert' class to algorithm lists  
+*/
+ 
+
+export const name = "core/algorithms";
+
+
+export async function run(conf) {
+    const elements = document.querySelectorAll("ol.algorithm");
+    elements.forEach(element => {
+      const children = Array.from(element.children);
+      children.forEach( item => {
+        if (item.innerHTML.startsWith("Assert: "))
+        {
+          item.classList.add("assert");
+        }
+      })
+    });
+}
