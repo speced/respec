6215e24932e982c74df566b444dd759c4c601e15
saurabhmittal16 <saurabhmittal1606@gmail.com>
2019-01-02 18:12:36 +0530

    test for internal-slot-warn lint rule

---
+++ tests/spec/core/linter-rules/internal-slot-warn-spec.js
@@ -0,0 +1,49 @@
+"use strict";
+describe("Core Linter Rule - 'internal-slot-warn'", () => {
+  const ruleName = "internal-slot-warn";
+  const config = {
+    lint: { [ruleName]: true },
+  };
+  let rule;
+  beforeAll(async () => {
+    rule = await new Promise(resolve => {
+      require([`core/linter-rules/${ruleName}`], ({ rule }) => resolve(rule));
+    });
+  });
+  const doc = document.implementation.createHTMLDocument("test doc");
+  beforeEach(() => {
+    // Make sure every unordered test get an empty document
+    // See: https://github.com/w3c/respec/pull/1495
+    while (doc.body.firstChild) {
+      doc.body.removeChild(doc.body.firstChild);
+    }
+  });
+
+  it("return error when no '.' between var and a for internal slots", async () => {
+    doc.body.innerHTML = `
+    <var>bar1</var><a>[[foo]]</a>
+    <var>bar2</var>.<a>[[foo]]</a>
+    <var>bar3</var>.<a>[[foo]]</a>
+    `;
+    const results = await rule.lint(config, doc);
+    expect(results.length).toEqual(1);
+
+    const result = results[0];
+    expect(result.name).toEqual(ruleName);
+    expect(result.occurrences).toEqual(1);
+
+    const offendingElement = result.offendingElements[0];
+    const { previousSibling } = offendingElement;
+    // offending element's previous element won't have '.'
+    expect(previousSibling.textContent).not.toEqual(".");
+  });
+
+  it("no error when correct pattern is followed", async () => {
+    doc.body.innerHTML = `
+      <var>bar</var>.<a>[[foo]]</a>
+      <var>bar</var>.<a>[[foo]]</a>
+    `;
+    const results = await rule.lint(config, doc);
+    expect(results.length).toEqual(0);
+  });
+});
