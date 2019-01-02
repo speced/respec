d91898c21ec626486e2c12e6be0a27a5ce1a428b
Swapnilr1 <swapnilrustagi97792@gmail.com>
2019-01-02 15:46:21 +0530

    Optimized the implementation as suggested

--- src/core/algorithms.js
+++ src/core/algorithms.js
@@ -5,13 +5,8 @@ Currently used only for adding 'assert' class to algorithm lists
 export const name = "core/algorithms";
 
 export async function run() {
-  const elements = document.querySelectorAll("ol.algorithm");
-  elements.forEach(element => {
-    const children = Array.from(element.children);
-    children.forEach(item => {
-      if (item.innerHTML.startsWith("Assert: ")) {
-        item.classList.add("assert");
-      }
-    });
-  });
+  const elements = Array.from(document.querySelectorAll("ol.algorithm li"));
+  elements
+    .filter(li => li.innerText.startsWith("Assert: "))
+    .forEach(li => li.classList.add("assert"));
 }
