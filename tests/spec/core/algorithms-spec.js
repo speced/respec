55405ead958e6884e25ccb3524b10f196076d32c
Marcos Cáceres <marcos@marcosc.com>
2019-01-05 15:02:03 +1100

    Whitespace fixes

--- tests/spec/core/algorithms-spec.js
+++ tests/spec/core/algorithms-spec.js
@@ -7,11 +7,13 @@ describe("Core — Algorithm Lists", () => {
       body:
         makeDefaultBody() +
         `<section>
-            <ol class='algorithm'>
-            <li><!-- whitespace -->    Assert: This one should have assert class added to it.  </li>
+          <ol class='algorithm'>
+            <li>
+              Assert: one.
+            </li>
             <li>This one shouldn't be modified.</li>
-            <li>Assert: This one should have assert class added to it.</li>
-            </ol>
+            <li>Assert: two.</li>
+          </ol>
         </section>`,
     };
     const doc = await makeRSDoc(ops);
@@ -24,14 +26,13 @@ describe("Core — Algorithm Lists", () => {
       body:
         makeDefaultBody() +
         `<section>
-            <ol>
-            <li>Assert: This one is not an algorithm, shouldn't be modified.</li>
-            <li>This one shouldn't be modified.</li>
-            </ol>
+          <ol>
+            <li>Assert: shouldn't be modified.</li>
+            <li>This shouldn't be modified.</li>
+          </ol>
         </section>`,
     };
     const doc = await makeRSDoc(ops);
     expect(doc.querySelectorAll(".assert").length).toEqual(0);
   });
-
 });
