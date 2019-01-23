4016ad91591ce987618a5345bbd9c4d5da454c81
pradeepgangwar <pradeepgangwar39@gmail.com>
2019-01-23 11:19:14 +0530

    Add core/defaults

---
+++ src/core/defaults.js
@@ -0,0 +1,47 @@
+/**
+ * Sets the core defaults
+ */
+export const name = "core/defaults";
+import { rule as checkInternalSlots } from "./linter-rules/check-internal-slots";
+import { rule as checkPunctuation } from "./linter-rules/check-punctuation";
+import linter from "./linter";
+import { rule as localRefsExist } from "./linter-rules/local-refs-exist";
+import { rule as noHeadinglessSectionsRule } from "./linter-rules/no-headingless-sections";
+import { rule as noHttpPropsRule } from "./linter-rules/no-http-props";
+
+linter.register(
+  noHttpPropsRule,
+  noHeadinglessSectionsRule,
+  checkPunctuation,
+  localRefsExist,
+  checkInternalSlots
+);
+
+const coreDefaults = {
+  lint: {
+    "no-headingless-sections": true,
+    "no-http-props": true,
+    "check-punctuation": false,
+    "local-refs-exist": true,
+    "check-internal-slots": false,
+  },
+  pluralize: true,
+  specStatus: "base",
+  highlightVars: true,
+  license: "CC-BY",
+  addSectionLinks: true,
+};
+
+export default coreDefaults;
+
+export function run(conf) {
+  // assign the defaults
+  Object.assign(conf, {
+    ...coreDefaults,
+    ...conf,
+  });
+  Object.assign(conf.lint, {
+    ...coreDefaults.lint,
+    ...conf.lint,
+  });
+}
--- src/w3c/defaults.js
+++ src/w3c/defaults.js
@@ -2,38 +2,18 @@
  * Sets the defaults for W3C specs
  */
 export const name = "w3c/defaults";
-import { rule as checkInternalSlots } from "../core/linter-rules/check-internal-slots";
-import { rule as checkPunctuation } from "../core/linter-rules/check-punctuation";
 import { definitionMap } from "../core/dfn-map";
 import linter from "../core/linter";
-import { rule as localRefsExist } from "../core/linter-rules/local-refs-exist";
-import { rule as noHeadinglessSectionsRule } from "../core/linter-rules/no-headingless-sections";
-import { rule as noHttpPropsRule } from "../core/linter-rules/no-http-props";
 import { rule as privsecSectionRule } from "./linter-rules/privsec-section";
 
-linter.register(
-  noHttpPropsRule,
-  privsecSectionRule,
-  noHeadinglessSectionsRule,
-  checkPunctuation,
-  localRefsExist,
-  checkInternalSlots
-);
+linter.register(privsecSectionRule);
 
 const w3cDefaults = {
   lint: {
-    "no-headingless-sections": true,
     "privsec-section": true,
-    "no-http-props": true,
-    "check-punctuation": false,
-    "local-refs-exist": true,
-    "check-internal-slots": false,
   },
-  pluralize: true,
-  highlightVars: true,
   doJsonLd: false,
   license: "w3c-software-doc",
-  specStatus: "base",
   logos: [
     {
       src: "https://www.w3.org/StyleSheets/TR/2016/logos/W3C",
@@ -43,7 +23,6 @@ const w3cDefaults = {
       url: "https://www.w3.org/",
     },
   ],
-  addSectionLinks: true,
 };
 
 export function run(conf) {
