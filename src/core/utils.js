45edbcdf927c7fd8c4f89ed8082e7441cd0072a4
Johanna <Johanna-hub@users.noreply.github.com>
2019-01-29 10:23:55 +0000

    fix(core/examples): Correct href links (#2039)

--- src/core/utils.js
+++ src/core/utils.js
@@ -671,6 +671,8 @@ export function addId(elem, pfx = "", txt = "", noLC = false) {
 
   if (!id) {
     id = "generatedID";
+  } else if (pfx === "example") {
+    id = txt;
   } else if (/\.$/.test(id) || !/^[a-z]/i.test(id)) {
     id = `x${id}`; // trailing . doesn't play well with jQuery
   }
