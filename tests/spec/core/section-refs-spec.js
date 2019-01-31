36f62373ad21c9cf9919b686698dd8c9d91fad79
Marcos Cáceres <marcos@marcosc.com>
2019-01-31 19:54:37 +1100

    fix(core/structure): trim section titles (#2055)

--- tests/spec/core/section-refs-spec.js
+++ tests/spec/core/section-refs-spec.js
@@ -4,13 +4,16 @@ describe("Core - Section References", () => {
   it("should have produced the section reference", async () => {
     const ops = {
       config: makeBasicConfig(),
-      body: `${makeDefaultBody()}<section id='ONE'><h2>ONE</h2></section><section id='TWO'><a href='#ONE' class='sectionRef'></a></section>`,
+      body: `${makeDefaultBody()}
+        <section id='ONE'>
+          <h2>
+            ONE
+          </h2>
+        </section>
+        <section id='TWO'><a href='#ONE' class='sectionRef'></a></section>`,
     };
     const doc = await makeRSDoc(ops);
-    const one = doc.getElementById("ONE");
     const two = doc.getElementById("TWO");
-    const tit = one.children[0].textContent;
-
-    expect(two.querySelector("a").textContent).toEqual(`section § ${tit}`);
+    expect(two.querySelector("a").textContent).toBe("section § 1. ONE");
   });
 });
