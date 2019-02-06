08ed16cd855f857e3852753d0a188bc34ceff551
CodHeK <gaganganapathyas@yahoo.in>
2019-02-06 12:00:27 +0530

    added linter for checking charset is utf-8

--- src/w3c/defaults.js
+++ src/w3c/defaults.js
@@ -6,6 +6,7 @@ import { coreDefaults } from "../core/defaults";
 import { definitionMap } from "../core/dfn-map";
 import linter from "../core/linter";
 import { rule as privsecSectionRule } from "./linter-rules/privsec-section";
+import { rule as checkCharset } from './linter-rules/check-charset';
 
 linter.register(privsecSectionRule);
 
