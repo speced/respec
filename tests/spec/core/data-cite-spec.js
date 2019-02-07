e5db8c9b9ecf04e3ba840e564341c0a477042aa8
Pradeep Gangwar <pradeepgangwar39@gmail.com>
2019-02-07 10:05:59 +0530

    feat(core/data-cite): Add spec title when expanding <a data-cite='' "> (#2058)

--- tests/spec/core/data-cite-spec.js
+++ tests/spec/core/data-cite-spec.js
@@ -263,6 +263,67 @@ describe("Core — data-cite attribute", () => {
     });
   });
 
+  it("Adds title to a reference when inline-link is empty normative reference", async () => {
+    const ops = {
+      config: makeBasicConfig(),
+      body: `${makeDefaultBody()}
+        <section>
+          <p id="t1"><a data-cite="HTML"></a></p>
+          <p id="t2"><a data-cite="Fetch"></a></p>
+          <p id="t3"><a data-cite="HTML">This should not be replaced</a></p>
+        </section>
+      `,
+    };
+    const doc = await makeRSDoc(ops);
+    let a = doc.querySelector("#t1 > a");
+    expect(a.textContent).toBe("HTML Standard");
+    expect(a.href).toBe("https://html.spec.whatwg.org/multipage/");
+    expect(doc.getElementById("bib-html").closest("section").id).toBe(
+      "normative-references"
+    );
+    a = doc.querySelector("#t2 > a");
+    expect(a.textContent).toBe("Fetch Standard");
+    expect(a.href).toBe("https://fetch.spec.whatwg.org/");
+    expect(doc.getElementById("bib-fetch").closest("section").id).toBe(
+      "normative-references"
+    );
+    a = doc.querySelector("#t3 > a");
+    expect(a.textContent).toBe("This should not be replaced");
+    expect(a.href).toBe("https://html.spec.whatwg.org/multipage/");
+    expect(doc.getElementById("bib-fetch").closest("section").id).toBe(
+      "normative-references"
+    );
+  });
+
+  it("Adds title to a reference when inline-link is empty normative reference in definition", async () => {
+    const ops = {
+      config: makeBasicConfig(),
+      body: `${makeDefaultBody()}
+        <section>
+          <p id="t1"><dfn data-cite="WHATWG-HTML#test"></dfn></p>
+          <p id="t2"><dfn data-cite="WHATWG-HTML#test">This should not change</dfn></p>
+        </section>
+      `,
+    };
+    const doc = await makeRSDoc(ops);
+    let dfn = doc.querySelector("#t1 > dfn");
+    expect(dfn).toBeTruthy();
+    let dfnA = doc.querySelector("#t1 > dfn > a");
+    expect(dfnA.textContent).toBe("HTML Standard");
+    expect(dfnA.href).toBe("https://html.spec.whatwg.org/multipage/#test");
+    expect(doc.getElementById("bib-whatwg-html").closest("section").id).toBe(
+      "normative-references"
+    );
+    dfn = doc.querySelector("#t2 > dfn");
+    expect(dfn).toBeTruthy();
+    dfnA = doc.querySelector("#t2 > dfn > a");
+    expect(dfnA.textContent).toBe("This should not change");
+    expect(dfnA.href).toBe("https://html.spec.whatwg.org/multipage/#test");
+    expect(doc.getElementById("bib-whatwg-html").closest("section").id).toBe(
+      "normative-references"
+    );
+  });
+
   it("does not create external bibliography reference when when external spec id matches its Short Name", async () => {
     const body = `
       <section>
