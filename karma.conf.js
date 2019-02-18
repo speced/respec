34081a79a2bca28a5e8ed1852f50b056637745b9
CodHeK <gaganganapathyas@yahoo.in>
2019-02-18 10:31:45 +0530

    typo fix

--- karma.conf.js
+++ karma.conf.js
@@ -112,7 +112,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: "dots", "progress"
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ["mocha", "progess", "coverage", "coveralls"],
+    reporters: ["mocha", "progress", "coverage", "coveralls"],
 
     // web server port
     port: config.port || 9876,
