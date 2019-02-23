d072a62b4d86d289b88b46217322752df0960b30
kismat <kismat@localhost.localdomain>
2019-02-23 18:46:04 +0100

    replace substr with  toMatch

--- tests/spec/core/biblio-spec.js
+++ tests/spec/core/biblio-spec.js
@@ -225,7 +225,7 @@ it("makes sure references section has expected localization text", async () => {
   const { textContent } = doc.querySelector("#references h2");
   const [normRef, infoRef] = doc.querySelectorAll("#references h3");
   expect(doc.documentElement.lang).toBe("nl");
-  expect(textContent.substr(3)).toBe("Referenties");
-  expect(normRef.textContent.substr(4)).toBe("Normatieve referenties");
-  expect(infoRef.textContent.substr(4)).toBe("Informatieve referenties");
+  expect(textContent).toMatch("Referenties");
+  expect(normRef.textContent).toMatch("Normatieve referenties");
+  expect(infoRef.textContent).toMatch("Informatieve referenties");
 });
