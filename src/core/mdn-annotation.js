cb38897a91f68e770fe8cf82b8c13ac6ce52aa58
EvsChen <chensy1996@sjtu.edu.cn>
2019-02-24 18:16:51 +0800

    feat(core/mdn): enable mdn link

--- src/core/mdn-annotation.js
+++ src/core/mdn-annotation.js
@@ -1,17 +1,18 @@
 import { fetchAndCache } from "./utils";
+import hyperHTML from "hyperhtml";
+import mdnCss from "text!../../assets/mdn-annotation.css";
 
 export const name = "core/mdn-annoatation";
 
 const SPEC_MAP_URL =
   "https://raw.githubusercontent.com/w3c/mdn-spec-links/master/SPECMAP.json";
 const JSON_BASE = "https://w3c.github.io/mdn-spec-links/";
+const MDN_URL_BASE = "https://developer.mozilla.org/en-US/docs/Web/";
 
 async function fetchAndCacheJson(url, maxAge) {
   if (!url) return {};
   const request = new Request(url);
-  const response = await fetchAndCache(request, maxAge);
-  const json = await response.json();
-  return json;
+  return await fetchAndCache(request, maxAge).then(r => r.json());
 }
 
 export async function run(conf) {
@@ -28,7 +29,55 @@ export async function run(conf) {
   if (!hasSpecJson) {
     return;
   }
-  await fetchAndCacheJson(`${JSON_BASE}/${shortName}.json`, maxAge);
-
-  // TODO: parse the doc and look for keys in the mdn spec json
+  const mdnSpecJson = await fetchAndCacheJson(
+    `${JSON_BASE}/${shortName}.json`,
+    maxAge
+  );
+  const nodesWithId = document.querySelectorAll("[id]");
+  const unlikelyTagNames = ["STYLE", "SCRIPT", "BODY"];
+  document.head.appendChild(hyperHTML`<style>${[mdnCss]}</style>`);
+  document.head.appendChild(hyperHTML`<script>
+     function toggleStatus(div) {
+       div.parentNode.classList.toggle('wrapped');
+     }
+  </script>`);
+  nodesWithId.forEach(node => {
+    if (unlikelyTagNames.indexOf(node.tagName) > -1) {
+      return;
+    }
+    if (!(mdnSpecJson[node.id] && Array.isArray(mdnSpecJson[node.id]))) {
+      return;
+    }
+    const mdnSpecArray = mdnSpecJson[node.id];
+    let targetAncestor = node;
+    // Find the furthest ancestor that is a direct child of <body>
+    while (targetAncestor.parentNode.tagName !== "BODY") {
+      targetAncestor = targetAncestor.parentNode;
+    }
+    const targetSibling = targetAncestor.previousElementSibling;
+    let mdnBox;
+    if (targetSibling && targetSibling.class === "mdn before") {
+      // If the target ancestor already has a mdnBox inserted, we just use it
+      mdnBox = targetSibling;
+    } else {
+      mdnBox = hyperHTML`<aside class="mdn before">
+        <input type="button" onclick="toggleStatus(this)" value="⋰" />
+      </aside>`;
+      document.body.insertBefore(mdnBox, targetAncestor);
+    }
+    mdnSpecArray.forEach(spec => {
+      const mdnDiv = document.createElement("div");
+      mdnDiv.innerHTML += `<b>MDN </b>`;
+      const { slug, summary } = spec;
+      const mdnSubPath = slug.slice(slug.indexOf("/") + 1);
+      mdnDiv.innerHTML += `
+        <details>
+          <summary>
+            <a title="${summary}" href="${MDN_URL_BASE}${slug}">${mdnSubPath}</a>
+          </summary>
+        </details>
+      `;
+      mdnBox.appendChild(mdnDiv);
+    });
+  });
 }
