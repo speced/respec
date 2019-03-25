467d3c9cd24aa0677329991d584c8e3bbfe88ab2
Thijs Brentjens <thijs@brentjensgeoict.nl>
2019-03-25 10:28:26 +0100

    Geonovum: prettier code

--- src/geonovum/informative.js
+++ src/geonovum/informative.js
@@ -1,4 +1,4 @@
-// Module w3c/informative
+// Module geonovum/informative
 // Mark specific sections as informative, based on CSS
 import hyperHTML from "hyperhtml";
 export const name = "geonovum/informative";
@@ -8,6 +8,6 @@ export function run() {
     .map(informative => informative.querySelector("h2, h3, h4, h5, h6"))
     .filter(heading => heading)
     .forEach(heading => {
-      heading.parentNode.insertBefore(hyperHTML`<p><em>Dit onderdeel is niet normatief.</em></p>`, heading.nextSibling);
+      heading.after(hyperHTML`<p><em>Dit onderdeel is niet normatief.</em></p>`);
     });
 }
