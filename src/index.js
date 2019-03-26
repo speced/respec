ceba56dc30496473ca3bb3a2642eb52439f7181c
Kagami Sascha Rosylight <saschanaz@outlook.com>
2019-03-26 17:08:46 +0900

    lint fix

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 // @ts-check
+import { cleanup } from "./core/exporter";
 import { createRespecDocument } from "./respec-document";
 import insertStyle from "./core/style";
 import insertW3CStyle from "./w3c/style";
 import reindent from "./core/reindent";
-import { cleanup } from "./core/exporter";
 
 /**
  * @param {string|Document} doc A document that will be preprocessed
