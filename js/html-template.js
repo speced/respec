e9e4e099adafb26a2a66be7d4e80e2d67ba853e3
Kagami Sascha Rosylight <saschanaz@outlook.com>
2019-03-28 11:54:07 +0900

    pre-import for webpack+requirejs compat

--- js/html-template.js
+++ js/html-template.js
@@ -1,20 +1,18 @@
 // A shim to use hyperhtml on Node.js.
 // TODO: migrate to ES module when top-level await arrives
 (function (factory) {
-  if (typeof module === "object" && typeof module.exports === "object") {
-    var v = factory(require, exports);
+  if (typeof define === "function" && define.amd) {
+    define(["require", "exports", "hyperhtml"], factory);
+  } else if (typeof module === "object" && typeof module.exports === "object") {
+    const hypermorphic =
+      typeof document !== "undefined"
+        ? require("hyperhtml")
+        : require("viperhtml");
+    var v = factory(require, exports, hypermorphic);
     if (v !== undefined) module.exports = v;
   }
-  else if (typeof define === "function" && define.amd) {
-    define(["require", "exports"], factory);
-  }
-})(function (require, exports) {
+})(function (require, exports, hypermorphic) {
   "use strict";
-  const hypermorphic =
-    typeof document !== "undefined"
-      ? require("hyperhtml")
-      : require("viperhtml");
-
   exports.__esModule = true;
   exports.default = function(strings, ...args) {
     const result = (hypermorphic.default || hypermorphic)(strings, ...args);
