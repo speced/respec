ae95787caedfe2d9b9043fc8a0ab9a525ed1a0c6
Mark Polyakov <mark@markasoftware.com>
2019-03-31 11:49:33 -0700

    Put ORCID logo back in HTML as SVG

--- src/w3c/templates/show-people.js
+++ src/w3c/templates/show-people.js
@@ -55,7 +55,25 @@ export default (conf, name, items = []) => {
       contents.push(
         html`
           <a class="p-name orcid" href="${p.orcid}"
-            ><div class="orcid-logo"></div
+            ><svg
+              width="16"
+              height="16"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 256 256"
+            >
+              <style>
+                .st1 {
+                  fill: #fff;
+                }
+              </style>
+              <path
+                d="M256 128c0 70.7-57.3 128-128 128S0 198.7 0 128 57.3 0 128 0s128 57.3 128 128z"
+                fill="#a6ce39"
+              />
+              <path
+                class="st1"
+                d="M86.3 186.2H70.9V79.1h15.4v107.1zM108.9 79.1h41.6c39.6 0 57 28.3 57 53.6 0 27.5-21.5 53.6-56.8 53.6h-41.8V79.1zm15.4 93.3h24.5c34.9 0 42.9-26.5 42.9-39.7C191.7 111.2 178 93 148 93h-23.7v79.4zM88.7 56.8c0 5.5-4.5 10.1-10.1 10.1s-10.1-4.6-10.1-10.1c0-5.6 4.5-10.1 10.1-10.1s10.1 4.6 10.1 10.1z"
+              /></svg
           ></a>
         `
       );
