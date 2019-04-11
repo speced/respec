b9fb7085961f8943ff135985b50673219b759d41
Kagami Sascha Rosylight <saschanaz@outlook.com>
2019-04-11 11:57:37 +0900

    simplify the loop

--- src/core/webidl-clipboard.js
+++ src/core/webidl-clipboard.js
@@ -16,35 +16,18 @@ copyButton.title = "Copy IDL to clipboard";
 copyButton.classList.add("respec-button-copy-paste", "removeOnSave");
 
 export async function run() {
-  Array.from(document.querySelectorAll("pre.idl"))
-    .map(elem => {
-      const button = copyButton.cloneNode(true);
-      const definition = elem.querySelector(
-        `span[id^="idl-def-"], span[id^="dom-"]`
-      );
-      let target = "#";
-      if (definition === null && elem.id === "") {
-        if (elem.parentElement.id === "idl-index") {
-          target += "actual-idl-index";
-        } else {
-          elem.id = `idl-${String(Math.random()).substr(2)}`;
-          target += elem.id;
-        }
-      } else {
-        target += elem.id || definition.id;
-      }
-      return { button, elem, target };
+  for (const pre of document.querySelectorAll("pre.idl")) {
+    const button = copyButton.cloneNode(true);
+    const wrapper = document.createElement("div");
+    button.addEventListener("click", () => {
+      clipboardWriteText(pre.textContent);
     })
-    .forEach(({ elem, button, target }) => {
-      const wrapper = document.createElement("div");
-      button.addEventListener("click", () => {
-        clipboardWriteText(document.querySelector(target).textContent);
-      })
-      elem.replaceWith(wrapper);
-      wrapper.appendChild(button);
-      wrapper.appendChild(elem);
-    });
+    pre.replaceWith(wrapper);
+    wrapper.append(button, pre);
+  }
 }
+
+
 
 /**
  * Mocks navigator.clipboard.writeText()
