f90d12b1cda60dafb78e2b12d2c73be64bb7edcc
Kagami Sascha Rosylight <saschanaz@outlook.com>
2019-04-15 16:52:49 +0900

    test: add firefox to travis (#2277)
    * test: add firefox to travis
    
    * try using build matrix expansion
    
    * build stage
    
    * FirefoxHeadlessPref
    
    * use env instead to be more visible in travis
    
    * yaml reference
    
    * syntax fix

--- karma.conf.js
+++ karma.conf.js
@@ -130,6 +130,12 @@ module.exports = function(config) {
           "javascript.options.dynamicImport": true,
         },
       },
+      FirefoxHeadlessPref: {
+        base: "FirefoxHeadless",
+        prefs: {
+          "javascript.options.dynamicImport": true,
+        },
+      },
     },
 
     // Continuous Integration mode
@@ -148,12 +154,8 @@ module.exports = function(config) {
   };
   if (process.env.TRAVIS) {
     process.env.CHROME_BIN = require("puppeteer").executablePath();
-    options.detectBrowsers.enabled = false;
     options.autoWatch = false;
-    options.singleRun = true;
     options.concurrency = 1;
-    options.reporters = ["mocha"];
-    options.browsers = ["ChromeHeadless"];
   }
   if (process.env.BROWSERS) {
     options.detectBrowsers.enabled = false;
