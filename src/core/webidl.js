3ee618cbde0ec8d27a02b99fc0e923ad8724adb6
Marcos Cáceres <mcaceres@mozilla.com>
2019-05-01 14:37:46 +1000

    lint fix

--- src/core/webidl.js
+++ src/core/webidl.js
@@ -31,8 +31,8 @@ const templates = {
     return hyperHTML`<span class='idlSectionComment'>${t}</span>`;
   },
   generic(keyword) {
-    // Shepherd classifies "interfaces" as starting with capital letters, 
-    // like Promise, FrozenArray, etc. 
+    // Shepherd classifies "interfaces" as starting with capital letters,
+    // like Promise, FrozenArray, etc.
     return /^[A-Z]/.test(keyword)
       ? hyperHTML`<a data-xref-type="_IDL_">${keyword}</a>`
       : // Other keywords like sequence, maplike, etc...
