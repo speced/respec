657f3eaf753562a7002427458678186eb5063637
Kagami Sascha Rosylight <saschanaz@outlook.com>
2019-06-02 11:10:31 +0900

    fix(core/xref): solve typescript errors (#2379)

--- src/core/xref.js
+++ src/core/xref.js
@@ -143,7 +143,7 @@ function getRequestEntry(elem) {
   // if element itself contains data-cite, we don't take inline context into account
   if (dataciteElem !== elem) {
     const closestSection = elem.closest("section");
-    /** @type {NodeListOf<HTMLElement>} */
+    /** @type {Iterable<HTMLElement>} */
     const bibrefs = closestSection
       ? closestSection.querySelectorAll("a.bibref")
       : [];
@@ -166,6 +166,7 @@ function getRequestEntry(elem) {
 
   let { xrefFor: forContext } = elem.dataset;
   if (!forContext && isIDL) {
+    /** @type {HTMLElement} */
     const dataXrefForElem = elem.closest("[data-xref-for]");
     if (dataXrefForElem) {
       forContext = dataXrefForElem.dataset.xrefFor;
