48636cacfe9ed4a0845af28b8fcf35f222799e20
Sid Vishnoi <sidvishnoi8@gmail.com>
2019-07-31 13:58:33 +0530

    remove hyperhtml

--- src/ui/search-xref.js
+++ src/ui/search-xref.js
@@ -1,7 +1,6 @@
 // Module ui/search-xref
 // Search xref database
 import { lang as defaultLang } from "../core/l10n.js";
-import hyperHTML from "hyperhtml";
 import { ui } from "../core/ui.js";
 
 const XREF_URL = "https://respec.org/xref/";
@@ -17,7 +16,9 @@ const l10n = localizationStrings[lang];
 const button = ui.addCommand(l10n.title, show, "Ctrl+Shift+Alt+x", "📚");
 
 function show() {
-  const iframe = hyperHTML`
-    <iframe src="${XREF_URL}" id="xref-ui" onload="this.classList.add('ready')"></iframe>`;
+  const iframe = document.createElement("iframe");
+  iframe.id = "xref-ui";
+  iframe.src = XREF_URL;
+  iframe.onload = () => iframe.classList.add("ready");
   ui.freshModal(l10n.title, iframe, button);
 }
