f468fad5288ccb35eec347563e6f8f45b35d6e00
Kagami Sascha Rosylight <saschanaz@outlook.com>
2019-07-31 21:22:58 +0900

    nodeCrypto

--- js/crypto.js
+++ js/crypto.js
@@ -4,16 +4,16 @@
   if (typeof define === "function" && define.amd) {
     define(["exports"], factory);
   } else if (typeof module === "object" && typeof module.exports === "object") {
-    const crypto = require("crypto");
-    const v = factory(exports, crypto);
+    const nodeCrypto = require("crypto");
+    const v = factory(exports, nodeCrypto);
     if (v !== undefined) module.exports = v;
   }
-})((exports, crypto) => {
+})((exports, nodeCrypto) => {
   "use strict";
   exports.__esModule = true;
   exports.sha1Digest = str => {
-    if (crypto) {
-      const hash = crypto.createHash("sha1");
+    if (nodeCrypto) {
+      const hash = nodeCrypto.createHash("sha1");
       hash.update(str);
       return Promise.resolve(hash.digest().buffer);
     }
