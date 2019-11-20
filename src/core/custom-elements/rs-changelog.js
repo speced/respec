376fa836d729934bebc7af1076060867d4c61984
Sid Vishnoi <sidvishnoi8@gmail.com>
2019-11-20 17:20:38 +0530

    review feedback

--- src/core/custom-elements/rs-changelog.js
+++ src/core/custom-elements/rs-changelog.js
@@ -9,7 +9,7 @@
  */
 import { github } from "../github.js";
 import { hyperHTML } from "../import-maps.js";
-import { pub } from "../pubsubhub.js";
+import { showInlineError } from "../utils.js";
 
 export const name = "rs-changelog";
 
@@ -34,7 +34,7 @@ export default class ChangelogElement extends HTMLElement {
       ${{
         any: fetchCommits(from, to, filter)
           .then(commits => toHTML(commits))
-          .catch(error => pub("error", error.message))
+          .catch(error => showInlineError(this, error.message, error.message))
           .finally(() => {
             this.dispatchEvent(new CustomEvent("done"));
           }),
@@ -46,6 +46,8 @@ export default class ChangelogElement extends HTMLElement {
 }
 
 async function fetchCommits(from, to, filter) {
+  /** @type {Commit[]} */
+  let commits;
   try {
     const { api: githubAPI } = await github;
     const url = new URL("commits", githubAPI);
@@ -58,17 +60,17 @@ async function fetchCommits(from, to, filter) {
         `Request to ${url} failed with status code ${res.status}`
       );
     }
-    /** @type {Commit[]} */
-    const commits = await res.json();
+    commits = await res.json();
     if (!commits.length) {
       throw new Error(`No commits between ${from}..${to}.`);
     }
-    return commits.filter(filter);
+    commits = commits.filter(filter);
   } catch (error) {
     const msg = `Error loading commits from GitHub. ${error.message}`;
     console.error(error);
     throw new Error(msg);
   }
+  return commits;
 }
 
 async function toHTML(commits) {
