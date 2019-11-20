45c1ead1a154a94068bdd3953a620f44c756e5db
Marcos Cáceres <marcos@marcosc.com>
2019-11-20 16:29:06 +1100

    docs(w3c/headers): w3c-software-doc is the default

--- src/w3c/headers.js
+++ src/w3c/headers.js
@@ -82,13 +82,12 @@
 //          - href: a URL for the value (e.g., "https://foo.com/issues"). Optional.
 //          - class: a string representing CSS classes. Optional.
 //  - license: can be one of the following
-//      - "w3c", currently the default (restrictive) license
 //      - "cc-by", which is experimentally available in some groups (but likely to be phased out).
 //          Note that this is a dual licensing regime.
 //      - "cc0", an extremely permissive license. It is only recommended if you are working on a document that is
 //          intended to be pushed to the WHATWG.
 //      - "w3c-software", a permissive and attributions license (but GPL-compatible).
-//      - "w3c-software-doc", the W3C Software and Document License
+//      - "w3c-software-doc", (default) the W3C Software and Document License
 //            https://www.w3.org/Consortium/Legal/2015/copyright-software-and-document
 import { ISODate, concatDate, joinAnd } from "../core/utils.js";
 import cgbgHeadersTmpl from "./templates/cgbg-headers.js";
