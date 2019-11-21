c4a431484bfa7de552318524127593045bd5e7bb
Marcos Cáceres <mcaceres@mozilla.com>
2019-11-21 13:58:56 +1100

    review feedback

--- src/core/link-to-dfn.js
+++ src/core/link-to-dfn.js
@@ -205,29 +205,29 @@ function isCode(dfn) {
 
 /**
  * Wrap links by <code>.
- * @param {HTMLAnchorElement} ant a link
+ * @param {HTMLAnchorElement} anchor a link
  * @param {HTMLElement} dfn a definition
  */
-function wrapAsCode(ant, dfn) {
+function wrapAsCode(anchor, dfn) {
   // only add code to IDL when the definition matches
-  const term = ant.textContent.trim();
+  const term = anchor.textContent.trim();
   const isIDL = dfn.dataset.hasOwnProperty("idl");
-  const needsCode =
-    ant.firstElementChild && ant.firstElementChild.localName === "code"
-      ? false
-      : shouldWrapByCode(dfn, term);
+  const needsCode = shouldWrapByCode(anchor) || shouldWrapByCode(dfn, term);
   if (!isIDL || needsCode) {
-    wrapInner(ant, document.createElement("code"));
+    wrapInner(anchor, document.createElement("code"));
   }
 }
 
 /**
- * @param {HTMLElement} dfn
+ * @param {HTMLElement} elem
  * @param {string} term
  */
-function shouldWrapByCode(dfn, term) {
-  const { dataset } = dfn;
-  if (dfn.textContent.trim() === term) {
+function shouldWrapByCode(elem, term = "") {
+  if (elem.firstElementChild && elem.firstElementChild.localName === "code") {
+    return false;
+  }
+  const { dataset } = elem;
+  if (elem.textContent.trim() === term) {
     return true;
   } else if (dataset.title === term) {
     return true;
