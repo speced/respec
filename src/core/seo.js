6c84af5f9861bc1367e94e7639dffc5287182ee1
Antonin Delpeuch <antonin@delpeuch.eu>
2019-11-26 12:02:47 +0000

    Simplify src/core/seo.js

--- src/core/seo.js
+++ src/core/seo.js
@@ -7,16 +7,10 @@
 export const name = "core/seo";
 
 export function run() {
-  // This needs to run before JSON-LD export runs, because it relies on
-  // the meta tag being present to extract the description itself.
   const firstParagraph = document.querySelector("#abstract p:first-of-type");
   if (!firstParagraph) {
     return; // no abstract, so nothing to do
   }
-  insertMetaDescription(firstParagraph);
-}
-
-function insertMetaDescription(firstParagraph) {
   // Normalize whitespace: trim, remove new lines, tabs, etc.
   const doc = firstParagraph.ownerDocument;
   const content = firstParagraph.textContent.replace(/\s+/, " ").trim();
