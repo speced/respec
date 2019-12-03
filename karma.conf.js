2446babcdc0cdd3877d8cee6f64bfbb673ca1c11
Kagami Sascha Rosylight <saschanaz@outlook.com>
2019-12-03 21:55:10 +0900

    refactor: remove requirejs dependency

--- karma.conf.js
+++ karma.conf.js
@@ -8,7 +8,7 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ["jasmine", "requirejs", "detectBrowsers"],
+    frameworks: ["jasmine", "detectBrowsers"],
 
     // configuration
     detectBrowsers: {
