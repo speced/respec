6f59d37e281aac89ffd00abe4327670a750320b1
ridhishjain <ultimate.ridhish@gmail.com>
2019-12-17 02:11:30 +0530

    Update inlines.js

--- src/core/inlines.js
+++ src/core/inlines.js
@@ -182,7 +182,6 @@ export function run(conf) {
   document.normalize();
   if (!document.querySelector("section#conformance")) {
     // make the document informative
-    // throw an error like "Conformance not found"
     document.body.classList.add("informative");
   }
   conf.normativeReferences = new InsensitiveStringSet();
