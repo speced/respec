d0543407fe1f59934ac50b0700d3b0e6d15f3012
Suyash Salampuria <44131389+SuyashSalampuria@users.noreply.github.com>
2019-12-23 13:50:28 +0530

    BREAKING CHANGE: removed reqRef class support (#2653)

--- tests/spec/core/requirements-spec.js
+++ tests/spec/core/requirements-spec.js
@@ -18,20 +18,4 @@ describe("Core — Requirements", () => {
     expect(ref.textContent).toBe("Req. 1");
     expect(ref.getAttribute("href")).toBe("#req-id");
   });
-
-  it("should process requirement references", async () => {
-    const body = `
-      <a href='#req-id' class='reqRef'></a>
-      <a href='#foo' class='reqRef'></a>
-      <p class='req' id='req-id'>REQ</p>
-    `;
-    const ops = makeStandardOps(null, body);
-    const doc = await makeRSDoc(ops);
-
-    const refs = doc.querySelectorAll("a.reqRef");
-    expect(refs.length).toBe(2);
-    const [validRef, invalidRef] = refs;
-    expect(validRef.textContent).toBe("Req. 1");
-    expect(invalidRef.textContent).toBe("Req. not found 'foo'");
-  });
 });
