25bf9e37d6e4a4ff3698955a7c778f81d0692a8c
ridhishjain <ultimate.ridhish@gmail.com>
2019-12-29 15:17:57 +0530

    adding localization strings again

--- src/core/l10n.js
+++ src/core/l10n.js
@@ -1,33 +1,22 @@
-// @ts-check
-/**
- * Module core/l10n
- *
- * Looks at the lang attribute on the root element and uses it
- * to manage the config.l10n object so that other parts of the system can
- * localize their text.
- */
-export const name = "core/l10n";
-
-const html = document.documentElement;
-if (html && !html.hasAttribute("lang")) {
-  html.lang = "en";
-  if (!html.hasAttribute("dir")) {
-    html.dir = "ltr";
-  }
-}
-
-// We use en-US as the base
 const base = {
   about_respec: "About",
   abstract: "Abstract",
+  author: "Author:",
+  authors: "Authors:",
   bug_tracker: "Bug tracker:",
   close_parens: ")",
   definition_list: "Definitions",
+  editor: "Editor:",
   editors_note: "Editor's note",
+  editors: "Editors:",
   feature_at_risk: "(Feature at Risk) Issue",
+  former_editor: "Former editor:",
+  former_editors: "Former editors:",
   info_references: "Informative references",
   issue_summary: "Issue Summary",
   issue: "Issue",
+  latest_editors_draft: "Latest editor's draft:",
+  latest_published_version: "Latest published version:",
   list_of_definitions: "List of Definitions",
   norm_references: "Normative references",
   note: "Note",
@@ -45,6 +34,9 @@ const base = {
 
 const ko = {
   abstract: "요약",
+  author: "저자:",
+  authors: "저자:",
+  latest_published_version: "최신 버전:",
   this_version: "현재 버전:",
 };
 
@@ -52,7 +44,13 @@ const zh = {
   about_respec: "关于",
   abstract: "摘要",
   bug_tracker: "错误跟踪：",
+  editor: "编辑：",
+  editors: "编辑：",
   file_a_bug: "反馈错误",
+  former_editor: "原编辑：",
+  former_editors: "原编辑：",
+  latest_editors_draft: "最新编辑草稿：",
+  latest_published_version: "最新发布版本：",
   note: "注",
   open_bugs: "修正中的错误",
   participate: "参与：",
@@ -62,24 +60,37 @@ const zh = {
 
 const ja = {
   abstract: "要約",
+  author: "著者：",
+  authors: "著者：",
   bug_tracker: "バグの追跡履歴：",
+  editor: "編者：",
+  editors: "編者：",
   file_a_bug: "問題報告",
+  former_editor: "以前の版の編者：",
+  former_editors: "以前の版の編者：",
+  latest_editors_draft: "最新の編集用草案：",
+  latest_published_version: "最新バージョン：",
   note: "注",
   open_bugs: "改修されていないバグ",
   participate: "参加方法：",
   this_version: "このバージョン：",
   toc: "目次",
 };
-
 const nl = {
   about_respec: "Over",
   abstract: "Samenvatting",
+  author: "Auteur:",
+  authors: "Auteurs:",
   bug_tracker: "Meldingensysteem:",
   definition_list: "Lijst van Definities",
+  editor: "Redacteur:",
   editors_note: "Redactionele noot",
+  editors: "Redacteurs:",
   file_a_bug: "Dien een melding in",
   info_references: "Informatieve referenties",
   issue_summary: "Lijst met issues",
+  latest_editors_draft: "Laatste werkversie:",
+  latest_published_version: "Laatst gepubliceerde versie:",
   list_of_definitions: "Lijst van Definities",
   norm_references: "Normatieve referenties",
   note: "Noot",
@@ -96,13 +107,19 @@ const nl = {
 
 const es = {
   abstract: "Resumen",
+  author: "Autor:",
+  authors: "Autores:",
   bug_tracker: "Repositorio de bugs:",
   close_parens: ")",
+  editor: "Editor:",
   editors_note: "Nota de editor",
+  editors: "Editores:",
   file_a_bug: "Nota un bug",
   info_references: "Referencias informativas",
   issue_summary: "Resumen de la cuestión",
   issue: "Cuestión",
+  latest_editors_draft: "Borrador de editor mas reciente:",
+  latest_published_version: "Versión publicada mas reciente:",
   norm_references: "Referencias normativas",
   note: "Nota",
   open_bugs: "Bugs abiertos",
@@ -113,7 +130,6 @@ const es = {
   toc: "Tabla de Contenidos",
   warning: "Aviso",
 };
-
 export const l10n = {
   en: { ...base },
   ko: { ...base, ...ko },
@@ -122,12 +138,9 @@ export const l10n = {
   nl: { ...base, ...nl },
   es: { ...base, ...es },
 };
-
 l10n["zh-hans"] = l10n.zh;
 l10n["zh-cn"] = l10n.zh;
-
 export const lang = html && html.lang in l10n ? html.lang : "en";
-
 /**
  * @template {Record<string, Record<string, string|Function>>} T
  * @param {T} localizationStrings
@@ -148,7 +161,6 @@ export function getIntlData(localizationStrings) {
     },
   });
 }
-
 export function run(config) {
   config.l10n = l10n[lang] || l10n.en;
 }
