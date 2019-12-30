302fd1ec48d89fc17593afff803aae306149150e
Marcos Cáceres <marcos@marcosc.com>
2019-12-31 00:35:58 +1100

    Revert "fix(core/markdown): use reindent() instead (#2650)"
    This reverts commit d33de51ec5100987ce3b6aef95888a3ab0c29fea.

--- tests/spec/core/markdown-spec.js
+++ tests/spec/core/markdown-spec.js
@@ -44,7 +44,6 @@ describe("Core - Markdown", () => {
       </div>
     `;
     const ops = makeStandardOps({ format: "markdown" }, body);
-    ops.abstract = null;
     const doc = await makeRSDoc(ops);
     expect(doc.querySelector(".note p em")).toBeTruthy();
     expect(doc.querySelector(".issue p em")).toBeTruthy();
@@ -59,7 +58,6 @@ describe("Core - Markdown", () => {
           * nested list item
     `;
     const ops = makeStandardOps({ format: "markdown" }, body);
-    ops.abstract = null;
     const doc = await makeRSDoc(ops);
     expect(doc.querySelector("code")).toBeFalsy();
     expect(doc.querySelector("#foo h2").textContent).toBe("1. Foo");
@@ -124,7 +122,6 @@ describe("Core - Markdown", () => {
 
         `;
     const ops = makeStandardOps({ format: "markdown" }, body);
-    ops.abstract = null;
     const doc = await makeRSDoc(ops);
     const foo = doc.querySelector("#foo h2");
     expect(foo.textContent).toBe("1. Foo");
@@ -211,7 +208,6 @@ describe("Core - Markdown", () => {
 
         `;
     const ops = makeStandardOps({ format: "markdown" }, body);
-    ops.abstract = null;
     const doc = await makeRSDoc(ops);
     const baz = doc.querySelector("#baz h2");
     expect(baz.textContent).toBe("2. Baz");
@@ -267,7 +263,6 @@ describe("Core - Markdown", () => {
       \`\`\`
     `;
     const ops = makeStandardOps({ format: "markdown" }, body);
-    ops.abstract = null;
     const doc = await makeRSDoc(ops);
     const [webidlBlock, jsBlock, normalBlock] = doc.querySelectorAll("pre");
 
@@ -300,7 +295,6 @@ describe("Core - Markdown", () => {
     `;
 
     const ops = makeStandardOps({ format: "markdown" }, body);
-    ops.abstract = null;
     const doc = await makeRSDoc(ops);
     const [mixedCaseWebidl] = doc.querySelectorAll("pre");
 
@@ -309,41 +303,6 @@ describe("Core - Markdown", () => {
     expect(
       mixedCaseWebidl.querySelector(".respec-button-copy-paste")
     ).toBeTruthy();
-  });
-
-  it("properly indents <pre> contents with no block indentation", async () => {
-    const idl = `function getAnswer() {\n  return 42;\n}`;
-    const body = `
-# test
-
-\`\`\` js
-function getAnswer() {
-  return 42;
-}
-\`\`\`
-    `;
-
-    const ops = makeStandardOps({ format: "markdown" }, body);
-    const doc = await makeRSDoc(ops);
-    const pre = doc.querySelector("pre");
-
-    expect(pre.textContent).toBe(idl);
-  });
-
-  it("does not parse texts inside <pre> as markdown", async () => {
-    const code = `1 * 2 * 3;`;
-    const body = `
-      <pre class="example">
-      ${code}
-      </pre>
-    `;
-
-    const ops = makeStandardOps({ format: "markdown" }, body);
-    ops.abstract = null;
-    const doc = await makeRSDoc(ops);
-    const pre = doc.querySelector("pre");
-
-    expect(pre.textContent).toBe(code);
   });
 
   describe("nolinks options", () => {
@@ -356,7 +315,6 @@ function getAnswer() {
         </div>
       `;
       const ops = makeStandardOps({ format: "markdown" }, body);
-      ops.abstract = null;
       const doc = await makeRSDoc(ops);
       const anchors = doc.querySelectorAll("#testElem a");
       expect(anchors.length).toBe(2);
@@ -437,21 +395,45 @@ function getAnswer() {
     });
   });
 
+  describe("Whitespace compatibility", () => {
+    it("normalises whitespace, but ignore white with pre tags", async () => {
+      const str = `   trim start\n    * trim 3 from start \n\n <pre>trim 1\n   if(x){\n\t party()</pre>\n  foo \n    bar`;
+      const ops = makeStandardOps(
+        null,
+        `<section id=markdown1 data-format=markdown>${str}`
+      );
+      const doc = await makeRSDoc(ops);
+      const [p1, ul, pre, p2] = doc.getElementById("markdown1").children;
+      expect(p1.localName).toBe("p");
+      expect(p1.textContent).toBe("trim start");
+      expect(p2.textContent).toBe("foo \n bar");
+      expect(ul.children.length).toBe(1);
+
+      const [li] = ul.children;
+      expect(li.localName).toBe("li");
+      expect(li.textContent).toBe("trim 3 from start ");
+
+      const preText = pre.textContent.split("\n");
+      expect(preText[0]).toBe("trim 1");
+      expect(preText[1]).toBe("   if(x){");
+      expect(preText[2]).toBe("     party()");
+    });
+  });
+
   describe("Markdown-inside-block backward compatibility", () => {
     it("parses indented <pre> after a list", async () => {
-      const idl = `dictionary Indented {\n  any shouldBeIndented;\n};`;
+      const idl = `dictionary Indented {\n  any shouldBeIndented();\n};`;
       const body = `
         <div>
         1. I lack double newlines between HTML block
         </div>
         <pre class="idl" id="pre">
         dictionary Indented {
-          any shouldBeIndented;
+          any shouldBeIndented();
         };
         </pre>
       `;
       const ops = makeStandardOps({ format: "markdown" }, body);
-      ops.abstract = null;
       const doc = await makeRSDoc(ops);
       const span = doc.querySelector("pre > span");
 
