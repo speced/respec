6052246b27ef5d95064a10e2e1c51ed1d2ec5c95
Sebastian Meyer <sebastian.meyer@opencultureconsulting.com>
2020-01-09 12:45:12 +0100

    Make test title more specific

--- tests/spec/core/utils-spec.js
+++ tests/spec/core/utils-spec.js
@@ -322,7 +322,7 @@ describe("Core - Utils", () => {
       expect(intl.bar).toBe("EN Bar");
     });
 
-    it("falls back to English string if language does not exist in language data", () => {
+    it("falls back to English string if language does not exist in localization data", () => {
       const intl = getIntlData(localizationStrings, "de");
       expect(intl.bar).toBe("EN Bar");
     });
