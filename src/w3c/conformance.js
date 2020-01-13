7df6d0a09726196a20a0b43a7192db1326bf1ed3
Sebastian Meyer <sebastian.meyer@opencultureconsulting.com>
2020-01-13 12:24:03 +0100

    Don't use shortened URL
    Co-Authored-By: Kagami Sascha Rosylight <saschanaz@outlook.com>

--- src/w3c/conformance.js
+++ src/w3c/conformance.js
@@ -41,7 +41,7 @@ const localizationStrings = {
           <a href="https://tools.ietf.org/html/bcp14">BCP 14</a>
           ${renderInlineCitation("RFC2119")} ${renderInlineCitation("RFC8174")}
           und unter Berücksichtigung von
-          <a href="https://goo.gl/6QZH4J">2119de</a>
+          <a href="https://github.com/adfinis-sygroup/2119/blob/master/2119de.rst">2119de</a>
           zu interpretieren, wenn und nur wenn ${plural ? "sie" : "es"} wie hier
           gezeigt durchgehend groß geschrieben wurde${plural ? "n" : ""}.
         </p>
