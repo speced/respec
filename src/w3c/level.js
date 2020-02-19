1104b4be828fd9cc83464c9c5eac73428c1d415d
janiceshiu <contrepoint21@gmail.com>
2020-02-19 20:10:11 +0800

    return early if level config is absent. less nesting too.

--- src/w3c/level.js
+++ src/w3c/level.js
@@ -17,19 +17,19 @@ const localizationStrings = {
 const l10n = getIntlData(localizationStrings);
 
 export function run(conf) {
+  if (!conf.hasOwnProperty("level")) return;
+
   const h1Elem = document.querySelector("h1#title");
 
-  if (conf.hasOwnProperty("level")) {
-    if (Number.isInteger(conf.level) && conf.level >= 0) {
-      h1Elem.append(` ${l10n.level} ${conf.level}`);
-      document.title = `${document.title} ${l10n.level} ${conf.level}`;
-      conf.shortName = `${conf.shortName}-${conf.level}`;
-    } else {
-      showInlineWarning(
-        h1Elem,
-        `The \`level\` configuration option must be a number greater or equal to 0. It is currently set to \`${conf.level}\``,
-        "Invalid level config."
-      );
-    }
+  if (Number.isInteger(conf.level) && conf.level >= 0) {
+    h1Elem.append(` ${l10n.level} ${conf.level}`);
+    document.title = `${document.title} ${l10n.level} ${conf.level}`;
+    conf.shortName = `${conf.shortName}-${conf.level}`;
+  } else {
+    showInlineWarning(
+      h1Elem,
+      `The \`level\` configuration option must be a number greater or equal to 0. It is currently set to \`${conf.level}\``,
+      "Invalid level config."
+    );
   }
 }
