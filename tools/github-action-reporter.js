d3d8612bff9d20345093d48dfef86c30b31938a3
Sid Vishnoi <sidvishnoi8@gmail.com>
2020-03-25 00:52:53 +0530

    try with escape(optionsString)

--- tools/github-action-reporter.js
+++ tools/github-action-reporter.js
@@ -53,10 +53,8 @@ function printFailure(failure) {
  * @param {Record<string, string>} options
  */
 function print(command, message = "", options = {}) {
-  let optionsString = new URLSearchParams(options)
-    .toString()
-    .replace(/&/g, ",");
-  if (optionsString) optionsString = ` ${optionsString}`;
+  let optionsString = `${new URLSearchParams(options)}`.replace(/&/g, ",");
+  if (optionsString) optionsString = ` ${escapeData(optionsString)}`;
   const msg = escapeData(message);
   const output = `::${command}${optionsString}::${msg}`;
   console.log(output);
