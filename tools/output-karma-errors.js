7f94ede174a1406bbe2ccd6dc9e45879e1704f9c
Sid Vishnoi <sidvishnoi8@gmail.com>
2020-03-24 18:56:03 +0530

    nits

--- tools/output-karma-errors.js
+++ tools/output-karma-errors.js
@@ -10,16 +10,18 @@ const failures = karmaResultsData.browsers[0].results
       .split("/base/tests/spec/")[1]
       .replace(/\)$/, "");
     const title = [`${suite[0]} (${location})`, ...suite.slice(1), description]
-      .map((s, i) => `${"\t".repeat(i === 0 ? 0 : 1)}${s}`)
+      .map((s, i) => `${" ".repeat(i === 0 ? 0 : 4)}${s}`)
       .join("\n");
     return `${title}\n${result.replace("Error: ", "")}`;
   });
 
-failures.forEach(failure => {
-  process.stdout.write(`::error:: ${escapeData(failure)}${EOL}`);
-});
+failures.forEach(writeFailure);
 if (failures.length) {
   process.exit(1);
+}
+
+function writeFailure(failureStr) {
+  process.stdout.write(`::error:: ${escapeData(failureStr)}${EOL}`);
 }
 
 function escapeData(s) {
