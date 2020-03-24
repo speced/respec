a2afd4e184219d62632253503825281b5055174d
Sid Vishnoi <sidvishnoi8@gmail.com>
2020-03-24 18:40:26 +0530

    core escape from @actions/core

--- tools/output-karma-errors.js
+++ tools/output-karma-errors.js
@@ -15,11 +15,13 @@ const failures = karmaResultsData.browsers[0].results
 
 failures.forEach(failure => {
   const { title, result, location } = failure;
-  const output = `${title} (${location}):\n ${result
-    .replace("Error: ", "")
-    .replace(/\n/g, "%0A")}`;
-  process.stdout.write(`::error:: ${output}${EOL}`);
+  const output = `${title} (${location}):\n ${result.replace("Error: ", "")}`;
+  process.stdout.write(`::error:: ${escapeData(output)}${EOL}`);
 });
 if (failures.length) {
   process.exit(1);
+}
+
+function escapeData(s) {
+  return s.replace(/%/g, "%25").replace(/\r/g, "%0D").replace(/\n/g, "%0A");
 }
