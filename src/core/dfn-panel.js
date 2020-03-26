3a67275b26467cd38901e26446c24b0dcd5551b5
Sid Vishnoi <sidvishnoi8@gmail.com>
2020-03-26 18:42:14 +0530

    avoid using hyperHTML

--- src/core/dfn-panel.js
+++ src/core/dfn-panel.js
@@ -1,18 +1,18 @@
 // Constructs "dfn panels" which show all the local references to a dfn and a
 // self link to the selected dfn. Based on Bikeshed's dfn panels at
 // https://github.com/tabatkins/bikeshed/blob/ef44162c2e/bikeshed/dfnpanels.py
+//
+// Note: This module does not use hyperHTML as its functionality is injected in
+// exported docs and we want to add minimal JavaScript to exported docs.
 import { fetchAsset } from "./text-loader.js";
-import { hyperHTML } from "./import-maps.js";
 import { norm } from "./utils.js";
 
 export const name = "core/dfn-panel";
 
 export async function run() {
-  const css = await loadStyle();
-  document.head.insertBefore(
-    hyperHTML`<style>${css}</style>`,
-    document.querySelector("link")
-  );
+  const style = document.createElement("style");
+  style.textContent = await loadStyle();
+  document.head.insertBefore(style, document.querySelector("link"));
 
   /** @type {HTMLElement} */
   let panel;
@@ -67,15 +67,15 @@ function createPanel(dfn) {
   const href = `#${id}`;
   const links = document.querySelectorAll(`a[href="${href}"]`);
 
-  /** @type {HTMLElement} */
-  const panel = hyperHTML`
+  const tempNode = document.createElement("div");
+  tempNode.innerHTML = `
     <aside class="dfn-panel" id="dfn-panel">
       <b><a class="self-link" href="${href}">Permalink</a></b>
       <b>Referenced in:</b>
       ${referencesToHTML(id, links)}
     </aside>
   `;
-  return panel;
+  return tempNode.querySelector("aside");
 }
 
 /**
@@ -85,7 +85,7 @@ function createPanel(dfn) {
  */
 function referencesToHTML(id, links) {
   if (!links.length) {
-    return hyperHTML`<ul><li>Not referenced in this document.</li></ul>`;
+    return `<ul><li>Not referenced in this document.</li></ul>`;
   }
 
   /** @type {Map<string, string[]>} */
@@ -116,12 +116,12 @@ function referencesToHTML(id, links) {
    * @returns {HTMLLIElement}
    */
   const listItemToHTML = entry =>
-    hyperHTML`<li>${toLinkProps(entry).map(
-      link => hyperHTML`<a href="#${link.id}">${link.title}</a>${" "}`
-    )}</li>`;
+    `<li>${toLinkProps(entry)
+      .map(link => `<a href="#${link.id}">${link.title}</a>${" "}`)
+      .join("")}</li>`;
 
   const listItems = [...titleToIDs].map(listItemToHTML);
-  return hyperHTML`<ul>${listItems}</ul>`;
+  return `<ul>${listItems}</ul>`;
 }
 
 /** @param {HTMLAnchorElement} link */
