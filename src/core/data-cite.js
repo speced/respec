c30806e042a36197fa37d074c8c8266d3ec3feb1
Sid Vishnoi <sidvishnoi8@gmail.com>
2020-04-10 17:12:23 +0530

    refactor(core/data-cite): do not require conf in citeDetailsConverter (#2826)
    * do not require conf in citeDetailsConverter
    * reuse single instance of citeDetailsConverter -> toCiteDetails
    * remove redundant param from linkInlineCitations
    * reuse single instance of requestLookup -> toRequestLookup
    * remove self-cite console.log message

--- src/core/data-cite.js
+++ src/core/data-cite.js
@@ -24,78 +24,73 @@ import {
 import { sub } from "./pubsubhub.js";
 export const name = "core/data-cite";
 
-function requestLookup(conf) {
-  const toCiteDetails = citeDetailsConverter(conf);
-  return async elem => {
-    const originalKey = elem.dataset.cite;
-    const { key, frag, path } = toCiteDetails(elem);
-    let href = "";
-    let title = "";
-    // This is just referring to this document
-    if (key.toLowerCase() === conf.shortName.toLowerCase()) {
-      console.log(
-        elem,
-        `The reference "${key}" is resolved into the current document per \`conf.shortName\`.`
-      );
-      href = document.location.href;
-    } else {
-      // Let's go look it up in spec ref...
-      const entry = await resolveRef(key);
-      if (!entry) {
-        showInlineWarning(elem, `Couldn't find a match for "${originalKey}"`);
-        return;
+const THIS_SPEC = "__SPEC__";
+
+async function toLookupRequest(elem) {
+  const originalKey = elem.dataset.cite;
+  const { key, frag, path } = toCiteDetails(elem);
+  let href = "";
+  let title = "";
+  // This is just referring to this document
+  if (key === THIS_SPEC) {
+    href = document.location.href;
+  } else {
+    // Let's go look it up in spec ref...
+    const entry = await resolveRef(key);
+    if (!entry) {
+      showInlineWarning(elem, `Couldn't find a match for "${originalKey}"`);
+      return;
+    }
+    href = entry.href;
+    title = entry.title;
+  }
+  if (path) {
+    // See: https://github.com/w3c/respec/issues/1856#issuecomment-429579475
+    const relPath = path.startsWith("/") ? `.${path}` : path;
+    href = new URL(relPath, href).href;
+  }
+  if (frag) {
+    href = new URL(frag, href).href;
+  }
+  switch (elem.localName) {
+    case "a": {
+      if (elem.textContent === "" && elem.dataset.lt !== "the-empty-string") {
+        elem.textContent = title;
       }
-      href = entry.href;
-      title = entry.title;
-    }
-    if (path) {
-      // See: https://github.com/w3c/respec/issues/1856#issuecomment-429579475
-      const relPath = path.startsWith("/") ? `.${path}` : path;
-      href = new URL(relPath, href).href;
-    }
-    if (frag) {
-      href = new URL(frag, href).href;
-    }
-    switch (elem.localName) {
-      case "a": {
-        if (elem.textContent === "" && elem.dataset.lt !== "the-empty-string") {
-          elem.textContent = title;
-        }
-        elem.href = href;
-        if (!path && !frag) {
-          const cite = document.createElement("cite");
-          elem.replaceWith(cite);
-          cite.append(elem);
-        }
-        break;
+      elem.href = href;
+      if (!path && !frag) {
+        const cite = document.createElement("cite");
+        elem.replaceWith(cite);
+        cite.append(elem);
       }
-      case "dfn": {
-        const anchor = document.createElement("a");
-        anchor.href = href;
-        if (!elem.textContent) {
-          anchor.textContent = title;
-          elem.append(anchor);
-        } else {
-          wrapInner(elem, anchor);
-        }
-        if (!path && !frag) {
-          const cite = document.createElement("cite");
-          cite.append(anchor);
-          elem.append(cite);
-        }
-        if ("export" in elem.dataset) {
-          showInlineError(
-            elem,
-            "Exporting an linked external definition is not allowed. Please remove the `data-export` attribute",
-            "Please remove the `data-export` attribute."
-          );
-          delete elem.dataset.export;
-        }
-        elem.dataset.noExport = "";
-        break;
-      }
+      break;
     }
-  };
+    case "dfn": {
+      const anchor = document.createElement("a");
+      anchor.href = href;
+      if (!elem.textContent) {
+        anchor.textContent = title;
+        elem.append(anchor);
+      } else {
+        wrapInner(elem, anchor);
+      }
+      if (!path && !frag) {
+        const cite = document.createElement("cite");
+        cite.append(anchor);
+        elem.append(cite);
+      }
+      if ("export" in elem.dataset) {
+        showInlineError(
+          elem,
+          "Exporting an linked external definition is not allowed. Please remove the `data-export` attribute",
+          "Please remove the `data-export` attribute."
+        );
+        delete elem.dataset.export;
+      }
+      elem.dataset.noExport = "";
+      break;
+    }
+  }
 }
 
 /**
@@ -109,6 +104,9 @@ function makeComponentFinder(component) {
   };
 }
 
+const findFrag = makeComponentFinder("#");
+const findPath = makeComponentFinder("/");
+
 /**
  * @typedef {object} CiteDetails
  * @property {string} key
@@ -116,51 +114,53 @@ function makeComponentFinder(component) {
  * @property {string} frag
  * @property {string} path
  *
- * @return {(elem: HTMLElement) => CiteDetails};
+ * @param {HTMLElement} elem
+ * @return {CiteDetails};
  */
-export function citeDetailsConverter(conf) {
-  const findFrag = makeComponentFinder("#");
-  const findPath = makeComponentFinder("/");
-  return function toCiteDetails(elem) {
-    const { dataset } = elem;
-    const { cite: rawKey, citeFrag, citePath } = dataset;
-    // The key is a fragment, resolve using the shortName as key
-    if (rawKey.startsWith("#") && !citeFrag) {
-      // Closes data-cite not starting with "#"
-      /** @type {HTMLElement} */
-      const closest = elem.parentElement.closest(
-        `[data-cite]:not([data-cite^="#"])`
-      );
-      const { key: parentKey, isNormative: closestIsNormative } = closest
-        ? toCiteDetails(closest)
-        : { key: conf.shortName || "", isNormative: false };
-      dataset.cite = closestIsNormative ? parentKey : `?${parentKey}`;
-      dataset.citeFrag = rawKey.replace("#", ""); // the key is acting as fragment
-      return toCiteDetails(elem);
-    }
-    const frag = citeFrag ? `#${citeFrag}` : findFrag(rawKey);
-    const path = citePath || findPath(rawKey).split("#")[0]; // path is always before "#"
-    const { type } = refTypeFromContext(rawKey, elem);
-    const isNormative = type === "normative";
-    // key is before "/" and "#" but after "!" or "?" (e.g., ?key/path#frag)
-    const hasPrecedingMark = /^[?|!]/.test(rawKey);
-    const key = rawKey.split(/[/|#]/)[0].substring(Number(hasPrecedingMark));
-    const details = { key, isNormative, frag, path };
-    return details;
-  };
+export function toCiteDetails(elem) {
+  const { dataset } = elem;
+  const { cite: rawKey, citeFrag, citePath } = dataset;
+  // The key is a fragment, resolve using the shortName as key
+  if (rawKey.startsWith("#") && !citeFrag) {
+    // Closes data-cite not starting with "#"
+    /** @type {HTMLElement} */
+    const closest = elem.parentElement.closest(
+      `[data-cite]:not([data-cite^="#"])`
+    );
+    const { key: parentKey, isNormative: closestIsNormative } = closest
+      ? toCiteDetails(closest)
+      : { key: THIS_SPEC, isNormative: false };
+    dataset.cite = closestIsNormative ? parentKey : `?${parentKey}`;
+    dataset.citeFrag = rawKey.replace("#", ""); // the key is acting as fragment
+    return toCiteDetails(elem);
+  }
+  const frag = citeFrag ? `#${citeFrag}` : findFrag(rawKey);
+  const path = citePath || findPath(rawKey).split("#")[0]; // path is always before "#"
+  const { type } = refTypeFromContext(rawKey, elem);
+  const isNormative = type === "normative";
+  // key is before "/" and "#" but after "!" or "?" (e.g., ?key/path#frag)
+  const hasPrecedingMark = /^[?|!]/.test(rawKey);
+  const key = rawKey.split(/[/|#]/)[0].substring(Number(hasPrecedingMark));
+  const details = { key, isNormative, frag, path };
+  return details;
 }
 
 export async function run(conf) {
-  const toCiteDetails = citeDetailsConverter(conf);
+  const shortNameRegex = new RegExp(
+    String.raw`\b${conf.shortName.toLowerCase()}\b`,
+    "i"
+  );
   /** @type {NodeListOf<HTMLElement>} */
   const cites = document.querySelectorAll("dfn[data-cite], a[data-cite]");
   Array.from(cites)
     .filter(el => el.dataset.cite)
+    .map(el => {
+      el.dataset.cite = el.dataset.cite.replace(shortNameRegex, THIS_SPEC);
+      return el;
+    })
     .map(toCiteDetails)
     // it's not the same spec
-    .filter(({ key }) => {
-      return key.toLowerCase() !== (conf.shortName || "").toLowerCase();
-    })
+    .filter(({ key }) => key !== THIS_SPEC)
     .forEach(({ isNormative, key }) => {
       if (!isNormative && !conf.normativeReferences.has(key)) {
         conf.informativeReferences.add(key);
@@ -173,21 +173,15 @@ export async function run(conf) {
   sub("beforesave", cleanup);
 }
 
-/**
- * @param {Document} doc
- * @param {*} conf
- */
-export async function linkInlineCitations(doc, conf = respecConfig) {
-  const toLookupRequest = requestLookup(conf);
+export async function linkInlineCitations() {
   const elems = [
-    ...doc.querySelectorAll(
+    ...document.querySelectorAll(
       "dfn[data-cite]:not([data-cite='']), a[data-cite]:not([data-cite=''])"
     ),
   ];
-  const citeConverter = citeDetailsConverter(conf);
 
   const promisesForMissingEntries = elems
-    .map(citeConverter)
+    .map(toCiteDetails)
     .map(async entry => {
       const result = await resolveRef(entry.key);
       return { entry, result };
