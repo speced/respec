f85a8864e37ade1cfb0c15e19d23ce14e9c4cb43
Sid Vishnoi <sidvishnoi8@gmail.com>
2020-04-14 22:28:51 +0530

    nit: another param type in CaseInsensitiveMap

--- src/core/utils.js
+++ src/core/utils.js
@@ -920,7 +920,7 @@ export class CaseInsensitiveMap extends Map {
   }
   /**
    * @param {String} key
-   * @param {*} value
+   * @param {ValueType} value
    */
   set(key, value) {
     super.set(key.toLowerCase(), value);
