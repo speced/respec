9a841f49ad5ab920e2256daecbfbe223eecc592b
Sid Vishnoi <sidvishnoi8@gmail.com>
2020-06-03 14:41:56 +0530

    rename hyperHTML to html

--- src/core/dfn-panel.js
+++ src/core/dfn-panel.js
@@ -2,7 +2,7 @@
 // self link to the selected dfn. Based on Bikeshed's dfn panels at
 // https://github.com/tabatkins/bikeshed/blob/ef44162c2e/bikeshed/dfnpanels.py
 import { fetchAsset, fetchBase } from "./text-loader.js";
-import { html as hyperHTML } from "./import-maps.js";
+import { html } from "./import-maps.js";
 import { norm } from "./utils.js";
 
 export const name = "core/dfn-panel";
@@ -10,7 +10,9 @@ export const name = "core/dfn-panel";
 export async function run() {
   const css = await loadStyle();
   document.head.insertBefore(
-    hyperHTML`<style>${css}</style>`,
+    html`<style>
+      ${css}
+    </style>`,
     document.querySelector("link")
   );
 
@@ -38,7 +40,7 @@ function createPanel(dfn) {
 
   const panelId = `dfn-panel-for-${dfn.id}`;
   /** @type {HTMLElement} */
-  const panel = hyperHTML`
+  const panel = html`
     <aside class="dfn-panel" id="${panelId}" hidden>
       <span class="caret"></span>
       <b><a class="self-link" href="${href}">Permalink</a></b>
@@ -56,7 +58,9 @@ function createPanel(dfn) {
  */
 function referencesToHTML(id, links) {
   if (!links.length) {
-    return hyperHTML`<ul><li>Not referenced in this document.</li></ul>`;
+    return html`<ul>
+      <li>Not referenced in this document.</li>
+    </ul>`;
   }
 
   /** @type {Map<string, string[]>} */
@@ -86,13 +90,15 @@ function referencesToHTML(id, links) {
    * @param {[string, string[]]} entry
    * @returns {HTMLLIElement}
    */
-  const listItemToHTML = entry =>
-    hyperHTML`<li>${toLinkProps(entry).map(
-      link => hyperHTML`<a href="#${link.id}">${link.title}</a>${" "}`
-    )}</li>`;
+  const listItemToHTML = entry => html`<li>
+    ${toLinkProps(entry).map(
+      link => html`<a href="#${link.id}">${link.title}</a>${" "}`
+    )}
+  </li>`;
 
-  const listItems = [...titleToIDs].map(listItemToHTML);
-  return hyperHTML`<ul>${listItems}</ul>`;
+  return html`<ul>
+    ${[...titleToIDs].map(listItemToHTML)}
+  </ul>`;
 }
 
 /** @param {HTMLAnchorElement} link */
