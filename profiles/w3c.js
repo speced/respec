ce2b75f2be919ad3e25d668fa474ceea7021b918
Sid Vishnoi <sidvishnoi8@gmail.com>
2020-06-15 23:55:18 +0530

    feat(core/web-monetization): support adding monetization meta tag

--- profiles/w3c.js
+++ profiles/w3c.js
@@ -64,6 +64,7 @@ const modules = [
   import("../src/core/algorithms.js"),
   import("../src/core/anchor-expander.js"),
   import("../src/core/custom-elements/index.js"),
+  import("../src/core/web-monetization.js"),
   /* Linters must be the last thing to run */
   import("../src/core/linter.js"),
   import("../src/core/a11y.js"),
---
+++ src/core/web-monetization.js
@@ -0,0 +1,28 @@
+// @ts-check
+/**
+ * This module adds a "monetization" meta-tag to enable web-monetization.
+ *
+ * The meta-tag is added only to "live" documents, and is removed from generated
+ * static documents.
+ */
+import { html } from "./import-maps.js";
+
+export const name = "core/web-monetization";
+
+const DEFAULT_PAYMENT_POINTER = "$wallet.example.com/respec";
+
+export function run(conf) {
+  const { monetization } = conf;
+  if (monetization === false) {
+    return;
+  }
+
+  const paymentPointer =
+    typeof monetization === "string" ? monetization : DEFAULT_PAYMENT_POINTER;
+
+  document.head.append(html`<meta
+    name="monetization"
+    content="${paymentPointer}"
+    class="removeOnSave"
+  />`);
+}
