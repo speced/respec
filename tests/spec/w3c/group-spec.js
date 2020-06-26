a25d8f89eea23350a5aee97bbf263bc91be326e8
Sid Vishnoi <sidvishnoi8@gmail.com>
2020-06-26 15:09:02 +0530

    nit: avoid destructring in test

--- tests/spec/w3c/group-spec.js
+++ tests/spec/w3c/group-spec.js
@@ -13,29 +13,27 @@ describe("W3C — Group", () => {
   }
 
   it("adds group details when a single group is specified", async () => {
-    const { wg, wgId, wgPatentURI, wgURI } = await getGroupConf({
-      group: "payments",
-    });
-    expect(wg).toBe("Web Payments Working Group");
-    expect(wgId).toBe("83744");
-    expect(wgPatentURI).toBe("https://www.w3.org/2004/01/pp-impl/83744/status");
-    expect(wgURI).toBe("https://www.w3.org/Payments/WG/");
+    const conf = await getGroupConf({ group: "payments" });
+    expect(conf.wg).toBe("Web Payments Working Group");
+    expect(conf.wgId).toBe("83744");
+    expect(conf.wgPatentURI).toBe(
+      "https://www.w3.org/2004/01/pp-impl/83744/status"
+    );
+    expect(conf.wgURI).toBe("https://www.w3.org/Payments/WG/");
   });
 
   it("adds group details when a multiple groups are specified", async () => {
-    const { wg, wgId, wgPatentURI, wgURI } = await getGroupConf({
-      group: ["payments", "webapps"],
-    });
-    expect(wg).toEqual([
+    const conf = await getGroupConf({ group: ["payments", "webapps"] });
+    expect(conf.wg).toEqual([
       "Web Payments Working Group",
       "Web Applications Working Group",
     ]);
-    expect(wgId).toEqual([83744, 114929]);
-    expect(wgPatentURI).toEqual([
+    expect(conf.wgId).toEqual([83744, 114929]);
+    expect(conf.wgPatentURI).toEqual([
       "https://www.w3.org/2004/01/pp-impl/83744/status",
       "https://www.w3.org/2004/01/pp-impl/114929/status",
     ]);
-    expect(wgURI).toEqual([
+    expect(conf.wgURI).toEqual([
       "https://www.w3.org/Payments/WG/",
       "https://www.w3.org/2019/webapps/",
     ]);
@@ -43,22 +41,20 @@ describe("W3C — Group", () => {
 
   it("overrides superseded options", async () => {
     const inputConf = { group: "payments", wg: "foo", wgId: "1234" };
-    const outputConf = await getGroupConf(inputConf);
-    expect(outputConf.wg).not.toBe(inputConf.wg);
-    expect(outputConf.wgId).not.toBe(inputConf.wgId);
-    expect(outputConf.wgPatentURI).not.toBe(inputConf.wgPatentURI);
-    expect(outputConf.wgURI).not.toBe(inputConf.wgURI);
+    const conf = await getGroupConf(inputConf);
+    expect(conf.wg).not.toBe(inputConf.wg);
+    expect(conf.wgId).not.toBe(inputConf.wgId);
+    expect(conf.wgPatentURI).not.toBe(inputConf.wgPatentURI);
+    expect(conf.wgURI).not.toBe(inputConf.wgURI);
   });
 
   it("fails if invalid group name is specified", async () => {
-    const { wg, wgId, wgPatentURI, wgURI } = await getGroupConf({
-      group: ["404", "webapps"],
-    });
-    expect(wg).toEqual(["Web Applications Working Group"]);
-    expect(wgId).toEqual([114929]);
-    expect(wgPatentURI).toEqual([
+    const conf = await getGroupConf({ group: ["404", "webapps"] });
+    expect(conf.wg).toEqual(["Web Applications Working Group"]);
+    expect(conf.wgId).toEqual([114929]);
+    expect(conf.wgPatentURI).toEqual([
       "https://www.w3.org/2004/01/pp-impl/114929/status",
     ]);
-    expect(wgURI).toEqual(["https://www.w3.org/2019/webapps/"]);
+    expect(conf.wgURI).toEqual(["https://www.w3.org/2019/webapps/"]);
   });
 });
