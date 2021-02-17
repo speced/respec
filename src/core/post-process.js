550605bf4f601365a75c3af015a37349d98056d1
Sid Vishnoi <sidvishnoi8@gmail.com>
2021-02-17 12:15:55 +0530

    refactor(core): run global plugins like regular plugins (#3312)
    run core/include-config, core/override-configuration, core/pre-process, and core/post-process as regular plugins

--- src/core/post-process.js
+++ src/core/post-process.js
@@ -10,46 +10,33 @@
  *  - afterEnd: final thing that is called.
  */
 import { showError } from "./utils.js";
-import { sub } from "./pubsubhub.js";
 
 export const name = "core/post-process";
 
-let doneResolver;
-export const done = new Promise(resolve => {
-  doneResolver = resolve;
-});
-
-sub(
-  "plugins-done",
-  async config => {
-    const result = [];
-    if (Array.isArray(config.postProcess)) {
-      const promises = config.postProcess
-        .filter(f => {
-          const isFunction = typeof f === "function";
-          if (!isFunction) {
-            const msg = "Every item in `postProcess` must be a JS function.";
-            showError(msg, name);
-          }
-          return isFunction;
-        })
-        .map(async f => {
-          try {
-            return await f(config, document);
-          } catch (err) {
-            const msg = `Function ${f.name} threw an error during \`postProcess\`.`;
-            const hint = "See developer console.";
-            showError(msg, name, { hint });
-            console.error(err);
-          }
-        });
-      const values = await Promise.all(promises);
-      result.push(...values);
-    }
-    if (typeof config.afterEnd === "function") {
-      result.push(await config.afterEnd(config, document));
-    }
-    doneResolver(result);
-  },
-  { once: true }
-);
+export async function run(config) {
+  if (Array.isArray(config.postProcess)) {
+    const promises = config.postProcess
+      .filter(f => {
+        const isFunction = typeof f === "function";
+        if (!isFunction) {
+          const msg = "Every item in `postProcess` must be a JS function.";
+          showError(msg, name);
+        }
+        return isFunction;
+      })
+      .map(async f => {
+        try {
+          return await f(config, document);
+        } catch (err) {
+          const msg = `Function ${f.name} threw an error during \`postProcess\`.`;
+          const hint = "See developer console.";
+          showError(msg, name, { hint });
+          console.error(err);
+        }
+      });
+    await Promise.all(promises);
+  }
+  if (typeof config.afterEnd === "function") {
+    await config.afterEnd(config, document);
+  }
+}
