a1796fa626f8c18b59273a216ffe2ee330e099ad
Sid Vishnoi <sidvishnoi8@gmail.com>
2021-03-19 16:02:08 +0530

    refactor(tools/builder): move copyFile worker to worker rollup

--- worker/rollup.config.js
+++ worker/rollup.config.js
@@ -2,12 +2,20 @@ import commonjs from "rollup-plugin-commonjs";
 import resolve from "rollup-plugin-node-resolve";
 import { terser } from "rollup-plugin-terser";
 
-export default {
-  input: "worker/respec-highlight.js",
-  output: {
-    dir: "builds/",
-    format: "iife",
-    name: "hljs",
+export default [
+  {
+    input: "worker/respec-highlight.js",
+    output: {
+      dir: "builds/",
+      format: "iife",
+      name: "hljs",
+    },
+    plugins: [resolve(), commonjs(), terser()],
   },
-  plugins: [resolve(), commonjs(), terser()],
-};
+  {
+    input: "worker/respec-worker.js",
+    output: {
+      dir: "builds/",
+    },
+  },
+];
