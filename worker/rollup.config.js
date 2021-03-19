6e16827b39fdf219a6924e30b8b107d89d9616bb
Sid Vishnoi <sidvishnoi8@gmail.com>
2021-03-19 15:58:43 +0530

    build respec-hljs here instead of separate repo

---
+++ worker/rollup.config.js
@@ -0,0 +1,13 @@
+import commonjs from "rollup-plugin-commonjs";
+import resolve from "rollup-plugin-node-resolve";
+import { terser } from "rollup-plugin-terser";
+
+export default {
+  input: "worker/respec-highlight.js",
+  output: {
+    dir: "builds/",
+    format: "iife",
+    name: "hljs",
+  },
+  plugins: [resolve(), commonjs(), terser()],
+};
