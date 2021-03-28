7d7d431e3fc0ad1670e510da4e08b074fa8c42c6
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2021-03-28 13:20:34 +0530

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -11,7 +11,7 @@ module.exports = config => {
         included: false,
       },
       {
-        pattern: "@(src|assets|js)/**/*.*",
+        pattern: "@(src|js)/**/*",
         included: false,
       },
       {
