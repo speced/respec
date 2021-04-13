b100c0026f2ba0d3248e1c3798e79ddff208fa66
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2021-04-13 14:57:45 +0530

    refactor(core/linter-rules/*): run as regular plugins (#3453)

--- profiles/w3c.js
+++ profiles/w3c.js
@@ -68,7 +68,12 @@ const modules = [
   import("../src/core/linter-rules/check-charset.js"),
   import("../src/core/linter-rules/check-punctuation.js"),
   import("../src/core/linter-rules/check-internal-slots.js"),
-  import("../src/core/linter.js"),
+  import("../src/core/linter-rules/local-refs-exist.js"),
+  import("../src/core/linter-rules/no-headingless-sections.js"),
+  import("../src/core/linter-rules/no-unused-vars.js"),
+  import("../src/core/linter-rules/privsec-section.js"),
+  import("../src/core/linter-rules/wpt-tests-exist.js"),
+  import("../src/core/linter-rules/no-http-props.js"),
   import("../src/core/a11y.js"),
 ];
 
--- src/core/defaults.js
+++ src/core/defaults.js
@@ -4,21 +4,6 @@
  */
 export const name = "core/defaults";
 
-import linter from "./linter.js";
-import { rule as localRefsExist } from "./linter-rules/local-refs-exist.js";
-import { rule as noHeadinglessSectionsRule } from "./linter-rules/no-headingless-sections.js";
-import { rule as noHttpPropsRule } from "./linter-rules/no-http-props.js";
-import { rule as noUnusedVars } from "./linter-rules/no-unused-vars.js";
-import { rule as privsecSection } from "./linter-rules/privsec-section.js";
-
-linter.register(
-  noHttpPropsRule,
-  noHeadinglessSectionsRule,
-  noUnusedVars,
-  localRefsExist,
-  privsecSection
-);
-
 export const coreDefaults = {
   lint: {
     "no-headingless-sections": true,
