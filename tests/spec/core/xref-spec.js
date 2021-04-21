77dddf86acd977f7987f191c7c31fe350ae0dfc7
Marcos Cáceres <marcos@marcosc.com>
2021-04-21 10:54:31 +1000

    test(core/xref-spec): URL changed

--- tests/spec/core/xref-spec.js
+++ tests/spec/core/xref-spec.js
@@ -437,7 +437,7 @@ describe("Core — xref", () => {
     const ops = makeStandardOps(config, body);
     const doc = await makeRSDoc(ops);
 
-    const expectedLink1 = `https://www.w3.org/TR/SVG/styling.html#elementdef-style`;
+    const expectedLink1 = `https://www.w3.org/TR/SVG2/styling.html#elementdef-style`;
     const expectedLink2 = `https://html.spec.whatwg.org/multipage/semantics.html#the-style-element`;
 
     const one = doc.getElementById("one");
