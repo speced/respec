52d5c598f445cd2514ee8cdeecfcad26ffcc790d
Marcos Cáceres <marcos@marcosc.com>
2021-06-17 21:44:28 +1000

    Add TS suggestion

--- src/core/utils.js
+++ src/core/utils.js
@@ -102,6 +102,7 @@ function joinFactory(type, style = "long") {
    * @template T
    * @param {string[]} items
    * @param {(value: string, index: number, array: string[]) => any} [mapper]
+   * @return (items: T[], mapper?: (item: T) => any), getter?: (item: T) => string) => {}
    */
   return (items, mapper) => {
     let elemCount = 0;
