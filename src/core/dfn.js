2e58a3a589edb5627d85631e35985ca9cc0d65f2
Marcos Cáceres <marcos@marcosc.com>
2021-07-27 14:58:05 +1000

    fix bad selector

--- src/core/dfn.js
+++ src/core/dfn.js
@@ -176,7 +176,7 @@ function processAsInternalSlot(title, dfn) {
   }
 
   // Don't export internal slots by default, as they are not supposed to be public.
-  if (!dfn.matches("export") && !dfn.matches("[data-export]")) {
+  if (!dfn.matches(".export") && !dfn.matches("[data-export]")) {
     dfn.dataset.noexport = "";
   }
 
