dcbebf9996b9a915bfaceccba8066509170c4841
Marcos Cáceres <marcos@marcosc.com>
2021-07-29 11:02:38 +1000

    feat(core/dfn): add support for task source types (#3717)

--- src/core/dfn.js
+++ src/core/dfn.js
@@ -92,6 +92,12 @@ function computeType(dfn, linkingText) {
     case slotRegex.test(linkingText):
       type = processAsInternalSlot(linkingText, dfn);
       break;
+
+    // A definition that ends with "task source" is a "task-source"...
+    case /\b task source$/i.test(linkingText):
+      type = "task-source";
+      processAsTaskSource(dfn);
+      break;
   }
 
   // Derive closest type
@@ -106,6 +112,13 @@ function computeType(dfn, linkingText) {
   }
   // Finally, addContractDefaults() will add the type to the dfn if it's not there.
   // But other modules may end up adding a type (e.g., the WebIDL module)
+}
+
+function processAsTaskSource(dfn) {
+  // Task sources are exported by default.
+  if (!dfn.matches(".no-export, [data-noexport]")) {
+    dfn.dataset.export = "";
+  }
 }
 
 // Deal with export/no export
--- tests/spec/core/dfn-spec.js
+++ tests/spec/core/dfn-spec.js
@@ -232,6 +232,59 @@ describe("Core — Definitions", () => {
     }
   });
 
+  it("automatically identifies task sources and exports them by default", async () => {
+    const body = `
+      <section>
+        <h2>Task sources</h2>
+        <p>
+          <!-- valid for export -->
+          <dfn id="task-1">I'm a
+            task
+            source
+          </dfn>
+          <!-- Won't match, because no name -->
+          <dfn id="task-2">
+            task source
+          </dfn>
+
+          <!-- explicit no export -->
+          <dfn id="task-3" data-noexport="">
+            secret
+            task source
+          </dfn>
+
+          <dfn class="no-export" id="task-4" data-dfn-type="custom-type">
+            Custom task source
+          </dfn>
+        </p>
+      </section>
+    `;
+    const ops = makeStandardOps(null, body);
+    const doc = await makeRSDoc(ops);
+
+    const dfn = doc.getElementById("task-1");
+    expect(dfn.dataset.dfnFor).toBeUndefined();
+    expect(dfn.dataset.export).toBe("");
+    expect(dfn.dataset.dfnType).toBe("task-source");
+
+    const dfn2 = doc.getElementById("task-2");
+    expect(dfn2.dataset.dfnFor).toBeUndefined();
+    expect(dfn2.dataset.export).toBeUndefined();
+    expect(dfn2.dataset.dfnType).toBe("dfn");
+
+    const dfn3 = doc.getElementById("task-3");
+    expect(dfn3.dataset.dfnFor).toBeUndefined();
+    expect(dfn3.dataset.export).toBeUndefined();
+    expect(dfn3.dataset.dfnType).toBe("task-source");
+    expect(dfn3.dataset.noexport).toBe("");
+
+    const dfn4 = doc.getElementById("task-4");
+    expect(dfn4.dataset.dfnFor).toBeUndefined();
+    expect(dfn4.dataset.export).toBeUndefined();
+    expect(dfn4.dataset.dfnType).toBe("custom-type");
+    expect(dfn4.dataset.noexport).toBe("");
+  });
+
   describe("internal slot definitions", () => {
     const body = `
       <section data-dfn-for="Test" data-cite="HTML">
