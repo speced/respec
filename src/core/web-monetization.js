d8687303402e722d02e8d8fa525a94e3a7fda9dd
Marcos Cáceres <marcos@marcosc.com>
2021-08-09 10:46:39 +1000

    feat(core/web-monetization): add object configuration

--- src/core/web-monetization.js
+++ src/core/web-monetization.js
@@ -9,25 +9,44 @@ import { html } from "./import-maps.js";
 
 export const name = "core/web-monetization";
 
-const DEFAULT_PAYMENT_POINTER = "$respec.org";
-
 export function run(conf) {
-  const { monetization } = conf;
-  if (monetization === false) {
+  if (conf.monetization === false) {
     return;
   }
+  const { monetization } = conf;
 
-  const paymentPointer =
-    typeof monetization === "string" ? monetization : DEFAULT_PAYMENT_POINTER;
+  const { removeOnSave, paymentPointer } = canonicalizeConfig(monetization);
 
-  if (paymentPointer === DEFAULT_PAYMENT_POINTER) {
-    document.head.append(
-      html`<!-- Support ReSpec's development - https://opencollective.com/respec --> `
-    );
-  }
+  const cssClass = removeOnSave ? "removeOnSave" : null;
   document.head.append(html`<meta
     name="monetization"
     content="${paymentPointer}"
-    class="removeOnSave"
+    class="${cssClass}"
   />`);
+}
+
+/**
+ * @param {object|string} rawConfig
+ * - {string} paymentPointer - The payment pointer to use.
+ * - {boolean} removeOnSave - Whether to remove the meta tag when the document is saved.
+ */
+function canonicalizeConfig(rawConfig) {
+  const config = {
+    paymentPointer: "$respec.org",
+    removeOnSave: true,
+  };
+  switch (typeof rawConfig) {
+    case "string":
+      config.paymentPointer = rawConfig;
+      break;
+    case "object":
+      if (rawConfig.paymentPointer) {
+        config.paymentPointer = String(rawConfig.paymentPointer);
+      }
+      if (rawConfig.removeOnSave === false) {
+        config.removeOnSave = false;
+      }
+      break;
+  }
+  return config;
 }
