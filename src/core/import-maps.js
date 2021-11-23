fcc4a4d7d9e4cd437c78d4b8cdae0ae8b96b7060
Marcos Cáceres <marcos@marcosc.com>
2021-11-23 11:18:46 +1100

    chore(package): update deps (#3891)

--- src/core/import-maps.js
+++ src/core/import-maps.js
@@ -1,7 +1,7 @@
 // @ts-check
 // Temporary workaround until browsers get real import-maps
 
-import * as _idb from "../../node_modules/idb/build/esm/index.js";
+import * as _idb from "../../node_modules/idb/build/index.js";
 import * as _webidl2 from "../../node_modules/webidl2/index.js";
 import { MIMEType as _MIMEType } from "../../node_modules/sniffy-mimetype/index.js";
 import { marked as _marked } from "../../node_modules/marked/lib/marked.esm.js";
