a83b8d88126c9e425d2dd68f0b0b40da982263fd
Marcos Cáceres <marcos@marcosc.com>
2022-01-31 14:31:35 +1100

    Update src/w3c/headers.js
    Co-authored-by: Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>

--- src/w3c/headers.js
+++ src/w3c/headers.js
@@ -107,7 +107,7 @@ export const name = "w3c/headers";
 // We use an "Australian Date" because it omits the ","
 // after the day of the month, which is required by the W3C.
 const dateLang = lang === "en" || lang.startsWith("en-") ? "en-AU" : lang;
-const W3CDate = new Intl.DateTimeFormat([dateLang], {
+const W3CDate = new Intl.DateTimeFormat(dateLang, {
   timeZone: "UTC",
   year: "numeric",
   month: "long",
