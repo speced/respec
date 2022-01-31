febdd01f27091a11dca424f76c3e1393eb401c8d
Marcos Cáceres <marcos@marcosc.com>
2022-01-31 16:37:25 +1100

    Add docs for pubsubhub again

--- src/core/pubsubhub.js
+++ src/core/pubsubhub.js
@@ -11,11 +11,11 @@ import { expose } from "./expose-modules.js";
 
 const subscriptions = new EventTarget();
 
-export function sub(topic, cb, options = { once: false }) {
-  const listener = e => cb(e.detail);
-  subscriptions.addEventListener(topic, listener, options);
-}
-
+/**
+ *
+ * @param {EventTopic} topic
+ * @param  {any} detail
+ */
 export function pub(topic, detail) {
   subscriptions.dispatchEvent(new CustomEvent(topic, { detail }));
   if (window.parent === window.self) {
@@ -24,6 +24,20 @@ export function pub(topic, detail) {
   // If this is an iframe, postMessage parent (used in testing).
   const args = String(JSON.stringify(detail?.stack || detail));
   window.parent.postMessage({ topic, args }, window.parent.location.origin);
+}
+
+/**
+ * Subscribes to a message type.
+ * @param  {EventTopic} topic The topic to subscribe to
+ * @param  {Function} cb         Callback function
+ * @param  {Object} [options]
+ * @param  {Boolean} [options.once] Add prop "once" for single notification.
+ * @return {Object}              An object that should be considered opaque,
+ *                               used for unsubscribing from messages.
+ */
+export function sub(topic, cb, options = { once: false }) {
+  const listener = e => cb(e.detail);
+  subscriptions.addEventListener(topic, listener, options);
 }
 
 expose(name, { sub });
