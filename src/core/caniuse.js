96489e566afbf117a429a81fc0be201c5d945f46
Marcos Cáceres <marcos@marcosc.com>
2022-03-08 19:22:41 +1100

    feat(core/caniuse): simplified caniuse display

--- src/core/caniuse.js
+++ src/core/caniuse.js
@@ -35,8 +35,9 @@ export function prepare(conf) {
   if (!conf.caniuse) {
     return; // nothing to do.
   }
-  const options = getNormalizedConf(conf);
-  conf.caniuse = options; // for tests
+  normalizeConf(conf);
+  validateBrowsers(conf);
+  const options = conf.caniuse;
   if (!options.feature) {
     return; // no feature to show
   }
@@ -47,12 +48,10 @@ export function prepare(conf) {
   >
     ${css}
   </style>`);
+}
 
-  const apiUrl = options.apiURL || API_URL;
-  // Initiate a fetch, but do not wait. Try to fill the cache early instead.
-  conf.state[name] = {
-    fetchPromise: fetchStats(apiUrl, options),
-  };
+function getLogoSrc(browser) {
+  return `https://cdnjs.cloudflare.com/ajax/libs/browser-logos/71.0.0/${browser}/${browser}.svg`;
 }
 
 export async function run(conf) {
@@ -60,31 +59,19 @@ export async function run(conf) {
   if (!options?.feature) return;
 
   const featureURL = new URL(options.feature, "https://caniuse.com/").href;
-
   const headDlElem = document.querySelector(".head dl");
-  const contentPromise = (async () => {
-    try {
-      const stats = await conf.state[name].fetchPromise;
-      return html`${{ html: stats }}`;
-    } catch (err) {
-      const msg = `Couldn't find feature "${options.feature}" on caniuse.com.`;
-      const hint = docLink`Please check the feature key on [caniuse.com](https://caniuse.com) and update ${"[caniuse]"}`;
-      showError(msg, name, { hint });
-      console.error(err);
-      return html`<a href="${featureURL}">caniuse.com</a>`;
-    }
-  })();
   const definitionPair = html`<dt class="caniuse-title">
       Browser support (caniuse.com):
     </dt>
     <dd class="caniuse-stats">
       ${{
-        any: contentPromise,
+        any: fetchStats(conf.caniuse)
+          .catch(err => handleError(err, options, featureURL))
+          .then(json => processJson(json, options)),
         placeholder: "Fetching data from caniuse.com...",
       }}
     </dd>`;
   headDlElem.append(...definitionPair.childNodes);
-  await contentPromise;
   pub("amend-user-config", { caniuse: options.feature });
   if (options.removeOnSave) {
     // Will remove the browser support cells.
@@ -98,48 +85,70 @@ export async function run(conf) {
   }
 }
 
+function handleError(err, options, featureURL) {
+  const msg = `Couldn't find feature "${options.feature}".`;
+  const hint = docLink`Please check the feature key on [caniuse.com](https://caniuse.com) and update ${"[caniuse]"}.`;
+  showError(msg, name, { hint });
+  console.error(err);
+  return html`<a href="${featureURL}">caniuse.com</a>`;
+}
+
 /**
  * returns normalized `conf.caniuse` configuration
  * @param {Object} conf   configuration settings
  */
-function getNormalizedConf(conf) {
-  const DEFAULTS = { versions: 4, removeOnSave: false };
+function normalizeConf(conf) {
+  const DEFAULTS = { removeOnSave: false, apiURL: API_URL, browsers: [] };
   if (typeof conf.caniuse === "string") {
-    return { feature: conf.caniuse, ...DEFAULTS };
+    conf.caniuse = { feature: conf.caniuse, ...DEFAULTS };
+    return;
   }
-  const caniuseConf = { ...DEFAULTS, ...conf.caniuse };
-  const { browsers } = caniuseConf;
-  if (Array.isArray(browsers)) {
-    const invalidBrowsers = browsers.filter(browser => !BROWSERS.has(browser));
-    if (invalidBrowsers.length) {
-      const names = codedJoinAnd(invalidBrowsers, { quotes: true });
-      const msg = docLink`Invalid browser(s): (${names}) in the \`browser\` property of ${"[caniuse]"}.`;
-      showWarning(msg, name);
-    }
+  conf.caniuse = { ...DEFAULTS, ...conf.caniuse };
+}
+function validateBrowsers({ caniuse }) {
+  const { browsers } = caniuse;
+  const invalidBrowsers = browsers.filter(browser => !BROWSERS.has(browser));
+  if (invalidBrowsers.length) {
+    const names = codedJoinAnd(invalidBrowsers, { quotes: true });
+    const msg = docLink`Invalid browser(s): (${names}) in the \`browser\` property of ${"[caniuse]"}.`;
+    showWarning(msg, name);
   }
-  return caniuseConf;
+}
+
+async function processJson(json, { feature }) {
+  const results = json.result;
+  const out = results.map(({ browser, version, caniuse }) => {
+    const ariaLabel =
+      caniuse === "u"
+        ? `${feature} is not supported in this browser.`
+        : `${feature} is supported since version ${version}.`;
+    return html`<li>
+      <img
+        width="24"
+        height="24"
+        src="${getLogoSrc(browser)}"
+        alt="${`${browser} ${version}`}"
+        title="${`${browser} ${version}`}"
+        area-label="${ariaLabel}"
+      />
+    </li>`;
+  });
+  return out;
 }
 
 /**
- * @param {string} apiURL
  * @typedef {Record<string, [string, string[]][]>} ApiResponse
  * @throws {Error} on failure
  */
-async function fetchStats(apiURL, options) {
-  const { feature, versions, browsers } = options;
-  const searchParams = new URLSearchParams();
-  searchParams.set("feature", feature);
-  searchParams.set("versions", versions);
-  if (Array.isArray(browsers)) {
-    searchParams.set("browsers", browsers.join(","));
-  }
-  searchParams.set("format", "html");
-  const url = `${apiURL}?${searchParams.toString()}`;
+async function fetchStats(options) {
+  const { feature, browsers, apiURL } = options;
+  const url = new URL(`./${feature}/`, apiURL);
+  browsers.forEach(browser => url.searchParams.append("browsers", browser));
   const response = await fetch(url);
   if (!response.ok) {
     const { status, statusText } = response;
     throw new Error(`Failed to get caniuse data: (${status}) ${statusText}`);
   }
-  const stats = await response.text();
+  const stats = await response.json();
   return stats;
 }
