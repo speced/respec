4686d8c2ad50a5f23bc117fbd28f1ec588af1754
Marcos Cáceres <marcos@marcosc.com>
2022-04-28 15:00:32 +1000

    chore: update deps + imports

--- src/core/import-maps.js
+++ src/core/import-maps.js
@@ -1,22 +1,14 @@
 // @ts-check
 // Temporary workaround until browsers get real import-maps
 
-import * as _idb from "../../node_modules/idb/build/esm/index.js";
+import * as _idb from "../../node_modules/idb/build/index.js";
 import * as _webidl2 from "../../node_modules/webidl2/index.js";
-import _marked from "../../node_modules/marked/lib/marked.esm.js";
+import { marked as _marked } from "../../node_modules/marked/lib/marked.esm.js";
 import _pluralize from "../../js/deps/builds/pluralize.js";
 import hyperHTML from "../../node_modules/hyperhtml/esm.js";
 
-/** @type {import("idb")} */
-// @ts-ignore
-export const idb = _idb;
-export const webidl2 = _webidl2;
-/** @type {import("hyperhtml").default} */
-// @ts-ignore
 export const html = hyperHTML;
-/** @type {import("marked")} */
-// @ts-ignore
+export const idb = _idb;
 export const marked = _marked;
-/** @type {import("pluralize")} */
-// @ts-ignore
 export const pluralize = _pluralize;
+export const webidl2 = _webidl2;
