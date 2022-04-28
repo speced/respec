78b6f3b12741046f8efb4328af37847d50e92533
Marcos Cáceres <marcos@marcosc.com>
2022-04-28 17:06:45 +1000

    chore: update deps + imports (#4141)

--- src/core/import-maps.js
+++ src/core/import-maps.js
@@ -8,19 +8,9 @@ import { marked as _marked } from "../../node_modules/marked/lib/marked.esm.js";
 import _pluralize from "../../js/deps/builds/pluralize.js";
 import hyperHTML from "../../node_modules/hyperhtml/esm.js";
 
-/** @type {import("idb")} */
-// @ts-ignore
-export const idb = _idb;
-export const webidl2 = _webidl2;
-/** @type {import("hyperhtml").default} */
-// @ts-ignore
 export const html = hyperHTML;
-/** @typeof marked */
+export const idb = _idb;
 export const marked = _marked;
-/** @type {import("pluralize")} */
-// @ts-ignore
-export const pluralize = _pluralize;
-
-/** @type {import("sniffy-mimetype")} */
-// @ts-ignore
 export const MIMEType = _MIMEType;
+export const pluralize = _pluralize;
+export const webidl2 = _webidl2;
