93f125b803fec99a40d863bcea8032c62f2a846e
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2022-10-11 17:44:06 +0530

    feat(core/algorithms): link `Assert:` to Infra spec dfn (#4288)

--- src/core/algorithms.js
+++ src/core/algorithms.js
@@ -3,17 +3,38 @@
 Currently used only for adding 'assert' class to algorithm lists
 */
 import css from "../styles/algorithms.css.js";
+import { html } from "./import-maps.js";
 
 export const name = "core/algorithms";
 
 export function run() {
-  const elements = Array.from(document.querySelectorAll("ol.algorithm li"));
-  elements
-    .filter(li => li.textContent.trim().startsWith("Assert: "))
-    .forEach(li => li.classList.add("assert"));
-  if (document.querySelector(".assert")) {
-    const style = document.createElement("style");
-    style.textContent = css;
-    document.head.appendChild(style);
+  const elements = Array.from(
+    /** @type {NodeListOf<HTMLLIElement>} */ (
+      document.querySelectorAll("ol.algorithm li")
+    )
+  ).filter(li => li.textContent.trim().startsWith("Assert: "));
+  if (!elements.length) {
+    return;
   }
+
+  for (const li of elements) {
+    li.classList.add("assert");
+
+    // Link "Assert" to https://infra.spec.whatwg.org/#assert
+    const textNode = li.firstChild;
+    if (
+      textNode instanceof Text &&
+      textNode.textContent.startsWith("Assert: ")
+    ) {
+      textNode.textContent = textNode.textContent.replace("Assert: ", "");
+      li.prepend(
+        html`<a href="https://infra.spec.whatwg.org/#assert">Assert</a>`,
+        ": "
+      );
+    }
+  }
+
+  const style = document.createElement("style");
+  style.textContent = css;
+  document.head.appendChild(style);
 }
