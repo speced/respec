1c5d8aed591865b97b4ade84b7d614ba16c8e654
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2022-10-11 15:59:21 +0530

    feat(core/algorithms): link `Assert:` to Infra spec dfn

--- src/core/algorithms.js
+++ src/core/algorithms.js
@@ -3,6 +3,7 @@
 Currently used only for adding 'assert' class to algorithm lists
 */
 import css from "../styles/algorithms.css.js";
+import { html } from "./import-maps.js";
 
 export const name = "core/algorithms";
 
@@ -18,6 +19,19 @@ export function run() {
 
   for (const li of elements) {
     li.classList.add("assert");
+
+    // Link "Assert" to https://infra.spec.whatwg.org/#assert
+    const textNode = li.firstChild;
+    if (
+      textNode instanceof Text &&
+      textNode.textContent.startsWith("Assert: ")
+    ) {
+      textNode.textContent = textNode.textContent.replace("Assert: ", "");
+      li.prepend(
+        html`<a href="https://infra.spec.whatwg.org/#assert">Assert</a>`,
+        ": "
+      );
+    }
   }
 
   const style = document.createElement("style");
