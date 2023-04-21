6f643839bc13afecb883626dd822e2b4bb6e379e
Nigel Megitt <nigel.megitt@bbc.co.uk>
2023-04-21 14:27:44 +0100

    Properly revert change to norm()

--- src/core/utils.js
+++ src/core/utils.js
@@ -174,9 +174,7 @@ export function xmlEscape(str) {
  * @param {string} str
  */
 export function norm(str) {
-  return str
-    .trim()
-    .replace(/\s+/g, " ");
+  return str.trim().replace(/\s+/g, " ");
 }
 
 /**
