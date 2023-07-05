eb9587f84ab0811791158e19f4afe794262a2458
Alexander Green <alexander.green@logius.nl>
2023-07-05 11:16:42 +0200

    Moved sidelabel colour to `<style>` #54

--- src/logius/label.js
+++ src/logius/label.js
@@ -1,4 +1,3 @@
-import css from "../styles/label.css.js";
 import { showWarning } from "../core/utils.js";
 
 export const name = "logius/label";
@@ -22,12 +21,23 @@ async function createLabel(conf) {
   sideLabel.innerHTML = `${conf.nl_organisationName} ${conf.typeText} - ${conf.statusText}`;
 
   sideLabel.setAttribute("class", "sidelabel");
-  sideLabel.setAttribute("style", `background-color: ${labelColor};`);
   document.body.appendChild(sideLabel);
 
   if (document.querySelector(".sidelabel")) {
     const style = document.createElement("style");
-    style.textContent = css;
+    style.textContent = `
+  .sidelabel {
+    position: fixed;
+    -webkit-transform-origin: top right;
+    right: 100%;
+    top: 0;
+    -webkit-transform: rotate(-90deg);
+    padding: 4px 50px 4px 10px;
+    color: white;
+    white-space: nowrap;
+    z-index: 1;
+    background-color: ${labelColor};
+  }`;
     document.head.appendChild(style);
   }
 }
