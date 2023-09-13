f8259063204aa469dea279f5d0d5823d183b80ed
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2023-09-13 12:25:33 +0530

    chore(deps): update dependencies, pnpm (#4537)

--- worker/rollup.config.js
+++ worker/rollup.config.js
@@ -1,6 +1,6 @@
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
-import { terser } from "rollup-plugin-terser";
+import terser from "@rollup/plugin-terser";
 
 export default [
   {
