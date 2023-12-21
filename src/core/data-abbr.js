042f7bc90bd08bae2a363b0e847bc0871057f205
Marcos Caceres <marcos@marcosc.com>
2023-12-21 14:01:08 +1100

    refactor(data-abbr): doc and code improvements

--- src/core/data-abbr.js
+++ src/core/data-abbr.js
@@ -1,9 +1,19 @@
 // @ts-check
-// Module core/data-abbr
-// - Finds all elements with data-abbr attribute and processes them.
-import { showError } from "./utils.js";
-export const name = "core/dfn-abbr";
+/**
+ * Module core/data-abbr
+ *
+ * Finds all elements with the data-abbr attribute and processes them.
+ *
+ * @module core/data-abbr
+ */
 
+import { showError } from "./utils.js";
+
+export const name = "core/data-abbr";
+
+/**
+ * Runs the data-abbr processing on elements with the data-abbr attribute.
+ */
 export function run() {
   /** @type {NodeListOf<HTMLElement>} */
   const elements = document.querySelectorAll("[data-abbr]");
@@ -23,31 +33,50 @@ export function run() {
     }
   }
 }
+
 /**
- * @param {HTMLElement} dfn
+ * Processes the dfn element with the data-abbr attribute.
+ * @param {HTMLElement} dfn - The dfn element.
  */
 function processDfnElement(dfn) {
   const abbr = generateAbbreviation(dfn);
-  // get normalized <dfn> textContent to remove spaces, tabs, new lines.
   const fullForm = dfn.textContent.replace(/\s\s+/g, " ").trim();
-  dfn.insertAdjacentHTML(
-    "afterend",
-    ` (<abbr title="${fullForm}">${abbr}</abbr>)`
-  );
-  const lt = dfn.dataset.lt || "";
-  dfn.dataset.lt = lt
-    .split("|")
-    .filter(i => i.trim())
-    .concat(abbr)
-    .join("|");
+  const abbrMarkup = createAbbreviationMarkup(fullForm, abbr);
+  dfn.insertAdjacentHTML("afterend", abbrMarkup);
+  updateLtAttribute(dfn, abbr);
 }
 
+/**
+ * Generates the abbreviation for the dfn element.
+ * @param {HTMLElement} elem - The dfn element.
+ * @returns {string} The generated abbreviation.
+ */
 function generateAbbreviation(elem) {
   if (elem.dataset.abbr) return elem.dataset.abbr;
-  // Generates abbreviation from textContent
-  // e.g., "Permanent Account Number" -> "PAN"
   return elem.textContent
     .match(/\b([a-z])/gi)
     .join("")
     .toUpperCase();
+}
+
+/**
+ * Creates the HTML markup for the abbreviation.
+ * @param {string} fullForm - The full form text.
+ * @param {string} abbr - The generated abbreviation.
+ * @returns {string} The abbreviation markup.
+ */
+function createAbbreviationMarkup(fullForm, abbr) {
+  return ` (<abbr title="${fullForm}">${abbr}</abbr>)`;
+}
+
+/**
+ * Updates the `data-lt` attribute of the dfn element with the generated abbreviation.
+ * @param {HTMLElement} dfn - The dfn element.
+ * @param {string} abbr - The generated abbreviation.
+ */
+function updateLtAttribute(dfn, abbr) {
+  const lt = dfn.dataset.lt || "";
+  const ltValues = lt.split("|").filter(i => i.trim());
+  ltValues.push(abbr);
+  dfn.dataset.lt = ltValues.join("|");
 }
