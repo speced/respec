9e2472a0ac5547ae00e654c2c18b2b74b3746ccc
Alexander Green <alexander.green@logius.nl>
2024-07-08 17:19:22 +0200

    conformance as separate function in override

--- src/logius/override.js
+++ src/logius/override.js
@@ -3,16 +3,23 @@ import { html } from "../core/import-maps.js";
 export const name = "logius/override";
 
 export function run(conf) {
-    const conformance = document.querySelector("section#conformance");
-    if (conformance && !conformance.classList.contains("override")) {
-        try {
-            const l10n = getIntlData(conf.localizationStrings);
-            const content = html`<p>${l10n.conformance}</p>`;
-            const heading = conformance.querySelector("h2");
-            conformance.replaceChildren(heading, content);
-        } catch {
-            // no custom conformance text
-        };
+    if (!conf.override) {
+        return;
     }
+    if (conformance && !conformance.classList.contains("override")) {
+        overrideConformance(conf);
+    }
+}
+
+function overrideConformance(conf) {
+    const conformance = document.querySelector("section#conformance");
+    try {
+        const l10n = getIntlData(conf.override);
+        const content = html`<p>${l10n.conformance}</p>`;
+        const heading = conformance.querySelector("h2");
+        conformance.replaceChildren(heading, content);
+    } catch {
+        // no custom conformance text
+    };
 }
 
