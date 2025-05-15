64d7c1b4d40d12069f67789e0adf2fd129e94c61
Kenneth G. Franqueiro <kfranqueiro@users.noreply.github.com>
2025-05-15 16:00:45 -0400

    Fix pre/postProcess to run async functions in order
    TODO: update integration tests

--- src/core/pre-process.js
+++ src/core/pre-process.js
@@ -14,26 +14,24 @@ export const name = "core/pre-process";
 
 export async function run(config) {
   if (Array.isArray(config.preProcess)) {
-    const promises = config.preProcess
-      .filter(f => {
-        const isFunction = typeof f === "function";
-        if (!isFunction) {
-          const msg = "Every item in `preProcess` must be a JS function.";
-          showError(msg, name);
-        }
-        return isFunction;
-      })
-      .map(async (f, i) => {
-        const fnName = `${name}/${f.name || `[${i}]`}`;
-        const utils = makePluginUtils(fnName);
-        try {
-          return await f(config, document, utils);
-        } catch (err) {
-          const msg = `Function ${f.name} threw an error during \`preProcess\`.`;
-          const hint = "See developer console.";
-          showError(msg, name, { hint, cause: err });
-        }
-      });
-    await Promise.all(promises);
+    const functions = config.preProcess.filter(f => {
+      const isFunction = typeof f === "function";
+      if (!isFunction) {
+        const msg = "Every item in `preProcess` must be a JS function.";
+        showError(msg, name);
+      }
+      return isFunction;
+    });
+    for (const [i, f] of functions.entries()) {
+      const fnName = `${name}/${f.name || `[${i}]`}`;
+      const utils = makePluginUtils(fnName);
+      try {
+        await f(config, document, utils);
+      } catch (err) {
+        const msg = `Function ${f.name} threw an error during \`preProcess\`.`;
+        const hint = "See developer console.";
+        showError(msg, name, { hint, cause: err });
+      }
+    }
   }
 }
