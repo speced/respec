ae73f00da97d1c5dd11a7637ed7937078274c320
Dominique Hazael-Massieux <dom@w3.org>
2025-07-22 10:14:13 +0200

    Integrate optimization from feedback
    https://github.com/speced/respec/pull/5014#pullrequestreview-3041716776

--- src/core/dfn.js
+++ src/core/dfn.js
@@ -72,7 +72,8 @@ export function run() {
     computeExport(dfn);
 
     // Only add `lt`s that are different from the text content and local-lts
-    const lt = titles.filter(t => !((dfn.dataset.localLt || "").split("|").includes(t)));
+    const localLt = (dfn.dataset.localLt || "").split("|");
+    const lt = titles.filter(t => !localLt.includes(t));
     if (lt.length > 1 || linkingText !== norm(dfn.textContent)) {
       dfn.dataset.lt = lt.join("|");
     }
