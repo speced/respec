3b8e836fa1209a7911a4e991143fb00d6f2c07ae
Daniel Montalvo <49305434+daniel-montalvo@users.noreply.github.com>
2026-02-27 10:07:04 +0100

    Update tests/spec/core/custom-elements/rs-changelog-spec.js
    Co-authored-by: Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>

--- tests/spec/core/custom-elements/rs-changelog-spec.js
+++ tests/spec/core/custom-elements/rs-changelog-spec.js
@@ -60,6 +60,6 @@ describe("Core - Custom Elements - <rs-changelog>", () => {
     // Verify links point to the custom repo, not the config repo
     const links = firstCommit.querySelectorAll("a");
     expect(links).toHaveSize(1);
-    expect(links[0].href).toBe("https://github.com/org/repo/commit/80aad0a");
+    expect(links[0].href).toBe("https://github.com/org/corp/commit/80aad0a");
   });
 });
